89f8776ae0c87d2e54175c2e527f22722b4c7177
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-02-11 11:16:39 +0200

    small fix of validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = current_user.posts.todays
+    @posts = current_user.posts#.todays
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post < ActiveRecord::Base
   serialize :social_networks, Hash
   belongs_to :user
   validates :text, presence: true
+  validates :scheduled_date, presence: true
 
   after_create :perform_worker
 
-  scope :todays, -> { where("scheduled_date > ? AND scheduled_date < ?", DateTime.new.at_beginning_of_day, DateTime.now) }
+  scope :todays, -> { where("scheduled_date > ? AND scheduled_date < ?", DateTime.now.beginning_of_day, DateTime.now.end_of_day) }
 
   def perform_worker
     PostWorker.perform_async()
