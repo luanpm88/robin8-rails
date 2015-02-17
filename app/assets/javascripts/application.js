5186b7ca56009a000edab92fc4236f556abd18ea
kbokhonko <kolya.bokhonko@gmail.com>
2015-02-17 16:53:52 +0200

    temp commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,35 +58,7 @@ ready = function() {
     },
     delay: 10000,
   });
-  //end default options for Bootstrap Growl
 
-  // // load the SDK Asynchronously
-  // window.fbAsyncInit = function() {
-  //   FB.init({
-  //     appId      : window.fb_api_key, //need change id
-  //     xfbml      : true,
-  //     version    : 'v2.2'
-  //   });
-  // };
-
-  // (function(d, s, id){
-  //    var js, fjs = d.getElementsByTagName(s)[0];
-  //    if (d.getElementById(id)) {return;}
-  //    js = d.createElement(s); js.id = id;
-  //    js.src = "//connect.facebook.net/en_US/sdk.js";
-  //    fjs.parentNode.insertBefore(js, fjs);
-  //  }(document, 'script', 'facebook-jssdk'));
-  // // end load the SDK Asynchronously
-
-  // // load Google+ sdk
-  // (function() {
-  //   var po = document.createElement('script');
-  //   po.type = 'text/javascript'; po.async = true;
-  //   po.src = 'https://plus.google.com/js/client:plusone.js';
-  //   var s = document.getElementsByTagName('script')[0];
-  //   s.parentNode.insertBefore(po, s);
-  // })()
-  // // end load Google+ sdk
   $('html').click(function(e) {
     if ($(e.target).closest('form').length == 0) {
       $('.navbar-search-lg').hide();
@@ -94,13 +66,6 @@ ready = function() {
       $('.progressjs-progress').hide();
     };
   });
-
-  // Navigation
-  // $(document).on("click", '#sidebar li a', function(e) {
-    // $('#sidebar li.active').removeClass('active');
-    // $(this).parent().addClass('active');
-  // });
-  // End Navigation
 
 };
 
