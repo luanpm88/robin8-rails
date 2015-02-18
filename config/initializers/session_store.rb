970202336f52ef71877107d9ef972a2f867eec9b
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-02-18 12:19:15 +0200

    changes staging configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 if Rails.env.development?
   Rails.application.config.session_store :cookie_store, key: '_robin8_session', domain: 'lvh.me'
 elsif Rails.env.staging?
-  Rails.application.config.session_store :cookie_store, key: '_robin8_session', domain: 'robin8-staging.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_robin8_session'
 elsif Rails.env.production?
   Rails.application.config.session_store :cookie_store, key: '_robin8_session', domain: 'robin8.com'
 end
