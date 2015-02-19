e9626ebbe689fd427fb1814231195a64c11fd7e5
Alex Pletnyov <oleksiy.pletnov@perfectial.com>
2015-02-19 12:59:20 +0200

    Destroy layout on stopping Monitoring module (ref #AYL-38)

--- app/assets/javascripts/backbone/modules/monitoring/show/show-controller.js
+++ app/assets/javascripts/backbone/modules/monitoring/show/show-controller.js
@@ -3,12 +3,8 @@ Robin.module('Monitoring.Show', function(Show, App, Backbone, Marionette, $, _){
   Show.Controller = {
 
     showMonitoringPage: function(){
-      var monitoringView = this.getMonitoringPageView();
-      Robin.layouts.main.content.show(monitoringView);
-    },
-
-    getMonitoringPageView: function(){
-      return new Show.LayoutView();
+      this.layout = new Show.LayoutView();
+      Robin.layouts.main.content.show(this.layout);
     }
   }
 
