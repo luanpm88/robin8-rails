33e2cfcc981552a05e21d405faf20c66bf9a2d20
entwood <yaroslav.zaletskyy@gmail.com>
2015-02-20 14:08:28 +0200

    minor fix

--- app/assets/javascripts/backbone/layouts/main.js
+++ app/assets/javascripts/backbone/layouts/main.js
@@ -17,10 +17,12 @@ Robin.Views.Layouts.Main = Backbone.Marionette.LayoutView.extend({
     var fName = Robin.currentUser.attributes.first_name;
     var lName = Robin.currentUser.attributes.last_name;
     var email = Robin.currentUser.attributes.email;
-    if (fName != null && lName != null) {
-      nameHolder.text(fName + ' ' + lName)
-    } else if (email != null) {
-      nameHolder.text(email)
+    console.log (nameHolder);
+    console.log (Robin.currentUser.attributes.email);
+    if (fName.length > 0 && lName.length > 0) {
+      nameHolder.text(fName + ' ' + lName);
+    } else if (email.length > 0) {
+      nameHolder.text(email);
     }
   },
 
