d7ac17c96afda7bd7e3993840aed4751a6458af9
Oleksandr Bratashov <abratashov@gmail.com>
2015-02-24 12:00:59 +0200

    AYL-66 added media file uploading for releases

--- app/models/release.rb
+++ app/models/release.rb
@@ -5,5 +5,8 @@ class Release < ActiveRecord::Base
   validates :user_id, presence: true
   validates :title, presence: true
 
+  has_many :attachments, as: :imageable, dependent: :destroy
+  accepts_nested_attributes_for :attachments, allow_destroy: true
+
   scope :by_news_room, ->(id) {where(news_room_id: id)}
 end
