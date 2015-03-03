6eab53eebe292b5c0216e5e1851ebce418f5d63b
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-03-03 08:29:56 +0200

    mailgun, ga, small fixes

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,13 +1,9 @@
 class FollowersController < ApplicationController
   
   def add
-    RestClient.post("#{url}lists/test@mg.robin8.com/members", email: params[:email])
+    @mailgun = Mailgun()
+    @mailgun.list_members(params[:list]).add(params[:email])
+    render json: {}
   end
-  
-  private
-
-    def url
-      url ||= "https://api:#{Rails.application.secrets.mailgun[:api_key]}@api.mailgun.net/v2/"
-    end
 
 end
