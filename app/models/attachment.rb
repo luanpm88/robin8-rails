8271833a3587e4b2e4ef6582b0140e2ef127146d
entwood <yaroslav.zaletskyy@gmail.com>
2015-03-04 19:02:27 +0200

    added amazon callbacks for newsroom media uploads

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,4 +2,12 @@ class Attachment < ActiveRecord::Base
   VALID_TYPES = ['image', 'file', 'video']
   belongs_to :imageable, polymorphic: true
   validates_inclusion_of :attachment_type, in: VALID_TYPES
+
+  after_create do |attachment|
+    AmazonStorageWorker.perform_async("attachment", attachment.id, attachment.url, nil, :url)
+  end
+
+  after_destroy do |attachment|
+    AmazonDeleteWorker.perform_in(20.seconds, attachment.url)
+  end
 end
