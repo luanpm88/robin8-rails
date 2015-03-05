9a3f4a6930cbc51b36047f08e8f4f8dda3dc88f3
Hamed Ramezanian <hamed.r.nik@gmail.com>
2015-03-05 15:33:07 +0000

    Add summaries, author full name and title to author's contact form

--- app/serializers/release_serializer.rb
+++ app/serializers/release_serializer.rb
@@ -2,10 +2,20 @@ class ReleaseSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :news_room_id, 
     :news_room, :title, :text, 
     :is_private, :logo_url, :created_at,
-    :concepts, :iptc_categories
+    :concepts, :iptc_categories, :summaries, :plain_title, :plain_text
 
   has_many :attachments
   has_one :news_room
+  
+  def plain_title
+    coder = HTMLEntities.new
+    coder.decode ActionController::Base.helpers.strip_tags(object.title)
+  end
+  
+  def plain_text
+    coder = HTMLEntities.new
+    coder.decode ActionController::Base.helpers.strip_tags(object.text)
+  end
   
   def concepts
     unless object.concepts.blank?
@@ -18,6 +28,14 @@ class ReleaseSerializer < ActiveModel::Serializer
   def iptc_categories
     unless object.iptc_categories.blank?
       JSON.parse(object.iptc_categories)
+    else
+      []
+    end
+  end
+  
+  def summaries
+    unless object.summaries.blank?
+      JSON.parse(object.summaries)
     else
       []
     end
