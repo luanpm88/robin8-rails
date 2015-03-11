5945552cb32c90bba579771936951fdb17718472
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-03-11 15:34:10 +0200

    fixed fonts

--- app/assets/javascripts/public_pages.js
+++ app/assets/javascripts/public_pages.js
@@ -38,12 +38,6 @@
 //= require ./lib/formValidation/formValidation.min
 //= require ./lib/formValidation/js/bootstrap.min
 
-//= require_tree ./backbone/config
-//= require backbone/init
-//= require_tree ./backbone/models
-//= require_tree ./backbone/collections
-//= require_tree ./backbone/layouts
-//= require_tree ./backbone/modules
 
 var ready;
 ready = function() {
@@ -57,35 +51,7 @@ ready = function() {
     },
     delay: 10000,
   });
-  //end default options for Bootstrap Growl
 
-  // // load the SDK Asynchronously
-  // window.fbAsyncInit = function() {
-  //   FB.init({
-  //     appId      : window.fb_api_key, //need change id
-  //     xfbml      : true,
-  //     version    : 'v2.2'
-  //   });
-  // };
-
-  // (function(d, s, id){
-  //    var js, fjs = d.getElementsByTagName(s)[0];
-  //    if (d.getElementById(id)) {return;}
-  //    js = d.createElement(s); js.id = id;
-  //    js.src = "//connect.facebook.net/en_US/sdk.js";
-  //    fjs.parentNode.insertBefore(js, fjs);
-  //  }(document, 'script', 'facebook-jssdk'));
-  // // end load the SDK Asynchronously
-
-  // // load Google+ sdk
-  // (function() {
-  //   var po = document.createElement('script');
-  //   po.type = 'text/javascript'; po.async = true;
-  //   po.src = 'https://plus.google.com/js/client:plusone.js';
-  //   var s = document.getElementsByTagName('script')[0];
-  //   s.parentNode.insertBefore(po, s);
-  // })()
-  // // end load Google+ sdk
   $('html').click(function(e) {
     if ($(e.target).closest('form').length == 0) {
       $('.navbar-search-lg').hide();
@@ -93,13 +59,6 @@ ready = function() {
       $('.progressjs-progress').hide();
     };
   });
-
-  // Navigation
-  // $(document).on("click", '#sidebar li a', function(e) {
-    // $('#sidebar li.active').removeClass('active');
-    // $(this).parent().addClass('active');
-  // });
-  // End Navigation
 
   //trimming space from both side of the string
   String.prototype.normalizeRSpace = function(len) {
