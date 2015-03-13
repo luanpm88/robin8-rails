8ca52371214cc8552265b36051fc21b2a98e86a7
entwood <yaroslav.zaletskyy@gmail.com>
2015-03-13 12:51:59 +0200

    fixed thumbnails for releases

--- app/assets/javascripts/backbone/modules/releases/views/item-view.js
+++ app/assets/javascripts/backbone/modules/releases/views/item-view.js
@@ -5,11 +5,35 @@ Robin.module('Releases', function(Releases, App, Backbone, Marionette, $, _){
     events: {
       'click #open_edit': 'openEditModal'
     },
+
+    initialize: function(){
+      var attachments = this.model.attributes.attachments;
+      if (this.model.attributes.logo_url) {
+        first_src = this.model.attributes.logo_url;
+      } else {
+        if(attachments.length > 0) {
+          var t = true 
+          _.every(attachments, function(attachment){
+            if(attachment.attachment_type == 'image') {
+              first_src = attachment.url;
+              t = false;
+            }
+            return t;
+          }); 
+        } else {
+          first_src="http://placehold.it/542x542";
+        }
+      }
+      this.model.attributes.first_src = first_src; 
+    },
+
     onShow: function(){
       $(".release-title").dotdotdot();
     },
+
     openEditModal: function(){
       Robin.vent.trigger("release:open_edit_modal", this.model);
     }
+
   });
 });
