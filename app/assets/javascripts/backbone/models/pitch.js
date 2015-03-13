10d5f65fc3245b47ea8892c128fc5f722585a331
Hamed Ramezanian <hamed.r.nik@gmail.com>
2015-03-13 17:58:08 +0000

    Change 'Name' to 'Handle' in twitter pitch template

--- app/assets/javascripts/backbone/models/pitch.js
+++ app/assets/javascripts/backbone/models/pitch.js
@@ -15,7 +15,7 @@ Robin.Models.Pitch = Backbone.RelationalModel.extend({
   urlRoot: '/pitches',
   defaults: {
     email_pitch: "Hi @[First Name],\n\nHere's a press release you might find interesting:\n\n\"@[Title]\"\n\n@[Summary]\n\nPlease let me know your thoughts.\n\nRegards@[UserFirstName]",
-    twitter_pitch: "Hey @[Name] here's a press release you might find interesting: @[Link]",
+    twitter_pitch: "Hey @[Handle] here's a press release you might find interesting: @[Link]",
     summary_length: 5,
     email_address: null,
     email_subject: null
