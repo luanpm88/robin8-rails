47bbd2eb16886a1c83e7ac41dab2878e7a24a167
kbokhonko <kolya.bokhonko@gmail.com>
2015-03-24 17:29:29 +0200

    correct redirecting for invoted users2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:add_ons]
 
   def home
-    if signed_in? && current_user.subscriptions.length > 0
+    if signed_in? && !current_user.active_subscription.blank?
       render "home", :layout => 'application'
     elsif signed_in?
       redirect_to pricing_path
