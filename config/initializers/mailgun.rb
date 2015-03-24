8ba5e6d747c850224932f146fea85e0f1f3d08e5
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-03-24 17:47:08 +0200

    170: fixed

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,4 +1,4 @@
-Mailgun.configure do |config|
-  config.api_key = Rails.application.secrets.mailgun[:api_key]
-  config.domain  = Rails.application.secrets.mailgun[:api_base_url]
-end
+# Mailgun.configure do |config|
+#   config.api_key = Rails.application.secrets.mailgun[:api_key]
+#   config.domain  = Rails.application.secrets.mailgun[:api_base_url]
+# end
