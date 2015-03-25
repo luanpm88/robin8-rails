6194b94ed46f019b00610102311ff4506c549109
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-03-25 14:30:15 +0200

    174: fixed

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -10,13 +10,7 @@ class FollowersController < ApplicationController
     @news_room = NewsRoom.find_by(subdomain_name: request.subdomain)
     @follower = @news_room.followers.build follower_params
     if @follower.save
-      # @mailgun = Mailgun()
-      # begin
-      #   @mailgun.list_members(params[:list]).add(params[:email], vars: { newsroom_id: params[:newsroom_id] }.to_json)
-      # rescue Exception
-      #   @mailgun.list_members(params[:list]).update(params[:email], vars: { newsroom_id: params[:newsroom_id] }.to_json)
-      # end
-      redirect_to follow_news_rooms_path, notice: "You've successfully subscribed."
+      redirect_to  new_public_news_room_follower_path, notice: "You've successfully subscribed."
     else
       render :new
     end
