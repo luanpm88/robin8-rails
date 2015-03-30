d6207bc49ed98f70fc84b0f5c88360ee2dc43ccb
kbokhonko <kolya.bokhonko@gmail.com>
2015-03-30 17:31:50 +0300

    refactor new stream from dashboard

--- app/assets/javascripts/backbone/modules/monitoring/show/streams-collection-view.js
+++ app/assets/javascripts/backbone/modules/monitoring/show/streams-collection-view.js
@@ -8,28 +8,24 @@ Robin.module('Monitoring.Show', function(Show, App, Backbone, Marionette, $, _){
     collection: new Robin.Collections.Streams(),
 
     initialize: function() {
-      this.collection.fetch({
-        success: this.addStream(),
+      var view = this;
+      var collection = this.collection;
+      collection.fetch({
+        success: function(model, response){
+          if (Robin.newStreamFromDashboard) {
+            var model = new Robin.Models.Stream();
+            var stream = new Robin.Models.Stream({model: model});
+            collection.push(stream);
+            view.$el.scrollLeft(view.$el.scrollWidth);
+            setTimeout(function(){ view.$el.scrollLeft(view.el.scrollWidth); }, 500);
+            Robin.newStreamFromDashboard = false
+          }
+        }
       });
     },
 
     onRender: function() {
-      var view = this;
-      if (Robin.newStreamFromDashboard) {
-        setTimeout(function(){ view.$el.scrollLeft(view.el.scrollWidth); }, 500);
-        Robin.newStreamFromDashboard = false
-      }
     },
-
-    addStream: function() {
-      if (Robin.newStreamFromDashboard) {
-        var model = new Robin.Models.Stream();
-        var stream = new Robin.Models.Stream({model: model});
-        stream.save();
-        this.collection.push(stream);
-        this.$el.scrollLeft(this.$el.scrollWidth);
-      }
-    }
 
   });
 
