eeec6ef7fd8cabfa0bbf70cddfde10b2f73e8d8e
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-04-09 16:31:26 +0300

    fixed default newsroom

--- app/models/user.rb
+++ app/models/user.rb
@@ -211,10 +211,11 @@ class User < ActiveRecord::Base
 
   private
   def create_default_news_room
-    self.news_rooms.create(
+    news_room = self.news_rooms.new(
         company_name: "#{self.email}'s Default Newsroom",
         subdomain_name: self.email_to_slug,
         default_news_room: true
     )
+    news_room.save(validate: false)
   end
 end
