408173144ac0beee384b762fff995858744480da
Razee Khan <razee@engintechnologies.com>
2015-04-09 22:53:08 +0500

    fix for after create newsroom

--- app/models/user.rb
+++ app/models/user.rb
@@ -211,7 +211,7 @@ class User < ActiveRecord::Base
 
   private
   def create_default_news_room
-    if user.is_primary
+    if is_primary?
       news_room = self.news_rooms.new(
           company_name: "#{self.email}'s Default Newsroom",
           subdomain_name: self.email_to_slug,
