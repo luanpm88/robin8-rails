516ee4ee73c76d42864371d9d10a2849051b7e9e
kbokhonko <kolya.bokhonko@gmail.com>
2015-04-15 16:46:08 +0300

    fix dates for payments at billing page

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -1,9 +1,13 @@
 class PaymentSerializer < ActiveModel::Serializer
   attributes :id, :user_product_id, :product_id, :card_last_four_digits, :card_type, :coupon, :amount, :status, :last_charge_result,
-             :created_at, :updated_at, :bluesnap_charge_id, :formatted_created_at
+             :created_at, :updated_at, :bluesnap_charge_id, :formatted_created_at, :another_format_created_at
 
   def formatted_created_at
     object.created_at.strftime("%d/%m/%Y %H:%M")
+  end
+
+  def another_format_created_at
+    object.created_at.strftime("%m/%d/%Y %H:%M")
   end
 
 end
