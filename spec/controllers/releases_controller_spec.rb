d8dffee04d4d6da359d5a2d6132095e3f6a246aa
Daniel Niiaziiev <yaroslav.zaletskyy@gmail.com>
2015-05-06 13:58:58 +0300

    test

--- spec/controllers/releases_controller_spec.rb
+++ spec/controllers/releases_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe ReleasesController, type: :controller do
+describe ReleasesController do
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates the release' do
+        # post :create, release: attributes_for(:release), format: :json
+        # post :create, release: attributes_for(:release), format: :json
+        
+        # Release.create(attributes_for(:release))
+        # expect(Release.count).to eq(1)
+      end
+    end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'with invalid attributes' do
+      it 'does not create the release' do
+        post :create, release: attributes_for(:release, title: nil), format: :json
+        expect(Release.count).to eq(0)
+      end
     end
   end
-
 end
