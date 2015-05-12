532d63850cafbd45f9d117871d493743c9d5f8cd
Daniel Niiaziiev <yaroslav.zaletskyy@gmail.com>
2015-05-12 20:19:45 +0300

    fix release model tests description

--- spec/models/releases_spec.rb
+++ spec/models/releases_spec.rb
@@ -14,19 +14,19 @@ describe Release do
       expect(release).to be_valid
     end
 
-    it "can't create release with invalid title" do
+    it "require valid title" do
       release = FactoryGirl.build(:release, title: nil)
       release.user = user
       expect(release).not_to be_valid
     end
 
-    it "can't create release with invalid user id" do
+    it "require valid user id" do
       release = FactoryGirl.build(:release, title: nil)
       release.user = nil
       expect(release).not_to be_valid
     end
 
-    it "can't create release when user cannot create release" do
+    it "require user can create release" do
       allow(user).to receive(:can_create_release).and_return(false)
       release = FactoryGirl.build(:release, title: 'Test title')
       release.user = user
