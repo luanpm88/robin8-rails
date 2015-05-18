638bb0fda882e6ed5512d4fa49511c2e9badec57
entwood <yaroslav.zaletskyy@gmail.com>
2015-05-18 10:50:06 +0300

    AYL-534: ActiveAdmin - enabled creation of products, pitches and streams

--- app/admin/pitch.rb
+++ app/admin/pitch.rb
@@ -1,18 +1,6 @@
 ActiveAdmin.register Pitch do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  permit_params :user_id , :release_id, :twitter_pitch, :email_pitch, :summary_length,
+                :email_address, :email_subject, :email_targets, :twitter_targets
 
 end
--- app/admin/stream.rb
+++ app/admin/stream.rb
@@ -1,18 +1,19 @@
 ActiveAdmin.register Stream do
 
+  permit_params :user_id , :name, :topics_raw, :blogs_raw, :sort_column, :position, :published_at, :keywords_raw, :keywords
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  form do |f|
+    f.inputs "Stream" do
+      f.input :user
+      f.input :name
+      f.input :topics_raw, :as => :text
+      f.input :blogs_raw, :as => :text
+      f.input :sort_column, :as => :select, :collection => ["published_at", "shares_count"]
+      f.input :position
+      f.input :published_at
+      f.input :keywords_raw, :as => :text
+    end
+    f.actions
+  end
 
 end
