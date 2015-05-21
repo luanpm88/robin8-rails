f1bfa95e1f203f2ee3583225fc8c6f958660dd17
Daniel Niiaziiev <yaroslav.zaletskyy@gmail.com>
2015-05-21 16:45:07 +0300

    img url check fix

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -101,7 +101,7 @@ class ReleasesController < ApplicationController
   def release_params
     params.require(:release).permit(:title, :text, :news_room_id, :is_private, 
       :logo_url, :thumbnail, :concepts, :iptc_categories, :summaries, :hashtags,
-      :characters_count, :words_count, :sentences_count, :url,
+      :characters_count, :words_count, :sentences_count,
       :paragraphs_count, :adverbs_count, :adjectives_count,
       :nouns_count, :organizations_count, :places_count, :people_count,
       attachments_attributes: [:id, :url, :attachment_type, :name, :thumbnail, :_destroy])
