6450033500fab1c939988f7451db0b00de2ea93e
Daniel Niiaziiev <yaroslav.zaletskyy@gmail.com>
2015-05-22 16:06:17 +0300

    posts model tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post do
+  let!(:user) { stub_model(User, email: 'test@test.com', id: 1) }
+
+  describe "validations of new post" do
+
+    it "create post with valid text" do
+      post = build(:post, text: 'Test text', user: user)
+      expect(post).to be_valid
+    end
+
+    it "require valid text" do
+      post = build(:post, text: '', user: user)
+      expect(post).not_to be_valid
+    end
+
+    it "require valid scheduled_date" do
+      post = build(:post, scheduled_date: '', user: user)
+      expect(post).not_to be_valid
+    end
+  end
 end
