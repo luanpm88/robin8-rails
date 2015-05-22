3bbf1d88495ca3c53b6f48e624b27f689a5be40f
Daniel Niiaziiev <yaroslav.zaletskyy@gmail.com>
2015-05-22 18:16:13 +0300

    posts model scopes tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Post do
   let!(:user) { stub_model(User, email: 'test@test.com', id: 1) }
+  
+  before do
+    allow_any_instance_of(Post).to receive(:perform_worker)
+  end
 
   describe "validations of new post" do
 
@@ -18,6 +22,24 @@ describe Post do
     it "require valid scheduled_date" do
       post = build(:post, scheduled_date: '', user: user)
       expect(post).not_to be_valid
+    end
+  end
+
+  describe "Posts" do
+
+    it "should have :todays post" do
+      create(:post, scheduled_date: DateTime.now + 4.hour)
+      expect(Post.todays).not_to be_empty
+    end
+
+    it "should have :tomorrows post" do
+      create(:post, scheduled_date: Date.tomorrow + 1.hour)
+      expect(Post.tomorrows).not_to be_empty
+    end
+
+    it "should have :others post" do
+      create(:post, scheduled_date: Date.tomorrow + 1.hour + 1.day)
+      expect(Post.others).not_to be_empty
     end
   end
 end
