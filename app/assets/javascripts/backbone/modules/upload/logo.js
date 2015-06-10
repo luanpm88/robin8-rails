298bc4e925868f646f6d53b9b67c8b5b7cf2b497
entwood <yaroslav.zaletskyy@gmail.com>
2015-06-10 11:04:10 +0300

    AYL-582: removed crop requrements from newsroom logo

--- app/assets/javascripts/backbone/modules/upload/logo.js
+++ app/assets/javascripts/backbone/modules/upload/logo.js
@@ -5,31 +5,33 @@ Robin.Views.LogoView = Backbone.Marionette.ItemView.extend({
   },
   onShow: function() {
     var viewObj = this;
-    ///set obligatory crop if news room
-    if (this.model.urlRoot == "/news_rooms") {
-      $("input").attr("data-crop", "340x340 upscale");
-    }
-
     this.widget = uploadcare.Widget('[role=uploadcare-uploader]').onUploadComplete(function(info){
       //create release thumbnail
-      if (viewObj.model.urlRoot == "/releases") {
+      if (viewObj.model.urlRoot == '/releases') {
         var thumbSrc = info.cdnUrl + '-/preview/340x340/-/stretch/off/-/resize/340x340/';
         $("#logo-image").attr('src', thumbSrc);
         viewObj.model.set('thumbnail', thumbSrc);
       } else {
         $("#logo-image").attr('src', info.cdnUrl);
       }
-      $('#image-uploader-logo').nailthumb({width:200,height:200,method:'resize',fitDirection:'center'});
-      viewObj.model.set(viewObj.options.field, info.cdnUrl);
+      //set model logo
+      if (viewObj.model.urlRoot == '/news_rooms') {
+         var imgSrc = info.cdnUrl + '-/preview/340x340/';
+         viewObj.model.set(viewObj.options.field, imgSrc);
+       } else {
+         viewObj.model.set(viewObj.options.field, info.cdnUrl);
+       }
+       //nailthumb
+       $('#image-uploader-logo').nailthumb({width:200,height:200,method:'resize',fitDirection:'center'});
     });
 
     if (this.model.get(this.options.field)){
-      if (this.model.urlRoot == "/releases") {
+      if (viewObj.model.urlRoot == '/releases') {
         var logoSrc = this.model.get('thumbnail');
       } else {
         var logoSrc = this.model.get(this.options.field);
       }
-      $("#logo-image").attr('src', logoSrc);
+      $('#logo-image').attr('src', logoSrc);
       $('#image-uploader-logo').nailthumb({width:200,height:200,method:'resize',fitDirection:'center'});
     }
 
