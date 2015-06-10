1d40305163ea479a50c5f4ca37ae560a00412c58
Pavlo Shabat <pavlo.shabat@gmail.com>
2015-06-10 17:53:32 +0300

    enabled filtering

--- app/assets/javascripts/backbone/modules/analytics/views/analytics-layout.js
+++ app/assets/javascripts/backbone/modules/analytics/views/analytics-layout.js
@@ -27,12 +27,25 @@ Robin.module('Analytics', function(Analytics, App, Backbone, Marionette, $, _){
     },
 
     changeEmailsNewsRoom: function(event) {
+      var $this = this;
       var emailsAnalyticsPageView = new Analytics.EmailsAnalyticsPage({
         collection: new Robin.Collections.NewsRooms()
       });
       
-      this.emailsAnalyticsRegion.show(emailsAnalyticsPageView);
+      $this.emailsAnalyticsRegion.show(emailsAnalyticsPageView);
       emailsAnalyticsPageView.renderEmailAnalytics($(event.target).val());
+
+      var collectionEmails = new Robin.Collections.EmailAnalytics()
+      collectionEmails.fetch({
+        url: '/news_rooms/' + $(event.target).val() +'/email_analytics',
+        success: function(collection, data, response){
+          var collection = new Robin.Collections.EmailAnalytics(data.authors);
+          var emailListView = new Analytics.EmailsListCompositeView({
+            collection: collection
+          });
+          $this.emailsListRegion.show(emailListView);
+        }
+      })
     },
 
     navigateToEmails: function(){
