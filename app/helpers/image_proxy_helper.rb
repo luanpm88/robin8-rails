7cd6272c508bd27de14307813d0ccba62236b1b2
Hamed Ramezanian <hamed.r.nik@gmail.com>
2015-06-11 17:50:37 +0100

    Change route of image proxy

--- app/helpers/image_proxy_helper.rb
+++ app/helpers/image_proxy_helper.rb
@@ -2,7 +2,7 @@ require 'base64'
 
 module ImageProxyHelper
   def proxy_image_tag(images, options = nil)
-    options[:src] = image_proxy_path(Base64.encode64(images))
+    options[:src] = image_proxy_path(images: Base64.encode64(images))
     tag("img", options)
   end
 end
