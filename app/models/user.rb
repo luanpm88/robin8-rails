502be67837b940fa044c2016c149011311f178b3
johnmcauley <john@wripl.com>
2015-06-30 15:50:48 +0100

    Remove all identitiies from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -241,48 +241,6 @@ class User < ActiveRecord::Base
     user_products.joins(:product).where("products.type ='AddOn' and (products.interval is NOT NULL OR products.interval >= '30')")
   end
 
-  def twitter_identity
-    identities.where(provider: 'twitter').first
-  end
-
-  def linkedin_identity
-    identities.where(provider: 'linkedin').first
-  end
-
-  def facebook_identity
-    identities.where(provider: 'facebook').first
-  end
-
-  def google_identity
-    identities.where(provider: 'google_oauth2').first
-  end
-
-  def twitter_identities
-    identities.where(provider: 'twitter')
-  end
-
-  def linkedin_identities
-    identities.where(provider: 'linkedin')
-  end
-
-  def facebook_identities
-    identities.where(provider: 'facebook')
-  end
-
-  def google_identities
-    identities.where(provider: 'google_oauth2')
-  end
-
-
-  def all_identities
-    identities_by_providers = {}
-    identities_by_providers[:twitter] = twitter_identities
-    identities_by_providers[:facebook] = facebook_identities
-    identities_by_providers[:google] = google_identities
-    identities_by_providers[:linkedin] = linkedin_identities
-    identities_by_providers 
-  end
-
   def twitter_post(message, identity_id=nil)
     identity = identity_id.nil? ? twitter_identity : Identity.find(identity_id)
     
