0d2798b264d12bec25fb14374b45a683c4bbddf6
Olga Kagitina <askai@redwerk.com>
2015-07-01 13:12:52 +0300

    Kol login payment button at profile adding Robin8 Inc

--- app/assets/javascripts/backbone/modules/authentication/signin/privacy.js
+++ app/assets/javascripts/backbone/modules/authentication/signin/privacy.js
@@ -4,7 +4,7 @@ Robin.module('Authentication.SignIn', function(SignIn, App, Backbone, Marionette
     template: 'modules/authentication/signin/templates/privacy',
 
     onRender: function() {
-      $('.signup-tag').text('MYPRGENIE TERMS OF USE, PRIVACY POLICY, AND DISTRIBUTION POLICIES');
+      $('.signup-tag').text('ROBIN8 INC TERMS OF USE, PRIVACY POLICY, AND DISTRIBUTION POLICIES');
     },
     
   });
