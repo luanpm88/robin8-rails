56ae9a93472d3fc8b3f75f48020a2bfa5e10579f
Abdulin Aleksei <freeloader@redwerk.com>
2015-08-14 23:13:39 +0300

    ROBIN8-130: changed scope for Facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,12 +256,12 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :twitter, Rails.application.secrets.twitter[:api_key], 
+  config.omniauth :twitter, Rails.application.secrets.twitter[:api_key],
     Rails.application.secrets.twitter[:api_secret]
   config.omniauth :linkedin, Rails.application.secrets.linkedin[:api_key],
     Rails.application.secrets.linkedin[:api_secret], :scope => 'r_basicprofile r_emailaddress rw_company_admin w_share'
   config.omniauth :facebook, Rails.application.secrets.facebook[:app_id],
-    Rails.application.secrets.facebook[:app_secret]
+    Rails.application.secrets.facebook[:app_secret], :scope => 'email,publish_actions'
   config.omniauth :google_oauth2, Rails.application.secrets.google[:client_id],
     Rails.application.secrets.google[:client_secret]
 end
