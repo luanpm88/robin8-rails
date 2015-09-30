dd90ac84080e976629e178b3748136b585af0d61
Anton Baklanov <bak1an@redwerk.com>
2015-09-30 13:13:30 +0300

    don't crash on monetize save

--- app/assets/javascripts/backbone/models/kol_profile.js
+++ app/assets/javascripts/backbone/models/kol_profile.js
@@ -9,21 +9,11 @@ Robin.Models.KolProfile = Backbone.Model.extend({
   },
 
   monetize: function(data, options){
-    var model = this,
-      url = '/kols/monetize',
-      options = {
-        data: data,
-        url: url,
-        type: 'POST'
-      };
 
-    var success = options.success;
-    options.success = function(resp) {
-      if (!model.set(model.parse(resp, options), options)) return false;
-      if (success) success(model, resp, options);
-      model.trigger('sync', model, resp, options);
-    };
+    this.save(data, _.extend(options, {
+      url: '/kols/monetize',
+      type: 'POST',
+    }));
 
-    return this.sync('read', this, options);
   },
 });
