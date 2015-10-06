8ddc839b4a33b36857e33d0dde014e3b8a89e6ce
Olga Kagitina <askai@redwerk.com>
2015-10-06 17:35:26 +0300

    add chins_instance variable

--- app/assets/javascripts/backbone/controllers/controller.js
+++ app/assets/javascripts/backbone/controllers/controller.js
@@ -1,7 +1,7 @@
 Robin.Controllers.AppController = Marionette.Controller.extend({
   initialize: function (options) {
     this.stopAll();
-     locale = !Robin.KOL ? Robin.currentUser.get('locale') : "";
+    locale = !Robin.KOL ? Robin.currentUser.get('locale') : "";
     Robin.module('Navigation').start();
     if (!Robin.KOL) {
       Robin.module('SaySomething').start();
