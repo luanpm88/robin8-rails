a66d3ea7a20e12fe1c782e58d5f05e52bbea376e
AndyHu <meeasyhappy@gmail.com>
2015-10-15 15:23:17 +0800

    roll back code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,11 @@ class ApplicationController < ActionController::Base
 
   before_action :set_translations
   before_action :china_redirect
+  helper_method :china_instance?
 
   def china_redirect
     if Rails.env.production? and china_client? and not china_instance?
-      # return redirect_to "http://robin8.cn#{request.fullpath}", :status => :moved_permanently
+      return redirect_to "http://robin8.cn#{request.fullpath}", :status => :moved_permanently
     end
   end
 
