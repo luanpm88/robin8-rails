db4ea2b8843483ba3ec9d8d4df3119d7526ba704
luotao <luotao0124@gmail.com>
2015-10-21 00:49:50 +0800

    add spec

--- spec/workers/integration_with_data_engine_worker_spec.rb
+++ spec/workers/integration_with_data_engine_worker_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe IntegrationWithDataEngineWorker do
   it { is_expected.to be_retryable 3 }
+
+  it 'logger the job when retries exhausted' do
+    IntegrationWithDataEngineWorker.within_sidekiq_retries_exhausted_block {
+      expect(Rails.logger).to receive(:warn)
+    }
+  end
 end
