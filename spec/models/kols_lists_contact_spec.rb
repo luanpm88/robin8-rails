e58c1e7f8d82b40aacb1d9330cdb736d95dfbf3b
luotao <luotao0124@gmail.com>
2015-10-20 09:46:47 +0800

    typo fix

--- spec/models/kols_lists_contact_spec.rb
+++ spec/models/kols_lists_contact_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe KolsListsContacts, type: :model do
+RSpec.describe KolsListsContact, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
