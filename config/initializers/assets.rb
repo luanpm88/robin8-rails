ec31c586a5ffde8c18640239cd6013eafab36168
huxl <huxl@huxldeMacBook-Pro.local>
2015-10-30 18:53:56 +0800

    adjust some asset file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,4 +16,4 @@ end
 Rails.application.config.assets.precompile += %w( landing.js landing.css
   email.css mail-template.css export_stories.css kol.css )
 Rails.application.config.assets.precompile += %w( public_pages.js
-  public_pages.css  website.css website.js export_stories.js kol.js kol/*.js kol/.scss)
+  public_pages.css  website.css website.js export_stories.js kol.js kol/*.js kol/*.css)
--- config/initializers/build_dev_js.rb
+++ config/initializers/build_dev_js.rb
@@ -1,40 +1,55 @@
 #TODO : 扩展到其他文件
 
-KolPath = "#{Rails.root}/app/assets/javascripts/kol"
+KolJsPath = "#{Rails.root}/app/assets/javascripts/kol"
+KolCssPath = "#{Rails.root}/app/assets/stylesheets/kol"
 PublicPath = "#{Rails.root}/public/assets"
-JsAsset = "dev_base"
-JsAssetCompact = "dev_base_compact"
+Asset = "dev_base"
+AssetCompact = "dev_base_compact"
 
-def generate_kol_dev_base
-  kol_line_arr = File.read("#{KolPath}/application.js").split("\n")
-  kol_reload_arr = File.read("#{KolPath}/dev_reload.js").split("\n")
+def generate_kol_js_dev_base
+  kol_line_arr = File.read("#{KolJsPath}/application.js").split("\n")
+  kol_reload_arr = File.read("#{KolJsPath}/dev_reload.js").split("\n")
   kol_other_content = kol_line_arr.map do |line|
-    unless kol_reload_arr.include?(line)
+    unless kol_reload_arr.include?(line) && line.start_with?("//=")
       line
     end
   end.compact.join("\n")
-  File.open("#{KolPath}/#{JsAsset}.js","w"){|f| f.write(kol_other_content)}
+  File.open("#{KolJsPath}/#{Asset}.js","w"){|f| f.write(kol_other_content)}
+end
+
+def generate_kol_css_dev_base
+  kol_line_arr = File.read("#{KolCssPath}/application.scss").split("\n")
+  kol_reload_arr = File.read("#{KolCssPath}/dev_reload.scss").split("\n")
+  kol_other_content = kol_line_arr.map do |line|
+    unless kol_reload_arr.include?(line) && line.start_with?("*=")
+      line
+    end
+  end.compact.join("\n")
+  File.open("#{KolCssPath}/#{Asset}.scss","w"){|f| f.write(kol_other_content)}
 end
 
 def compile_dev_file
   puts "--------start ---compile"
-  system("rm #{PublicPath}/kol/#{JsAsset}**.js")
+  system("rm #{PublicPath}/kol/#{Asset}**.js")
+  system("rm #{PublicPath}/kol/#{Asset}**.css")
   Rake::Task['assets:clean'].invoke
   system("RAILS_ENV=development rake assets:precompile")
   # Rake::Task['assets:precompile'].invoke
-  system("cp #{PublicPath}/kol/#{JsAsset}**.js #{PublicPath}/#{JsAssetCompact}.js")
+  system("cp #{PublicPath}/kol/#{Asset}**.js #{PublicPath}/#{AssetCompact}.js")
+  system("cp #{PublicPath}/kol/#{Asset}**.css #{PublicPath}/#{AssetCompact}.css")
   puts "--------end ---compile"
 end
 
 def monitor_reload_file
-  listener = Listen.to("#{KolPath}") do |modified, added, removed|
-    next if !modified.include?("#{KolPath}/dev_reload.js")
+  listener = Listen.to(KolJsPath,KolCssPath) do |modified, added, removed|
+    next if !modified[0].include?("dev_reload")
     puts "recompile generate dev base then compile it"
-    generate_kol_dev_base
+    generate_kol_js_dev_base
+    generate_kol_css_dev_base
     compile_dev_file
   end
   listener.start
-  puts "start monitor dev_reload.js"
+  puts "start monitor dev_reload"
 end
 
 if Rails.env == "development" and Robin8::Application.config.china_instance  && $0.include?("rails")  ## 避免 rake 启动
@@ -42,6 +57,7 @@ if Rails.env == "development" and Robin8::Application.config.china_instance  && 
   require 'rake'
   Robin8::Application.load_tasks
 
-  generate_kol_dev_base
+  generate_kol_js_dev_base
+  generate_kol_css_dev_base
   monitor_reload_file
 end
