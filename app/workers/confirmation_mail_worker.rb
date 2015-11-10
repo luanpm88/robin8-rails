5f218c946794d71ff2377fdea99ee6a3f436713c
luotao <luotao0124@gmail.com>
2015-11-10 17:24:53 +0800

    us instance send mail use mailgun

--- app/workers/confirmation_mail_worker.rb
+++ app/workers/confirmation_mail_worker.rb
@@ -3,20 +3,37 @@ class ConfirmationMailWorker
 
   def perform data
     Rails.logger.sidekiq.info "Started perform #{self.class.to_s} with args: #{data['to']}"
-    send_data = {
-      :from => 'noreply@robin8.net',
-      :fromname => 'Robin8',
-      :subject => '注册验证',
-      :use_maillist => false
-    }.merge data
 
-    sendcloud_client = Sendcloud::Client.new
+    if Rails.application.config.china_instance
+      # use sendcloud for china instance
+      send_data = {
+        :from => Rails.application.secrets.sendcloud[:from_address],
+        :fromname => Rails.application.secrets.sendcloud[:form_name],
+        :subject => I18n.t('devise.mailer.confirmation_instructions.subject'),
+        :use_maillist => false
+      }.merge data
 
-    begin
-      sendcloud_client.send_message(send_data)
-    rescue => e
-      Rails.logger.sidekiq.error "Perform #{self.class.to_s} failed with args: #{data['to']}, error msg: #{e.message}"
-      return
+      sendcloud_client = Sendcloud::Client.new
+      begin
+        sendcloud_client.send_message(send_data)
+      rescue => e
+        Rails.logger.sidekiq.error "Perform #{self.class.to_s} failed with args: #{data['to']}, error msg: #{e.message}"
+        return
+      end
+    else
+      # use mailgun for us instance
+      send_data = {
+        :from => Rails.application.secrets.mailgun[:from],
+        :subject => I18n.t('devise.mailer.confirmation_instructions.subject')
+      }.merge data
+
+      mg_client = Mailgun::Client.new(Rails.application.secrets.mailgun[:api_key], api_host='api.mailgun.net', api_version='v3')
+      begin
+        result = mg_client.send_message('mg.wall2flower.moe', send_data)
+      rescue => e
+        Rails.logger.sidekiq.error "Perform #{self.class.to_s} failed with args: #{data['to']}, error msg: #{e.message}"
+        return
+      end
     end
 
     Rails.logger.sidekiq.info "Completed perform #{self.class.to_s}"
