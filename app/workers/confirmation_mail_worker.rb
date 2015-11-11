273398082a30e9afed71c1496c6f6b243d71be8b
luotao <luotao0124@gmail.com>
2015-11-11 16:52:13 +0800

    use secrets.yml to config

--- app/workers/confirmation_mail_worker.rb
+++ app/workers/confirmation_mail_worker.rb
@@ -31,8 +31,9 @@ class ConfirmationMailWorker
       }.merge(data.tap { |d| d.delete(:token) })
 
       mg_client = Mailgun::Client.new(Rails.application.secrets.mailgun[:api_key], api_host='api.mailgun.net', api_version='v3')
+      send_domain = Rails.application.secrets.mailgun[:send_domain]
       begin
-        result = mg_client.send_message('mg.wall2flower.moe', send_data)
+        result = mg_client.send_message(send_domain, send_data)
       rescue => e
         Rails.logger.sidekiq.error "Perform #{self.class.to_s} failed with args: #{data['to']}, error msg: #{e.message}"
         return
