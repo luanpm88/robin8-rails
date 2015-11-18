d56feded0812ebde9a89141ddde8c5c02b6a5695
huxl <huxl@huxldeMacBook-Pro.local>
2015-11-18 16:01:08 +0800

    try to fix get component verify ticket

--- app/controllers/wechat_third_controller.rb
+++ app/controllers/wechat_third_controller.rb
@@ -8,29 +8,17 @@ class WechatThirdController < ApplicationController
   # 即公众号服务的AppId。
   # 这种系统事件推送通知（现在包括推送component_verify_ticket协议和推送取消授权通知），
   # 服务开发者收到后也需进行解密，接收到后只需直接返回字符串“success”
-  # Started POST "/wechat_third/notify?signature=fe42caa8d6fe8545952c1b6a3a927362ecf59f56&timestamp=1447771262&nonce=301792237&encrypt_type=aes&msg_signature=0dd7b2702411636af320e3b866629141cbcfbd57"
   def notify
-    Rails.logger.error("------notify---request.env #{request.env.to_yaml}- ---")       rescue nil
-    Rails.logger.error("-----request body --- #{request.env.rbuf}")                rescue nil
-    Rails.logger.error("-----request body --- #{request.env['rbuf']}")                rescue nil
-    $webchat_logger.info("--------notify-授权事件--")
-    wxXMLParams = request.env['rbuf']['xml']                                                 rescue nil
-    nowAppId = wxXMLParams["AppId"]    rescue ""
-    # 防止 ticket窜改
-    return render :text => "success" if nowAppId != WxThird::Util::AppId
-    # 加密过的数据
-    xmlEncrpyPost = wxXMLParams["Encrypt"]
     # 解密数据
     aes_key = Base64.decode64("#{WxThird::Util::AesKey}=")
-    content = QyWechat::Prpcrypt.decrypt(aes_key, xmlEncrpyPost, WxThird::Util::AppId)[0]
+    content = QyWechat::Prpcrypt.decrypt(aes_key, @encrypt_msg, WxThird::Util::AppId)[0]
     # 解密后的数据
-    decryptMsg = MultiXml.parse(content)["xml"]
-    if decryptMsg
-      nowAppId = decryptMsg["AppId"]
+    decrypt_msg = MultiXml.parse(content)["xml"]
+    if decrypt_msg
       # ticket 事件
-      if decryptMsg["InfoType"] == "component_verify_ticket"
-        $webchat_logger.info("-------component_verify_ticket---#{decryptMsg["ComponentVerifyTicket"]}")
-        Rails.cache.write(WxThird::Util.component_verify_ticket_key(nowAppId), decryptMsg["ComponentVerifyTicket"])
+      if decrypt_msg["InfoType"] == "component_verify_ticket"
+        $webchat_logger.info("-------component_verify_ticket---#{decrypt_msg['ComponentVerifyTicket']}")
+        Rails.cache.write(WxThird::Util.component_verify_ticket_key(WxThird::Util::AppId), decrypt_msg["ComponentVerifyTicket"])
       end
       # 最终返回成功就行
       render :text => "success"
@@ -48,16 +36,17 @@ class WechatThirdController < ApplicationController
   # before_skip 过滤器  只针对 ticket 取消授权等事件
   def valid_msg_signature
     begin
-    Rails.logger.info("------valid_msg_signature---#{params}")
-    timestamp = params["timestamp"]
-    nonce = params["nonce"]
-    @rbuf = request['rbuf']
-    encrypt_msg = request['rbuf']["Encrypt"]   rescue ""
-    msg_signature = params["msg_signature"]
-    sort_params = [WxThird::Util::AesKey, timestamp, nonce, encrypt_msg].sort.join
-    current_signature = Digest::SHA1.hexdigest(sort_params)
-    $webchat_logger.info("--------params[xml] - #{encrypt_msg} --")
-    return true
+      timestamp = params["timestamp"]
+      nonce = params["nonce"]
+      msg_signature = params["msg_signature"]
+      xml = MultiXml.parse(request.raw_post)["xml"]   rescue {}
+      @encrypt_msg = xml["Encrypt"]
+      @request_app_id = xml["AppId"]
+      sort_params = [WxThird::Util::DescryToken, timestamp, nonce, encrypt_msg].sort.join
+      current_signature = Digest::SHA1.hexdigest(sort_params)
+      $webchat_logger.info("--------params[xml] - #{WxThird::Util::AppId == @request_app_id} --")
+      $webchat_logger.info("--------params[xml] - #{current_signature == msg_signature} --")
+      return true
     rescue
       return true
     end
