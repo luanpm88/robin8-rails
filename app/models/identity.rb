afd708bae1243edc60a96466197a668b6331b660
huxl <huxl@huxldeMacBook-Pro.local>
2015-11-19 17:40:05 +0800

    fix

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -33,8 +33,8 @@ class Identity < ActiveRecord::Base
     create!(uid: auth[:uid], provider: auth[:provider], token: auth[:token], url: auth[:url],
            token_secret: auth[:token_secret], name: auth[:name], avatar_url: auth[:avatar_url],
            desc: auth[:desc], service_type_info: auth[:service_type_info],
-           verify_type_info: auth[:verify_type_info], wx_user_name: auth[:wx_user_name]#,
-           # serial_params: origin_auth.to_json
+           verify_type_info: auth[:verify_type_info], wx_user_name: auth[:wx_user_name],
+           serial_params: origin_auth.to_json
     )
   end
 
@@ -50,11 +50,6 @@ class Identity < ActiveRecord::Base
   def last30_posts
     0
   end
-
-  # def as_json
-  #
-  # end
-
 end
 
 
