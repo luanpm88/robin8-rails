acf07d5c55b106429f32ffecaa685b48fd6838af
luotao <luotao0124@gmail.com>
2015-11-26 20:01:21 +0800

    refactor code

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -3,20 +3,13 @@ module Users
     respond_to :html, :json
 
     def create
-      self.resource = resource_class.send_reset_password_instructions(resource_params)
-
-      unless self.resource.persisted?
-        self.resource = Kol.send_reset_password_instructions resource_params
-        resource_name = :kol
-      end
-
-      yield resource if block_given?
-
-      if successfully_sent?(resource)
-        respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
-      else
-        respond_with(resource)
+      super do
+        unless self.resource.persisted?
+          self.resource = Kol.send_reset_password_instructions resource_params
+          resource_name = :kol
+        end
       end
     end
+
   end
 end
