311cdd3b947bed97422166ddf4ec9beeddad6050
luotao <luotao0124@gmail.com>
2015-11-27 19:37:33 +0800

    bugfix

--- app/assets/javascripts/backbone/models/cn_recommendation.js
+++ app/assets/javascripts/backbone/models/cn_recommendation.js
@@ -3,9 +3,9 @@ Robin.Models.CnRecommendation = Backbone.Model.extend(
   toJSON: function(){
     var attrs = this.attributes;
 
-    var recommendation = _.extend(attrs, { 
-      image_name: "/recommendations/" + attrs.label +  Math.floor((Math.random()*3)+1) +".png"
-      //timeago: ($.timeago(attrs.published_at)).replace('about',''), 
+    var recommendation = _.extend(attrs, {
+      image_name: "recommendations/" + attrs.label +  Math.floor((Math.random()*3)+1) +".png"
+      //timeago: ($.timeago(attrs.published_at)).replace('about',''),
      });
     return { recommendation: recommendation };
   }
