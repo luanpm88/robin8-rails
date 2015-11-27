baa86a494ccec947bf6112304da79c9bd706dd73
AndyHu <meeasyhappy@gmail.com>
2015-11-27 18:28:06 +0800

    添加推荐 图片

--- app/assets/javascripts/backbone/models/cn_recommendation.js
+++ app/assets/javascripts/backbone/models/cn_recommendation.js
@@ -4,6 +4,7 @@ Robin.Models.CnRecommendation = Backbone.Model.extend(
     var attrs = this.attributes;
 
     var recommendation = _.extend(attrs, { 
+      image_name: "/recommendations/" + attrs.label +  Math.floor((Math.random()*3)+1) +".png"
       //timeago: ($.timeago(attrs.published_at)).replace('about',''), 
      });
     return { recommendation: recommendation };
--- app/controllers/cn_recommendations_controller.rb
+++ app/controllers/cn_recommendations_controller.rb
@@ -3,7 +3,8 @@ class CnRecommendationsController < ApplicationController
     begin
       page = params[:page].to_i
       page = page >= 1 ? page : 1
-      url = File.join(Rails.application.secrets.cn_recommendation_url, page.to_s) + "?labels=finance,digitcamera,dish,computer"
+      
+      url = File.join(Rails.application.secrets.cn_recommendation_url, page.to_s) + "?labels=#{get_labels.join(",")}"
 
       response = RestClient::Request.execute(method: :get, url: url,
                             timeout: 10)
@@ -11,5 +12,22 @@ class CnRecommendationsController < ApplicationController
    rescue Exception => e
       render :json => []
    end
+  end
+
+  private
+  def get_labels
+    return default_labels unless current_kol
+    %w(wechat wechat-third weibo).each do |item|
+      current_kol.identities.where(:provider => item).each do |identity|
+        if identity.present? and identity.iptc_categories.present?
+          return identity.iptc_categories.map(&:name)
+        end
+      end
+    end
+    return default_labels
+  end
+
+  def default_labels
+    ["finance", "digitcamera", "dish", "computer"]
   end
 end
