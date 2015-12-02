38c681c96a0923ed156cc2cb4ec0a6eb314c0985
huxl <huxl@huxldeMacBook-Pro.local>
2015-12-02 15:09:54 +0800

    fix : sign out info also display on sign-in page

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,5 +2,12 @@ module Users
   class SessionsController < Devise::SessionsController
     respond_to :html, :json
 
+    # only hide flash info
+    def destroy
+      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      yield if block_given?
+      respond_to_on_destroy
+    end
+
   end
 end
