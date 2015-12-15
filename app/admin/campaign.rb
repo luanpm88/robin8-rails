1ecf41c73ad2a94e98fe9673c6bb9d4405779ae8
james <tardis54@163.com>
2015-12-15 16:59:57 +0800

    campaign active admin

--- app/admin/campaign.rb
+++ app/admin/campaign.rb
@@ -14,5 +14,21 @@ ActiveAdmin.register Campaign do
   #   permitted
   # end
 
-
+  index do
+    id_column
+    column :name
+    column "Advertiser" do |my_resource|
+      my_resource.user.name
+    end
+    column "Created time", :created_at
+    column "Start time", :start_time
+    column "End time", :deadline
+    column :budget
+    column "Spent" do |my_resource|
+      my_resource.get_fee_info.split('/').first
+    end
+    column "Actual Clicks",  :avail_click
+    column "CPC", :per_click_budget
+    actions
+  end
 end
