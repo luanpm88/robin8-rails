252b3c6444dc859d581fb5f89e83b589697ed60b
james <tardis54@163.com>
2015-12-17 17:57:29 +0800

    add social uid to kol

--- app/controllers/kols_controller.rb
+++ app/controllers/kols_controller.rb
@@ -54,7 +54,7 @@ class KolsController < ApplicationController
 
   def create_kol_from_social_account
     auth_params = params[:auth_params]
-    @kol = Kol.new({social_name: auth_params[:name], provider: auth_params[:provider]})
+    @kol = Kol.new({social_name: auth_params[:name], provider: auth_params[:provider], social_uid: auth_params[:uid]})
     @kol.country = 'China' if china_instance?
     @identity = @kol.identities.build(auth_params.to_hash)
     @identity.can_delete = false
