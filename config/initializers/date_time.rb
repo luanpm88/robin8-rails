a0398fd65d09e89319bebbd55e594732ba473e58
huxl <huxl@huxldeMacBook-Pro.local>
2015-12-18 17:41:32 +0800

    ROBIN8CN-206

---
+++ config/initializers/date_time.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+Time::DATE_FORMATS[:time]='%H:%M'
+Time::DATE_FORMATS[:date_time_without_year]='%m-%d %H:%M'
+Time::DATE_FORMATS[:date_time]='%Y-%m-%d %H:%M'
+Time::DATE_FORMATS[:date]='%Y-%m-%d'
+Time::DATE_FORMATS[:year_month]='%Y-%m'
+Time::DATE_FORMATS[:month_day]='%m-%d'
+Time::DATE_FORMATS[:all_time]='%Y-%m-%d %H:%M:%S'
+Time::DATE_FORMATS[:contracts]='%m.%d %H:%M'
+Time::DATE_FORMATS[:end_at]='%y-%m-%d %H:%M:%S'
+Time::DATE_FORMATS[:zh_date] = '%Y年%m月%d日'
+Time::DATE_FORMATS[:zh_day] = '%d日'
+Time::DATE_FORMATS[:unionpay] = '%Y%m%d%H%M%S'
+
