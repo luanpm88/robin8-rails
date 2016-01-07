309d5121d58751c2713fb6f277d4570d67ace832
james <tardis54@163.com>
2016-01-07 14:13:47 +0800

    send sms to kol if have avail click when campaign finished

--- lib/yun_pian/send_campaign_invite_result_sms.rb
+++ lib/yun_pian/send_campaign_invite_result_sms.rb
@@ -1,6 +1,6 @@
 module YunPian
   class SendCampaignInviteResultSms
-    def initialize(phone_number, status,
+    def initialize(phone_number, status=nil,
                    api_key = Rails.application.secrets.yunpian[:api_key])
       @phone_number = phone_number
       @status = status
@@ -15,6 +15,18 @@ module YunPian
       end
       begin
         res = ChinaSMS.to @phone_number, tpl_params, tpl_id: 1200383
+      rescue Exception => ex
+        Rails.logger.error ex
+        return nil
+      end
+    end
+
+    def send_campaign_finished_sms
+      return if @phone_number.blank?
+      ChinaSMS.use :yunpian, password: @api_key
+      tpl_params = {count: '3'}
+      begin
+        res = ChinaSMS.to @phone_number, tpl_params, tpl_id: 1199525
       rescue Exception => ex
         Rails.logger.error ex
         return nil
