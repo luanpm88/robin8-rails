a8b0d9362c9beee81998b09679306145ee1c6c88
james <tardis54@163.com>
2016-01-12 19:41:38 +0800

    fxi

--- app/models/campaign_invite.rb
+++ app/models/campaign_invite.rb
@@ -13,7 +13,7 @@ class CampaignInvite < ActiveRecord::Base
 
   def screenshot_pass
     campaign = self.campaign
-    if campaign.status == 'executed' && self.img_status != 'passed'
+    if (campaign.status == 'executed' || campaign.status = "executing") && self.img_status != 'passed'
       self.update_column(:img_status, 'passed')
       Rails.logger.info "-------- screenshot_check_pass:  ---cid:#{campaign.id}--"
     end
