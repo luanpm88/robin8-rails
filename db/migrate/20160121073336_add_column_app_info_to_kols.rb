16100c5df62e90c18829fe983c6c82c8c488be95
huxl <huxl@huxldeMacBook-Pro.local>
2016-01-22 18:34:18 +0800

    add devise_token to kol

--- db/migrate/20160121073336_add_column_app_info_to_kols.rb
+++ db/migrate/20160121073336_add_column_app_info_to_kols.rb
@@ -3,5 +3,6 @@ class AddColumnAppInfoToKols < ActiveRecord::Migration
     add_column :kols, :app_platform, :string
     add_column :kols, :app_version, :string
     add_column :kols, :private_token, :string
+    add_column :kols, :device_token, :string
   end
 end
--- lib/api/v1/sessions.rb
+++ lib/api/v1/sessions.rb
@@ -10,7 +10,7 @@ module API
           code_right = YunPian::SendRegisterSms.verify_code(params[:mobile_number], params[:code])
           return error!({error: 2, detail: '验证码错误'}, 403)   if !code_right
           kol = Kol.new(mobile_number: params[:mobile_number],  app_platform: params[:app_platform],
-                          app_version: params[:app_version]
+                          app_version: params[:app_version], device_token: pramams[:device_token]
           )
           if kol.save
             kol.reload
@@ -37,12 +37,12 @@ module API
 
         # 用户登录
         post 'sign_in' do
-          required_attributes! [:mobile_number, :code, :app_platform, :app_version]
+          required_attributes! [:mobile_number, :code, :app_platform, :app_version, :device_token]
           kol = Kol.find_by(mobile_number: params[:mobile_number])
           render error_403!({error: 1, detail: '手机号还没注册'}) if kol.blank?
           code_right = YunPian::SendRegisterSms.verify_code(params[:mobile_number], params[:code])
           return error!({error: 2, detail: '验证码错误'}, 403)   if !code_right
-          kol.update_attributes(app_platform: params[:app_platform], app_version: params[:app_version])
+          kol.update_attributes(app_platform: params[:app_platform], app_version: params[:app_version], device_token: pramams[:device_token])
           kol.reload
           present :error, 0
           present :kol, kol, with: API::V1::Entities::KolEntities::Summary
