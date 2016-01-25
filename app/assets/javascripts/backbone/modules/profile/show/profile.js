2d1c3c25ea19cb4bff4928042026acfc84b0129c
james <tardis54@163.com>
2016-01-25 15:39:32 +0800

    remove uploadcare

--- app/assets/javascripts/backbone/modules/profile/show/profile.js
+++ app/assets/javascripts/backbone/modules/profile/show/profile.js
@@ -77,105 +77,81 @@ Robin.module('Profile.Show', function(Show, App, Backbone, Marionette, $, _){
           }
         });
       } else {
-        if (Robin.chinaLocale){
-          var uploader = Qiniu.uploader({
-            runtimes: 'html5,flash,html4',    //上传模式,依次退化
-            browse_button: 'pickfiles',       //上传选择的点选按钮，**必需**
-            uptoken_url: "/users/qiniu_uptoken",
-                //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
-                //若未指定uptoken_url,则必须指定 uptoken ,uptoken由其他程序生成
-            unique_names: true,
-                // 默认 false，key为文件名。若开启该选项，SDK会为每个文件自动生成key（文件名）
-            // save_key: true,
-                // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK在前端将不对key进行任何处理
-            domain: '7xozqe.com1.z0.glb.clouddn.com',
-                //bucket 域名，下载资源时用到，**必需**
-            container: 'container',           //上传区域DOM ID，默认是browser_button的父元素，
-            max_file_size: '100mb',           //最大文件体积限制
-            flash_swf_url: 'js/plupload/Moxie.swf',  //引入flash,相对路径
-            max_retries: 3,                   //上传失败最大重试次数
-            dragdrop: true,                   //开启可拖曳上传
-            drop_element: 'container',        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
-            chunk_size: '4mb',                //分块上传时，每片的体积
-            auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
 
-            filters: {
-              mime_types : [
-                {title : "Image files", extensions: "jpg,jpeg,gif,png"}
-              ]
-            },
+        var uploader = Qiniu.uploader({
+          runtimes: 'html5,flash,html4',    //上传模式,依次退化
+          browse_button: 'pickfiles',       //上传选择的点选按钮，**必需**
+          uptoken_url: "/users/qiniu_uptoken",
+              //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
+              //若未指定uptoken_url,则必须指定 uptoken ,uptoken由其他程序生成
+          unique_names: true,
+              // 默认 false，key为文件名。若开启该选项，SDK会为每个文件自动生成key（文件名）
+          // save_key: true,
+              // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK在前端将不对key进行任何处理
+          domain: '7xozqe.com1.z0.glb.clouddn.com',
+              //bucket 域名，下载资源时用到，**必需**
+          container: 'container',           //上传区域DOM ID，默认是browser_button的父元素，
+          max_file_size: '100mb',           //最大文件体积限制
+          flash_swf_url: 'js/plupload/Moxie.swf',  //引入flash,相对路径
+          max_retries: 3,                   //上传失败最大重试次数
+          dragdrop: true,                   //开启可拖曳上传
+          drop_element: 'container',        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+          chunk_size: '4mb',                //分块上传时，每片的体积
+          auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
 
-            init: {
-                'FilesAdded': function(up, files) {
-                    plupload.each(files, function(file) {
-                        // 文件添加进队列后,处理相关的事情
-                    });
-                },
-                'BeforeUpload': function(up, file) {
-                       // 每个文件上传前,处理相关的事情
-                },
-                'UploadProgress': function(up, file) {
-                       // 每个文件上传时,处理相关的事情
-                },
-                'FileUploaded': function(up, file, info) {
+          filters: {
+            mime_types : [
+              {title : "Image files", extensions: "jpg,jpeg,gif,png"}
+            ]
+          },
 
-                  var domain = up.getOption('domain');
-                  var res = jQuery.parseJSON(info);
-                  var imageView2 = '-400'
-                  var sourceLink = 'http://' + domain + '/' + res.key + imageView2; //获取上传成功后的文件的Url
-                  $.ajax({
-                    method: "POST",
-                    url: "/users/set_avatar_url",
-                    beforeSend: function(xhr) {
-                      xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
-                    },
-                    data: {"avatar_url": sourceLink}
-                  })
-                    .done(function(data){
-                      $("#avatar-image").attr('src', sourceLink);
-                      Robin.currentUser.set({avatar_url: sourceLink});
-                    });
-
-                },
-                'Error': function(up, err, errTip) {
-                   //上传出错时,处理相关的事情
-                },
-                'UploadComplete': function() {
-                       //队列文件处理完毕后,处理相关的事情
-                }
-                // 'Key': function(up, file) {
-                //     // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
-                //     // 该配置必须要在 unique_names: false , save_key: false 时才生效
-                //     var key = "";
-                //     // do something with key here
-                //     return key
-                // }
-            }
-          });
-
-        } else {
-          if (this.model.attributes.avatar_url) {
-            $("#avatar-image").attr('src', this.model.attributes.avatar_url);
-          }
-          var viewObj = this;
-          this.widget = uploadcare.Widget('[role=uploadcare-uploader]').onUploadComplete(function(info){
-            $("#avatar-image").attr('src', info.cdnUrl);
-            viewObj.model.set({avatar_url: info.cdnUrl});
-          });
-          this.widget.validators.push(this.maxFileSize(3145728));
-          if (Robin.afterConfirmationMessage != undefined) {
-            $.growl(Robin.afterConfirmationMessage,{
-              offset: 65,
-              delay: 5000,
-              placement: {
-                from: "top",
-                align: "right"
+          init: {
+              'FilesAdded': function(up, files) {
+                  plupload.each(files, function(file) {
+                      // 文件添加进队列后,处理相关的事情
+                  });
               },
-              type: 'success'
-            });
-            Robin.afterConfirmationMessage = undefined
+              'BeforeUpload': function(up, file) {
+                     // 每个文件上传前,处理相关的事情
+              },
+              'UploadProgress': function(up, file) {
+                     // 每个文件上传时,处理相关的事情
+              },
+              'FileUploaded': function(up, file, info) {
+
+                var domain = up.getOption('domain');
+                var res = jQuery.parseJSON(info);
+                var imageView2 = '-400'
+                var sourceLink = 'http://' + domain + '/' + res.key + imageView2; //获取上传成功后的文件的Url
+                $.ajax({
+                  method: "POST",
+                  url: "/users/set_avatar_url",
+                  beforeSend: function(xhr) {
+                    xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+                  },
+                  data: {"avatar_url": sourceLink}
+                })
+                  .done(function(data){
+                    $("#avatar-image").attr('src', sourceLink);
+                    Robin.currentUser.set({avatar_url: sourceLink});
+                  });
+
+              },
+              'Error': function(up, err, errTip) {
+                 //上传出错时,处理相关的事情
+              },
+              'UploadComplete': function() {
+                     //队列文件处理完毕后,处理相关的事情
+              }
+              // 'Key': function(up, file) {
+              //     // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+              //     // 该配置必须要在 unique_names: false , save_key: false 时才生效
+              //     var key = "";
+              //     // do something with key here
+              //     return key
+              // }
           }
-        }
+        });
       }
     },
 
