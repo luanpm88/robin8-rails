13b728356209409a067a6dfdf9eab5e9c7b81279
huxl <huxl@huxldeMacBook-Pro.local>
2016-01-26 14:53:45 +0800

    fix

--- lib/api/v1/entities/campaign_entities.rb
+++ lib/api/v1/entities/campaign_entities.rb
@@ -2,28 +2,28 @@ module API
   module V1
     module CampaignEntities
       class Summary
-        format_with(:iso_timestamp) { |dt| dt.iso8601 rescue nil }
-        expose :name, :description, :img_url, :status, :message, :url, :per_budget_type, :per_action_budget, :budget
-        with_options(format_with: :iso_timestamp) do
-          expose :deadline
-          expose :start_time
-          expose :remain_time
-        end
-        expose 'avail_click' do |campaign|
-          campaign.get_avail_click
-        end
-        expose 'total_click' do |campaign|
-          campaign.get_total_click
-        end
-        expose 'take_budget' do |campaign|
-          campaign.take_budget
-        end
-        expose 'remain_budget' do |campaign|
-          campaign.remain_budget
-        end
-        expose 'share_time' do |campaign|
-          campaign.get_share_time
-        end
+        # format_with(:iso_timestamp) { |dt| dt.iso8601 rescue nil }
+        # expose :name, :description, :img_url, :status, :message, :url, :per_budget_type, :per_action_budget, :budget
+        # with_options(format_with: :iso_timestamp) do
+        #   expose :deadline
+        #   expose :start_time
+        #   expose :remain_time
+        # end
+        # expose 'avail_click' do |campaign|
+        #   campaign.get_avail_click
+        # end
+        # expose 'total_click' do |campaign|
+        #   campaign.get_total_click
+        # end
+        # expose 'take_budget' do |campaign|
+        #   campaign.take_budget
+        # end
+        # expose 'remain_budget' do |campaign|
+        #   campaign.remain_budget
+        # end
+        # expose 'share_time' do |campaign|
+        #   campaign.get_share_time
+        # end
       end
 
     end
