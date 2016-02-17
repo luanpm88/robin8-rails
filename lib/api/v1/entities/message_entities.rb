655aee5f1ed3d90e8353fdeaa805dd7e8b093a57
huxl <huxl@huxldeMacBook-Pro.local>
2016-02-17 16:12:05 +0800

    add api :message

---
+++ lib/api/v1/entities/message_entities.rb
@@ -0,0 +1,19 @@
+module API
+  module V1
+    module Entities
+      module MessageEntities
+        class Summary < Grape::Entity
+          format_with(:iso_timestamp) { |dt| dt.iso8601 rescue nil }
+          expose :title, :message_type, :is_read, :url, :logo_url, :sender
+          expose :desc do |message|
+            message.desc || (message.item.name   rescue nil)
+          end
+          with_options(format_with: :iso_timestamp) do
+            expose :read_at
+          end
+        end
+      end
+    end
+  end
+end
+
--- lib/api/v1/messages.rb
+++ lib/api/v1/messages.rb
@@ -6,16 +6,31 @@ module API
           authenticate!
         end
 
+        params do
+          requires :status, type: String, values: ['all', 'unread', 'read']
+          optional :page, type: Integer
+        end
         get '/' do
-          cities = City.all
+          messages = current_kol.messages.send(params[:status]).page(params[:page]).per_page(10)
           present :error, 0
-          present :cities, cities, with: API::V1::Entities::CityEntities::Summary
+          to_paginate(messages)
+          present :messages, messages, with: API::V1::Entities::MessageEntities::Summary
         end
 
-        put 'read' do
 
+        params do
+          requires :id, type: Integer
         end
-
+        put ':id/read' do
+          message = current_kol.messages.find(params[:id])   rescue nil
+          if message
+            message.read
+            present :error, 0
+            present :message, message, with: API::V1::Entities::MessageEntities::Summary
+          else
+            return error_403!({error: 1, detail: '你查看的消息不存在' })
+          end
+        end
       end
     end
   end
