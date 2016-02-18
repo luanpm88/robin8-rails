84266e1a7d906fc963f4604ae5d31a80731a58f8
AndyHu <meeasyhappy@gmail.com>
2016-02-18 14:55:43 +0800

    给Hello World React 添加注释

--- client/app/bundles/HelloWorld/startup/HelloWorldAppClient.jsx
+++ client/app/bundles/HelloWorld/startup/HelloWorldAppClient.jsx
@@ -8,8 +8,21 @@ import HelloWorld from '../containers/HelloWorld';
 // This is how you get props from the Rails view into the redux store.
 // This code here binds your smart component to the redux store.
 export default (props) => {
+  // store 介绍： http://cn.redux.js.org/docs/basics/Store.html
   const store = createStore(props);
+
+  // 监听 state 更新时，打印日志
+  // 注意 subscribe() 返回一个函数用来注销监听器
+  console.log(store.getState());
+  let unsubscribe = store.subscribe(() =>
+    console.log(store.getState())
+  );
+  // 取消监听使用 unsubscribe(); 便于调试时 追踪变化 流程。
+
   const reactComponent = (
+    // 追踪一下provider 的原理
+    // 如何 将 HelloWorld 需要的参数 传进来的
+    // andy 的理解 是 store 会作为 props 一个参数 参进去。
     <Provider store={store}>
       <HelloWorld />
     </Provider>
