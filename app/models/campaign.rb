0c4590e3ea52dc6f3c89442a72abe91833e0f3fd
james <tardis54@163.com>
2016-02-23 12:18:07 +0800

    create campaign_action_url

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -21,6 +21,7 @@ class Campaign < ActiveRecord::Base
   has_many :weibo, through: :weibo_invites
   has_many :articles
   has_many :kol_categories, :through => :kols
+  has_many :campaign_action_urls, autosave: true
 
   has_many :campaign_categories
   has_many :iptc_categories, :through => :campaign_categories
