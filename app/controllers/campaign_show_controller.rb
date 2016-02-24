7ee1085bd190e96d8f89a249cc0e7483845ff7a8
AndyHu <meeasyhappy@gmail.com>
2016-02-24 14:13:15 +0800

    重构campaign show controller

--- app/controllers/campaign_show_controller.rb
+++ app/controllers/campaign_show_controller.rb
@@ -3,29 +3,23 @@ class CampaignShowController < ApplicationController
   layout 'website'
 
   def show
-    uuid_params = Base64.decode64(params[:uuid])
+    uuid_params = JSON.parse(Base64.decode64(params[:uuid]))
 
-    campaign_id = JSON.parse(uuid_params)['campaign_id']     rescue nil
+    campaign_id = uuid_params['campaign_id']
     @campaign = Campaign.find campaign_id rescue nil
     return render :text => "你访问的Campaign 不存在" if @campaign.nil?
+    
     Rails.logger.info "-----show ---#{@campaign.status} -- #{params[:uuid]} --- #{cookies[:_robin8_visitor]} --- #{request.remote_ip}"
+    
     if @campaign && @campaign.status == 'agreed'
       redirect_to @campaign.url
     elsif @campaign
-      other_options = {}
-
       if @campaign.is_cpa?
-        other_options[:step] = (uuid_params["step"] || 1).to_i
-        if other_options[:step] == 1
-          campaign_invite = CampaignInvite.where(:uuid => params[:uuid]).first     rescue nil
-          
-          expired_at = (@campaign.deadline > Time.now ? @campaign.deadline : Time.now)
-          Rails.cache.write(cookies[:_robin8_visitor] + ":cpa_campaign_id:#{@campaign.id}", campaign_invite.id, :expired_at => expired_at) if campaign_invite
-        end
+        deal_with_cpa_campaign uuid_params
+      else
+        CampaignShowWorker.perform_async(params[:uuid], cookies[:_robin8_visitor], request.remote_ip, request.user_agent, request.referer, other_options)
+        redirect_to @campaign.url
       end
-
-      CampaignShowWorker.perform_async(params[:uuid], cookies[:_robin8_visitor], request.remote_ip, request.user_agent, request.referer, other_options)
-      redirect_to @campaign.url
     end
   end
 
@@ -40,5 +34,22 @@ class CampaignShowController < ApplicationController
     # end
   end
 
+  private
+  def deal_with_cpa_campaign uuid_params
+    other_options = {}
+    other_options[:step] = (uuid_params["step"] || 1).to_i
+    if other_options[:step] == 1
+      campaign_invite = CampaignInvite.where(:uuid => params[:uuid]).first     rescue nil
+      expired_at = (@campaign.deadline > Time.now ? @campaign.deadline : Time.now)
+      Rails.cache.write(cookies[:_robin8_visitor] + ":cpa_campaign_id:#{@campaign.id}", campaign_invite.id, :expired_at => expired_at) if campaign_invite
+    end
 
+    CampaignShowWorker.perform_async(params[:uuid], cookies[:_robin8_visitor], request.remote_ip, request.user_agent, request.referer, other_options)
+    if other_options[:step] == 1
+      redirect_to @campaign.url
+    else
+      campaign_action_url = CampaignActionUrl.find_by :id => uuid_params[:campaign_action_id]
+      redirect_to (campaign_action_url.try(:action_url) || "http://robin8.net")
+    end
+  end
 end
