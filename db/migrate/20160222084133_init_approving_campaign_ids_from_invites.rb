03dd8e75b8928f5c74c107ed75274b82ec6ceccb
huxl <huxl@huxldeMacBook-Pro.local>
2016-02-29 10:35:39 +0800

    update comment

--- db/migrate/20160222084133_init_approving_campaign_ids_from_invites.rb
+++ db/migrate/20160222084133_init_approving_campaign_ids_from_invites.rb
@@ -2,7 +2,7 @@ class InitApprovingCampaignIdsFromInvites < ActiveRecord::Migration
   def change
     #删除以前无效 invites
     CampaignInvite.where("uuid is null and id < 163 ").delete_all
-    # 历史无上传截图的 (status 还是rejected)
+    # 历史无上传截图的 (之前只把img_status 至为rejected， status 还是finished)
     CampaignInvite.joins(:campaign).where("campaign_invites.status = 'finished' and campaign_invites.img_status ='rejected' and campaigns.status='settled'").each do |campaign_invite|
       campaign_invite.status = 'rejected'
       campaign_invite.save
@@ -11,10 +11,10 @@ class InitApprovingCampaignIdsFromInvites < ActiveRecord::Migration
       puts "---kol_id:#{kol.id}"
       # add campaign_id to kol receive_campaign list
       CampaignInvite.where(:kol_id => kol.id).collect{|invite| invite.campaign_id }.each do |campaign_id|
-        kol.add_campaign_id(campaign_id)
+        kol.add_campaign_id(campaign_id,false)
       end
 
-      # remove history rejected status as miss and get running from redis
+      # remove history rejected status as missed and get running from redis
       CampaignInvite.where(:kol_id => kol.id).where("status = 'rejected'  or status ='pending'").delete_all
     end
 
