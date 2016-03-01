f3e1b64aaefcb9e2f92373d926603669a2d04919
huxl <huxl@huxldeMacBook-Pro.local>
2016-03-01 11:10:46 +0800

    adminpanel display feedback

---
+++ app/admin/feedbacks.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Feedback do
+
+  remove_filter :kol
+  controller do
+    def scoped_collection
+      Feedback.includes(:kol)   # includes User / Brand models in listing products
+    end
+  end
+
+  index do
+    column "Kol" do |resource|
+      (link_to resource.kol.id, "/admin/kols/#{resource.kol.id}", :target => "_blank")
+    end
+
+    column "mobile_number" do |resource|
+      resource.kol.mobile_number
+    end
+
+    column :app_version
+    column :app_platform
+    column :os_version
+    column :device_model
+    column :content
+    column :created_at
+  end
+end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,4 @@
 class Feedback < ActiveRecord::Base
+
+  belongs_to :kol
 end
