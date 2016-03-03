7a51e55541c937837e16d06d852caf671d70e3c0
huxl <huxl@huxldeMacBook-Pro.local>
2016-03-03 16:45:14 +0800

    add get phone location

--- app/workers/phone_location_worker.rb
+++ app/workers/phone_location_worker.rb
@@ -1,10 +1,9 @@
 class PhoneLocationWorker
   include Sidekiq::Worker
-  sidekiq_options :queue => :phone_location, :retry => 1
+  sidekiq_options :queue => :phone_location, :retry => 0
 
   def perform(phone, store_key)
-
-
+    PhoneLocation.get_location(phone,store_key)
   end
 
 end
