5bee9419df8ecb8eda76b0bcef6e14dfb6cca391
david <davidqhr@gmail.com>
2016-03-08 15:13:13 +0800

    hot和static模式，用不同的css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,11 +19,7 @@ Rails.application.config.assets.precompile += %w(
   website.css website.js  kol/*.js kol/*.css app/*.js app/*.css)
 
 Rails.application.config.assets.precompile +=  %w(*.woff *.ttf *.svg *.eot)
-# Add client/assets/ folders to asset pipeline's search path.
-# If you do not want to move existing images and fonts from your Rails app
-# you could also consider creating symlinks there that point to the original
-# rails directories. In that case, you would not add these paths here.
-Rails.application.config.assets.paths << Rails.root.join("client", "assets", "stylesheets")
-Rails.application.config.assets.paths << Rails.root.join("client", "assets", "images")
-Rails.application.config.assets.paths << Rails.root.join("client", "assets", "fonts")
-Rails.application.config.assets.precompile += %w( generated/server-bundle.js )
+
+# react 部分 编译
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
+Rails.application.config.assets.precompile += %w( brand_static.css brand_static.js server-bundle.js )
