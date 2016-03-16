7544d93d1717c24838ddae997d8bf356a0d263b0
luotao <luotao0124@gmail.com>
2016-03-16 12:18:24 +0800

    custom success formatter(just for multi resource)
    - { items: [], paginate: {} }
    - sigle resource still is {id: '', name: ''...}

--- app/api/brand/v1/endpoints.rb
+++ app/api/brand/v1/endpoints.rb
@@ -1,7 +1,21 @@
+module SuccessFormatter
+  def self.call obj, env
+    if obj.is_a? Array
+      {
+        :items => obj,
+        :paginate => env['api.endpoint'].header
+      }
+    else
+      obj
+    end.to_json
+  end
+end
+
 module Brand
   module V1
     class Endpoints < Base
       version 'v1', using: :path
+      formatter :json, SuccessFormatter
 
       # helpers
       #
--- spec/api/user_api_spec.rb
+++ spec/api/user_api_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Brand::V1::UserAPI do
     before :each do
       @rich_user = FactoryGirl.create(:rich_user)
       FactoryGirl.create(:campaign, user: @rich_user)
-      
+
       login_as(@rich_user, :scope => :user)
     end
 
@@ -39,30 +39,33 @@ RSpec.describe Brand::V1::UserAPI do
     it 'returns campaigns belongs to current_user' do
       get '/brand_api/v1/user/campaigns'
 
-      pattern = [{
-        id: Integer,
-        name: String,
-        description: String,
-        short_description: wildcard_matcher,
-        img_url: wildcard_matcher,
-        status: String,
-        user: Hash,
-        message: wildcard_matcher,
-        url: String,
-        budget: Fixnum,
-        per_budget_type: String,
-        per_action_budget: Float,
-        deadline: String,
-        start_time: String,
-        avail_click: Fixnum,
-        total_click: Fixnum,
-        fee_info: String,
-        share_time: Integer,
-        take_budget: Float,
-        remain_budget: Float,
-        created_at: String,
-        updated_at: String
-      }]
+      pattern = {
+        items: [{
+          id: Integer,
+          name: String,
+          description: String,
+          short_description: wildcard_matcher,
+          img_url: wildcard_matcher,
+          status: String,
+          user: Hash,
+          message: wildcard_matcher,
+          url: String,
+          budget: Fixnum,
+          per_budget_type: String,
+          per_action_budget: Float,
+          deadline: String,
+          start_time: String,
+          avail_click: Fixnum,
+          total_click: Fixnum,
+          fee_info: String,
+          share_time: Integer,
+          take_budget: Float,
+          remain_budget: Float,
+          created_at: String,
+          updated_at: String
+        }],
+        paginate: Hash
+      }
       expect(response.body).to match_json_expression pattern
     end
   end
