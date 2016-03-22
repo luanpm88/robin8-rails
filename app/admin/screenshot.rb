0da130ddded63c54c46e590236b09a663e7f478e
james <tardis54@163.com>
2016-03-22 14:50:27 +0800

    improve active admin for screenshot

--- app/admin/screenshot.rb
+++ app/admin/screenshot.rb
@@ -3,11 +3,9 @@ ActiveAdmin.register_page "Screenshot" do
   content do
     campaigns =[]
     kols = []
-    # TODO check timeline
-    timeline = Rails.env.production? ? 4.days : 4.days
-    campaign_invites = CampaignInvite.joins(:campaign, :kol).where("screenshot != ? AND img_status = ? AND (campaign_invites.status = ? OR campaign_invites.status = ?) AND campaigns.deadline > ?", "", "pending", "approved", "finished", Time.now-timeline)
-    sum = campaign_invites.count
-    @campaign_invites = campaign_invites.limit(12)
+    timeline = Campaign::SettleWaitTimeForBrand
+    sum = CampaignInvite.joins(:campaign, :kol).where("screenshot != ? AND img_status = ? AND (campaign_invites.status = ? OR campaign_invites.status = ?) AND campaigns.deadline > ?", "", "pending", "approved", "finished", Time.now-timeline).count
+    @campaign_invites = CampaignInvite.joins(:campaign, :kol).where("screenshot != ? AND img_status = ? AND (campaign_invites.status = ? OR campaign_invites.status = ?) AND campaigns.deadline > ?", "", "pending", "approved", "finished", Time.now-timeline).limit(40)
     count = @campaign_invites.count
     count.times do |i|
       campaigns << @campaign_invites[i].campaign
