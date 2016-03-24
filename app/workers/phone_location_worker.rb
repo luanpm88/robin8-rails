00d15af93b22399fb3fd72774b93b6d9faad1306
huxl <huxl@huxldeMacBook-Pro.local>
2016-03-24 17:41:26 +0800

    remove worker for influence

--- app/workers/phone_location_worker.rb
+++
@@ -1,9 +0,0 @@
-class PhoneLocationWorker
-  include Sidekiq::Worker
-  sidekiq_options :queue => :phone_location, :retry => 0
-
-  def perform(phone, store_key, kol_uuid)
-    PhoneLocation.get_location(phone, store_key, kol_uuid)
-  end
-
-end
