87a910a9f5d35d3a623302613e58421696b98264
james <tardis54@163.com>
2016-03-24 16:58:20 +0800

    campaign 'DetailPartial' style

--- client/app/bundles/Robin8/components/shared/ShowError.jsx
+++ client/app/bundles/Robin8/components/shared/ShowError.jsx
@@ -11,12 +11,6 @@ export default class showError extends Component {
 
     let style = { color: 'red', marginTop: 8 }
 
-    if (field.name === 'per_action_budget') {
-      style = {color: 'red', marginTop: 8, paddingLeft: 45}
-    } else if (field.name === 'action_url' || field.name === 'short_url') {
-      style = {color: 'red', marginTop: 8, paddingLeft: 140}
-    }
-
     return (
       <div style={style}>{ (field.touched || field.dirty) && field.error }</div>
     );
