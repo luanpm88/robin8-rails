9b9470d21001d350a1cc7abda2b0890c607cc460
huxl <huxl@huxldeMacBook-Pro.local>
2016-03-24 17:27:42 +0800

    fix

--- lib/api/v2/notify.rb
+++ lib/api/v2/notify.rb
@@ -4,7 +4,7 @@ module API
       resources :notify do
         get 'clean_cache' do
           if params[:private_key] == 'robin8'
-            Articles::Store.reset_all_list
+            ::Articles::Store.reset_all_list
             present :error, 0
           else
             present :error, 1
