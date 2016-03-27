cfec2c68b5cc03140e64e9ba8aa24b9401f42fc9
luotao <luotao0124@gmail.com>
2016-03-27 10:46:59 +0800

    refactor code

--- app/controllers/marketing_dashboard/kols_controller.rb
+++ app/controllers/marketing_dashboard/kols_controller.rb
@@ -1,9 +1,14 @@
 class MarketingDashboard::KolsController < MarketingDashboard::BaseController
   def index
+    load_kols
+  end
+
+  private
+  def load_kols
     @kols = if params[:campaign_id]
-	      Campaign.find_by(params[:campaign_id]).kols.paginate(:page => 1, :per_page => 20)
-	    else
-	      Kol.all.paginate(:page => 1, :per_page => 20)
-	    end
+              Campaign.find_by(params[:campaign_id]).kols.paginate(:page => 1, :per_page => 20)
+            else
+              Kol.all.paginate(:page => 1, :per_page => 20)
+            end
   end
 end
