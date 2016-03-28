986641161758bd8a58bce1e9319cebca0fd3b2e4
AndyHu <meeasyhappy@gmail.com>
2016-03-28 19:49:47 +0800

    add Link

--- client/app/bundles/Robin8/components/shared/Nav.jsx
+++ client/app/bundles/Robin8/components/shared/Nav.jsx
@@ -1,6 +1,5 @@
-console.log("in BrandNav");
-
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
 export default class BrandNav extends React.Component {
   static propTypes = {
@@ -19,7 +18,7 @@ export default class BrandNav extends React.Component {
     return (
       <header className="navbar r-global-header">
         <div className="container-fluid">
-          <a href="#" className="logo">Robin8</a>
+          <Link to="/brand/" className="logo">Robin8</Link>
           <div className="navbar-user pull-right">
             <div className="dropdown ">
               <a href="#" data-toggle="dropdown" className="username">{ brand.get('email') }<i className="caret-arrow"></i></a>
