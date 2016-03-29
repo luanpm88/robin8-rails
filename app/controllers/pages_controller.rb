bdffe79c39cb1045512685ab227ed8f8d55dd7a5
luotao <luotao0124@gmail.com>
2016-03-29 16:07:18 +0800

    default to landingpage, if signin to /brand

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,30 +14,38 @@ class PagesController < ApplicationController
   end
 
   def home
-    if user_signed_in? && !current_user.active_subscription.blank?
-      render "home", :layout => 'application'
-    elsif user_signed_in?
-      render "home", :layout => 'application'
-    elsif kol_signed_in?
-      if current_kol.confirmed_at == nil && current_kol.provider == 'signup'
-        flash[:confirmation_alert] = @l.t('dashboard.check_to_activate')
-      end
-
-      to_verify_count = current_kol.campaign_invites.where(status: 'finished').where.not(img_status: 'passed').joins(:campaign).where('campaign_invites.avail_click > 0 AND campaigns.deadline > ?', Time.now - Campaign::SettleWaitTimeForKol).count
-      verify_failed_count = current_kol.campaign_invites.where(status: 'finished', img_status: 'rejected').count
-
-      if to_verify_count>0 or verify_failed_count>0
-        flash[:verify_count] = {
-          :to_verify_count => to_verify_count,
-          :verify_failed_count => verify_failed_count
-        }
-      end
-
-      render "home", :layout => 'kol'
+    if user_signed_in?
+      redirect_to '/brand'
     else
       render 'landing_page_v2', :layout => 'brand_v2'
     end
   end
+
+  # def home
+  #   if user_signed_in? && !current_user.active_subscription.blank?
+  #     render "home", :layout => 'application'
+  #   elsif user_signed_in?
+  #     render "home", :layout => 'application'
+  #   elsif kol_signed_in?
+  #     if current_kol.confirmed_at == nil && current_kol.provider == 'signup'
+  #       flash[:confirmation_alert] = @l.t('dashboard.check_to_activate')
+  #     end
+
+  #     to_verify_count = current_kol.campaign_invites.where(status: 'finished').where.not(img_status: 'passed').joins(:campaign).where('campaign_invites.avail_click > 0 AND campaigns.deadline > ?', Time.now - Campaign::SettleWaitTimeForKol).count
+  #     verify_failed_count = current_kol.campaign_invites.where(status: 'finished', img_status: 'rejected').count
+
+  #     if to_verify_count>0 or verify_failed_count>0
+  #       flash[:verify_count] = {
+  #         :to_verify_count => to_verify_count,
+  #         :verify_failed_count => verify_failed_count
+  #       }
+  #     end
+
+  #     render "home", :layout => 'kol'
+  #   else
+  #     render 'landing_page_v2', :layout => 'brand_v2'
+  #   end
+  # end
 
   def landing_page_brand
     render "landing_page_brand", :layout => 'landing'
