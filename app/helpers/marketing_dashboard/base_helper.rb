1a20e97f6cb26629bc2b9de8662bba47ae6e868a
luotao <luotao0124@gmail.com>
2016-03-30 16:58:01 +0800

    more smart multi_send method(auto convert *_id to url path)

--- app/helpers/marketing_dashboard/base_helper.rb
+++ app/helpers/marketing_dashboard/base_helper.rb
@@ -1,2 +1,19 @@
 module MarketingDashboard::BaseHelper
+  def multi_send obj, list
+    splited_list = list.split('.')
+    @result = obj
+    (splited_list.count > 0) ? (splited_list.each {|x| @result = parse_and_send(@result, x)}) : (@result = nil)
+    @result
+  end
+
+  private
+  def parse_and_send r, x
+    if x.include? '_id'
+      resource = x.sub '_id', ''
+      id = r.send x
+      link_to id, Rails.application.routes.url_helpers.send("marketing_dashboard_#{resource}_path", id)
+    else
+      r.send x
+    end
+  end
 end
