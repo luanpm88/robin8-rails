4b1d5fa2d1c373f43ca738cacc92f5e6261a90ae
james <tardis54@163.com>
2016-04-07 10:25:25 +0800

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
 
   has_many :transactions, :as => :account
 
-  validates_uniqueness_of :mobile_number, :message => "手机号码已经存在"
-  validates_uniqueness_of :name, :message => "品牌名称已经存在"
+  validates_uniqueness_of :mobile_number, allow_blank: true, :message => "手机号码已经存在"
+  validates_uniqueness_of :name, allow_blank: true, :message => "品牌名称已经存在"
   validates_length_of :password, :minimum => 6, :message => "密码长度最少为6位"
 
   # Include default devise modules. Others available are:
