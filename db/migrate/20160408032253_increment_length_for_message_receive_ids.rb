8e5eaa596e76ac6f1e2919abca9681e597b4dd8c
huxl <huxl@huxldeMacBook-Pro.local>
2016-04-08 13:42:17 +0800

    fix

--- db/migrate/20160408032253_increment_length_for_message_receive_ids.rb
+++ db/migrate/20160408032253_increment_length_for_message_receive_ids.rb
@@ -3,6 +3,6 @@ class IncrementLengthForMessageReceiveIds < ActiveRecord::Migration
     change_column :messages, :receiver_ids, :longtext, :limit => 165000
 
     change_column :push_messages, :receiver_ids, :longtext, :limit => 165000
-    change_column :messages, :receiver_cids, :longtext, :limit => 165000
+    change_column :push_messages, :receiver_cids, :longtext, :limit => 165000
   end
 end
