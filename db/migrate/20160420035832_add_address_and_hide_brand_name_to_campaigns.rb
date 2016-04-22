afa0bd140e7b3a9be9e5893b644395fa23d84a28
huxl <huxl@huxldeMacBook-Pro.local>
2016-04-22 17:35:35 +0800

    improve

--- db/migrate/20160420035832_add_address_and_hide_brand_name_to_campaigns.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressAndHideBrandNameToCampaigns < ActiveRecord::Migration
-  def change
-    add_column :campaigns, :address, :string
-    add_column :campaigns, :hide_brand_name, :boolean, default: false
-  end
-end
--- db/migrate/20160421114313_add_column_end_apply_to_campaign.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnEndApplyToCampaign < ActiveRecord::Migration
-  def change
-    add_column :campaigns, :end_apply_check, :boolean, :default => false
-  end
-end
