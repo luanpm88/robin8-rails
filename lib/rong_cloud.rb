e16e5d7cbf8dc1c3c873f9d10df9b97f7f1d3aec
huxl <huxl@huxldeMacBook-Pro.local>
2016-04-26 16:40:52 +0800

    improve

--- lib/rong_cloud.rb
+++ lib/rong_cloud.rb
@@ -18,7 +18,7 @@ class RongCloud
     server = "#{Server}/user/getToken.json"
     puts server
     res_json = RestClient.post server, {:userId => kol.id, :name => kol.name, :portraitUri => kol.avatar_url}, headers     rescue nil
-    return nil if res_json
+    return nil if res_json.blank?
     res  = JSON.parse res_json
     res['token']
   end
