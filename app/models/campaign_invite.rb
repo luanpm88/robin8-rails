abd4fa9a326dd85a1a70503e8af93dae7ebb998f
huxl <huxl@huxldeMacBook-Pro.local>
2016-04-28 15:01:23 +0800

    improve campaign model

--- app/models/campaign_invite.rb
+++ app/models/campaign_invite.rb
@@ -74,7 +74,7 @@ class CampaignInvite < ActiveRecord::Base
     elsif campaign.status == 'executed'
       ActiveRecord::Base.transaction do
         self.update_attributes(:img_status => 'passed', :status => 'settled')
-        if campaign.is_click_type?  || campaign.is_cpa?
+        if campaign.is_click_type?  || campaign.is_cpa_type?
           kol.income(self.avail_click * campaign.per_action_budget, 'campaign', campaign, campaign.user)
           Rails.logger.transaction.info "---kol_id:#{kol.id}----- screenshot_check_pass: -click--cid:#{campaign.id}---fee:#{self.avail_click * campaign.per_action_budget}---#avail_amount:#{kol.avail_amount}-"
         else
@@ -125,7 +125,7 @@ class CampaignInvite < ActiveRecord::Base
     return 0 if self.new_record?
     campaign = self.campaign
     return 0.0 if campaign.blank?
-    if campaign.is_click_type? or campaign.is_cpa?
+    if campaign.is_click_type? or campaign.is_cpa_type?
       (get_avail_click * campaign.per_action_budget).round(2)       rescue 0
     else
       campaign.per_action_budget.round(2) rescue 0
