7d8500c40f6792b0bc245125e8599f8fb51512d3
james <tardis54@163.com>
2016-05-18 15:32:47 +0800

    add after_tax_credits field

--- db/migrate/20160512081720_add_trade_no_to_transaction.rb
+++ db/migrate/20160512081720_add_trade_no_to_transaction.rb
@@ -1,6 +1,7 @@
 class AddTradeNoToTransaction < ActiveRecord::Migration
   def change
     add_column :transactions, :trade_no, :string, default: nil, limit: 191
+    add_column :transactions, :after_tax_credits, :string, default: nil
     add_index :transactions, :trade_no, :unique => true
   end
 end
