6290c96cd2a72245555ab7b2d97b8babf23256ba
james <tardis54@163.com>
2016-05-19 20:33:05 +0800

    参数位置错误

--- app/workers/campaign_show_worker.rb
+++ app/workers/campaign_show_worker.rb
@@ -4,6 +4,6 @@ class CampaignShowWorker
 
   def perform(invite_uuid, visitor_cookies, visitor_ip, visitor_agent, visitor_referer, proxy_ips, other_options={})
     Rails.logger.campaign_show_sidekiq.info "\n=============CampaignShowWorker --- #{invite_uuid} --- #{visitor_cookies} - #{visitor_ip} - #{visitor_agent} - #{visitor_referer} - #{other_options}"
-    CampaignShow.add_click(invite_uuid, visitor_cookies, visitor_ip, visitor_agent, visitor_referer, other_options, proxy_ips)
+    CampaignShow.add_click(invite_uuid, visitor_cookies, visitor_ip, visitor_agent, visitor_referer, proxy_ips, other_options)
   end
 end
