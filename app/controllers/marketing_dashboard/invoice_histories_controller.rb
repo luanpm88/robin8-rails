9f561072a8a6a7f4fa558637d8ae474d4497e943
james <tardis54@163.com>
2016-05-20 15:12:52 +0800

    unique validate for invoice history tracking_number

--- app/controllers/marketing_dashboard/invoice_histories_controller.rb
+++ app/controllers/marketing_dashboard/invoice_histories_controller.rb
@@ -21,6 +21,11 @@ class MarketingDashboard::InvoiceHistoriesController < MarketingDashboard::BaseC
     tracking_no = params[:tracking_no]
     @invoice_history = InvoiceHistory.find(params[:id])
     @invoice_history.update_attributes(tracking_number: tracking_no, status: 'sent')
-    redirect_to marketing_dashboard_invoice_histories_path
+    if @invoice_history.errors.messages.first.last.last
+      flash.now[:alert] = "快递单号重复，请检查后重新输入"
+      render 'send'
+    else
+      redirect_to marketing_dashboard_invoice_histories_path
+    end
   end
 end
--- app/models/invoice_history.rb
+++ app/models/invoice_history.rb
@@ -1,6 +1,7 @@
 class InvoiceHistory < ActiveRecord::Base
 
   validates :credits, :invoice_type, :title, :address, presence: true
-
+  validates :tracking_number, uniqueness: true
+  
   belongs_to :user
 end
