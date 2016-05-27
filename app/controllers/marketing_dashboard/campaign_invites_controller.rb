f87bd565426e81968cb879f304dbdc7058b6c420
AndyHu <meeasyhappy@gmail.com>
2016-05-27 17:39:44 +0800

    campaign observer

--- app/controllers/marketing_dashboard/campaign_invites_controller.rb
+++ app/controllers/marketing_dashboard/campaign_invites_controller.rb
@@ -12,7 +12,7 @@ class MarketingDashboard::CampaignInvitesController < MarketingDashboard::BaseCo
   def pending
     # valid_period = Campaign::SettleWaitTimeForBrand
     # @campaign_invites = CampaignInvite.joins(:campaign, :kol).where.not(:screenshot => "").where(:img_status => :pending).where(:status => ['approved', 'finished']).where("campaigns.deadline > ?", Time.now-valid_period).paginate(paginate_params)
-    params[:per_page] = 18
+    params[:per_page] = 12
     if params[:observer_status].to_i == 2
       @campaign_invites = CampaignInvite.where(:img_status => :pending, :observer_status => 2).where("screenshot is not NULL").order('created_at DESC').paginate(paginate_params)
     elsif params[:observer_status].to_i == 1
