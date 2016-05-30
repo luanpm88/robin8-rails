2a53844f1f9bab64fcb433d2a54f3e61af55a48d
huxl <huxl@huxldeMacBook-Pro.local>
2016-05-30 14:22:08 +0800

    fix

--- lib/api/v1_3/withdraws.rb
+++ lib/api/v1_3/withdraws.rb
@@ -12,7 +12,7 @@ module API
         post 'apply' do
           return {:error => 1, :detail => '金额满100方可提现'}  if params[:credits] < 100
           return {:error => 1, :detail => '请您先绑定支付宝'}  if current_kol.alipay_account.blank?
-          if current_kol.avail_amount > params[:credits] && params[:credits] > 0
+          if current_kol.avail_amount >= params[:credits] && params[:credits] > 0
             withdraw = Withdraw.new(:credits => params[:credits], :withdraw_type => 'alipay', :alipay_no => current_kol.alipay_account,
                                     :real_name => current_kol.alipay_name, :kol_id => current_kol.id)
             withdraw.status = 'pending'
