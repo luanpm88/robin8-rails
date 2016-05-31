62e4d61b975b0582230be8d35fc1ca3f81407259
huxl <huxl@huxldeMacBook-Pro.local>
2016-05-31 11:39:36 +0800

    valid price

--- lib/api/v1_3/kol_identity_prices.rb
+++ lib/api/v1_3/kol_identity_prices.rb
@@ -14,14 +14,17 @@ module API
 
         params do
           requires :provider, type: String, values: ['public_wechat', 'wechat', 'weibo']
-          requires :name, type: String
-          requires :follower_count, type: Integer
-          requires :belong_field, type: String
-          optional :headline_price, type: Float
-          optional :second_price, type: Float
-          optional :single_price, type: Float
+          optional :name, type: String
+          optional :follower_count, type: Integer
+          optional :belong_field, type: String
+          # optional :headline_price, type: Float
+          # optional :second_price, type: Float
+          # optional :single_price, type: Float
         end
         put 'set_price' do
+          return error_403!({error: 1, detail: '头条价格必须大于0数值，或置为空'}) if params[:headline_price].present? &&  params[:headline_price].to_f <= '0'
+          return error_403!({error: 1, detail: '次条价格必须大于0数值，或置为空'}) if params[:second_price].present? &&  params[:second_price].to_f <= '0'
+          return error_403!({error: 1, detail: '单条价格必须大于0数值，或置为空'}) if params[:single_price].present? &&  params[:single_price].to_f <= '0'
           identity_price = current_kol.kol_identity_prices.find_or_create_by(:provider => params[:provider])
           identity_price.update_columns(name: params[:name], follower_count: params[:follower_count],
                                         belong_field: params[:belong_field], headline_price: params[:headline_price],
