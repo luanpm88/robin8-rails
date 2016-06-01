b91405b4f75ee6340df73427c8d99a5b6aabd390
james <tardis54@163.com>
2016-06-01 18:18:20 +0800

    split reducer

--- client/app/bundles/Robin8/reducers/index.jsx
+++ client/app/bundles/Robin8/reducers/index.jsx
@@ -1,10 +1,9 @@
-import brandReducer from './brandReducer';
-import { $$initialState as $$brandState } from './brandReducer';
+import { combineReducers } from 'redux'
+import Immutable from 'immutable';
 
-export default {
-  $$brandStore: brandReducer
-};
-
-export const initialStates = {
-  $$brandState,
-}
+// import campaignReducer from './campaignReducer';
+// import financialReducer from './financialReducer';
+// import profileReducer from './profileReducer';
+import { initialState as campaignInitialState } from './campaignReducer'
+import { initialState as financialInitialState } from './financialReducer'
+import { initialState as profileInitialState } from './profileReducer'
