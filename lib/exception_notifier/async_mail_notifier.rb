13d11e5f5bec63c9a7cdad2811830a1549c3c91a
jyfcrw <jyfcrw@gmail.com>
2016-06-01 10:56:48 +0800

    优化异常邮件通知，过滤重复的错误通知

--- lib/exception_notifier/async_mail_notifier.rb
+++ lib/exception_notifier/async_mail_notifier.rb
@@ -13,7 +13,12 @@ module ExceptionNotifier
         messages << exception.backtrace
       end
 
-      ExceptionMailer.delay.deliver_notification(title, messages, to: @options[:recipients], from: @options[:sender])
+      ExceptionMailWorker.perform_async({
+        title: title,
+        message: messages,
+        to: @options[:recipients],
+        from: @options[:sender]
+      })
     end
 
     def self.default_options
