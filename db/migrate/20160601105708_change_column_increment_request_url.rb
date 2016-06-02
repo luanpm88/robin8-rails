59a2746a445237669528d18577fdcd9b9f97a5ee
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-02 10:23:01 +0800

    fix

--- db/migrate/20160601105708_change_column_increment_request_url.rb
+++ db/migrate/20160601105708_change_column_increment_request_url.rb
@@ -1,14 +1,15 @@
 class ChangeColumnIncrementRequestUrl < ActiveRecord::Migration
   def change
-    change_column :campaign_shows, :request_url, String, :limit => 1000
+    change_column :campaign_shows, :request_url, :string, :limit => 1000
 
-    change_column :kol_influence_values, :kol_uuid, String, :limit => 50
-    change_column :kol_influence_value_histories, :kol_uuid, String, :limit => 50
-    change_column :kol_contacts, :kol_uuid,  String, :limit => 50
-    change_column :tmp_kol_contacts, :kol_uuid, String, :limit => 50
+    change_column :kol_influence_values, :kol_uuid, :string, :limit => 50
+    change_column :kol_influence_value_histories, :kol_uuid, :string, :limit => 50
+    change_column :kol_contacts, :kol_uuid,  :string, :limit => 50
+    change_column :tmp_kol_contacts, :kol_uuid, :string, :limit => 50
     add_index :kol_influence_values, :kol_uuid
     add_index :kol_influence_value_histories, :kol_uuid
     add_index :kol_contacts, :kol_uuid
     add_index :tmp_kol_contacts, :kol_uuid
   end
 end
+
