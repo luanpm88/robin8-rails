0c16f668c863d9905873ba4e5cc9215e8c44391e
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-02 13:58:35 +0800

    improve

--- lib/identity_analysis/public_login.rb
+++ lib/identity_analysis/public_login.rb
@@ -35,6 +35,9 @@ module IdentityAnalysis
                                :params =>  params
                  )
       response = JSON.parse request.response_body   rescue {}
+      logger.info "=======login_with_account======="
+      logger.info cookie
+      logger.info  response.inspect
       cookies = ""
       if response["base_resp"].present? && response["base_resp"]["ret"] == 0
         cookies = append_cookies(cookies, request)
