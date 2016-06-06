a9c1351fdce718248e54681ddc407544f622a128
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-06 17:22:23 +0800

    fix

--- db/migrate/20160606063310_add_index_for_kol_tags.rb
+++ db/migrate/20160606063310_add_index_for_kol_tags.rb
@@ -10,7 +10,9 @@ class AddIndexForKolTags < ActiveRecord::Migration
     add_index :kol_tags, :tag_id
 
 
-    change_column :reward_tasks, :task_type, :string, :limit => 100
-    add_index :reward_tasks, :task_type, :unique => true
+    # change_column :reward_tasks, :task_type, :string, :limit => 100
+    # add_index :reward_tasks, :task_type, :unique => true
   end
 end
+
+
