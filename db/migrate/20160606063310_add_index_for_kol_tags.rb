6ae96a1f35afb33dfee24c8063e47de04cc77eec
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-06 17:59:24 +0800

    remove some

--- db/migrate/20160606063310_add_index_for_kol_tags.rb
+++ db/migrate/20160606063310_add_index_for_kol_tags.rb
@@ -1,13 +1,13 @@
 class AddIndexForKolTags < ActiveRecord::Migration
   def change
-    add_index :article_actions, :kol_id
-
-    add_index :campaign_shows, :kol_id
-
-    add_index :messages, :receiver_id
-
-    add_index :kol_tags, :kol_id
-    add_index :kol_tags, :tag_id
+    # add_index :article_actions, :kol_id
+    #
+    # add_index :campaign_shows, :kol_id
+    #
+    # add_index :messages, :receiver_id
+    #
+    # add_index :kol_tags, :kol_id
+    # add_index :kol_tags, :tag_id
 
 
     # change_column :reward_tasks, :task_type, :string, :limit => 100
