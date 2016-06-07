c5c7dcc69b87cdb8282426aa5bfd751d397ff194
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-07 13:01:26 +0800

    add actual_per_action_budget

--- db/migrate/20160603063335_add_tax_rate_to_campaigns.rb
+++ db/migrate/20160603063335_add_tax_rate_to_campaigns.rb
@@ -1,8 +1,12 @@
 class AddTaxRateToCampaigns < ActiveRecord::Migration
   def change
-    add_column :campaigns, :tax_rate, :float
+    # add_column :campaigns, :tax_rate, :float
+    add_column :campaigns, :actual_per_action_budget, :float
 
-    Campaign.update_all(:tax_rate => 1)
+    # Campaign.update_all(:tax_rate => 1)
+    Campaign.all.each do  |campaign|
+      campaign.update_column(:actual_per_action_budget,campaign.per_action_budget)
+    end
     User.create(:mobile_number => User::PlatformMobile, :name => 'plattform')
   end
 
