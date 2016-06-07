f65f622db8512d48a74e95c8a6c33cd218aeffa5
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-07 20:13:58 +0800

    fix

--- lib/api/v1_3/weixin_report.rb
+++ lib/api/v1_3/weixin_report.rb
@@ -23,11 +23,11 @@ module API
               login = @identity.newest_login
             end
             res = JSON.parse login.get_info     rescue {}
-            login.sync_info_to_identity(res['data']['user'])
+            login.sync_info_to_identity(res['data']['user'])          rescue nil
           end
           if res && res['status']
             present :error, 0
-            present :primary, res['data']['user'], with: API::V1_3::Entities::WeixinReportEntities::Primary
+            present :primary, (res['data']['user'] || {} rescue {}), with: API::V1_3::Entities::WeixinReportEntities::Primary
           else
             present :error, 1
             present :detail, '请求错误，请稍后再试'
@@ -55,7 +55,7 @@ module API
           end
           if res && res['status']
             present :error, 0
-            present :messages, res['data']['messages'], with: API::V1_3::Entities::WeixinReportEntities::Message
+            present :messages, (res['data']['messages'] || {} rescue {}), with: API::V1_3::Entities::WeixinReportEntities::Message
           else
             present :error, 1
             present :detail, '请求错误，请稍后再试'
@@ -84,7 +84,7 @@ module API
           end
           if res && res['status']
             present :error, 0
-            present :articles, res['data']['articles'], with: API::V1_3::Entities::WeixinReportEntities::Article
+            present :articles, (res['data']['articles'] || {} rescue {}), with: API::V1_3::Entities::WeixinReportEntities::Article
           else
             present :error, 1
             present :detail, '请求错误，请稍后再试'
@@ -112,7 +112,7 @@ module API
           end
           if res && res['status']
             present :error, 0
-            present :user_analysises, res['data']['user_analysises'], with: API::V1_3::Entities::WeixinReportEntities::UserAnalysise
+            present :user_analysises, (res['data']['user_analysises'] || {} rescue {}), with: API::V1_3::Entities::WeixinReportEntities::UserAnalysise
           else
             present :error, 1
             present :detail, '请求错误，请稍后再试'
