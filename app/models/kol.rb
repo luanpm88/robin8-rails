9c6e2016ab1c6f39146223a3ae45b48045d6b467
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-08 17:25:27 +0800

    hide mobile number

--- app/models/kol.rb
+++ app/models/kol.rb
@@ -382,7 +382,7 @@ class Kol < ActiveRecord::Base
     else
       kol = Kol.create!(mobile_number: params[:mobile_number],  app_platform: params[:app_platform],
                         app_version: params[:app_version], device_token: params[:device_token],
-                        IMEI: params[:IMEI], IDFA: params[:IDFA], name: params[:mobile_number],
+                        IMEI: params[:IMEI], IDFA: params[:IDFA], name: Kol.hide_real_mobile_number(params[:mobile_number]),
                         utm_source: params[:utm_source], app_city: app_city, os_version: params[:os_version],
                         device_model: params[:device_model], current_sign_in_ip: params[:current_sign_in_ip])
     end
@@ -481,6 +481,10 @@ class Kol < ActiveRecord::Base
 
   def get_kol_level
     self.kol_level || 'A'
+  end
+
+  def self.hide_real_mobile_number(mobile_number)
+    mobile_number.to_s[0,3] + "****" + mobile_number.to_s[7,4]   rescue mobile_number
   end
 
 end
