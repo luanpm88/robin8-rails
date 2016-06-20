68b9170651ad2537dc6b21d0df914802200cd104
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-20 12:15:26 +0800

    add auto check flag to campagin_invite

--- app/models/concerns/campaigns/campaign_process.rb
+++ app/models/concerns/campaigns/campaign_process.rb
@@ -128,7 +128,7 @@ module Campaigns
       Rails.logger.transaction.info "-------- settle_accounts_for_brand: cid:#{self.id}------status: #{self.status}"
       return if self.status != 'executed'
       #首先先付款给期间审核的kol
-      self.finished_invites.update_all({:img_status => 'passed'})
+      self.finished_invites.update_all({:img_status => 'passed', :auto_check => true})
       settle_accounts_for_kol
       #剩下的邀请  状态全设置为拒绝
       ActiveRecord::Base.transaction do
