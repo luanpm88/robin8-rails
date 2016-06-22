3baaac8e2883b507a44f5f2b018876bd28e4ae46
AndyHu <meeasyhappy@gmail.com>
2016-06-22 17:49:58 +0800

    cpi

---
+++ client/app/bundles/Robin8/components/campaigns/show/Install.jsx
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import ReactEcharts from '..'
+
+export default class Install extends React.Component{
+  constructor(props, context){
+    super(props, context);
+  }
+
+  render(){
+
+  }
+}
