3edbc4cc19daba80c0d5a6df5aab2be8a4b40f50
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-27 11:54:45 +0800

    encry cpi data

--- app/models/cpi_reg.rb
+++ app/models/cpi_reg.rb
@@ -1,9 +1,19 @@
 class CpiReg < ActiveRecord::Base
 
-  def self.create_reg(params)
-     cpi_reg = self.create!(appid: params[:appid], bundle_name: params[:bundle_name], app_platform: params[:app_platform],
+  ApiToken = "ecce21119d1931d238cebfb53107bf5e"
+  def self.valid_data?(data)
+    data.is_a?(Hash) &&  data['appid'].present? &&  data['device_uuid'].present?   && data['api_token'] == ApiToken
+  end
+
+  def self.had_reg?(data)
+    CpiReg.where(:appid => data['appid'], :device_uuid => data['device_uuid']).where(:status => 'success').size > 0
+  end
+
+  def self.create_reg(params, decry_data)
+     cpi_reg = self.create!(bundle_name: params[:bundle_name], app_platform: params[:app_platform],
                             app_version: params[:app_version], city_name: params[:city_name],
-                            os_version: params[:os_version], device_model:params[:device_model], reg_ip: params[:reg_ip])
+                            os_version: params[:os_version], device_model:params[:device_model], reg_ip: params[:reg_ip],
+                            appid: decry_data['appid'], device_uuid: decry_data['device_uuid'])
      CampaignShow.update_inviter(cpi_reg)
   end
 
