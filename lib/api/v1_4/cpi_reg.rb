a72deda7b3d06086bf54ae737a16b1edfd64bf15
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-27 11:54:45 +0800

    encry cpi data

--- lib/api/v1_4/cpi_reg.rb
+++ lib/api/v1_4/cpi_reg.rb
@@ -1,26 +1,29 @@
 module API
   module V1_4
     class CpiReg < Grape::API
-      ApiToken = "ecce21119d1931d238cebfb53107bf5e"
       resources :cpi_reg do
 
         desc '注册通知'
         params do
-          requires :api_token, type: String
-          requires :appid, type: String
-          requires :reg_ip, type: String
           requires :bundle_name, type: String
           requires :app_platform, type: String
           requires :app_version, type: String
           requires :os_version, type: String
           requires :device_model, type: String
-          optional :city_name, type: String
+          requires :encryption_data, type: String
         end
         post 'reg_notice' do
-          return {:error => 1, :detail => 'api_token错误'}  if params[:api_token] != CpiReg::ApiToken
-          params[:reg_ip] = request.ip    #override
-          ::CpiReg.create_reg(params)
-          present :error, 0
+          decry_data = AuthToken.decry_cpi_data(params[:encryption_data]) if ::CpiReg.valid_data?(decry_data) == false
+            present :error, 1
+            present :detail, '数据格式不对'
+          elsif ::CpiReg.had_reg?(decry_data)
+            present :error, 1
+            present :detail, '该用户已经注册'
+          else
+            params[:reg_ip] = request.ip    #override
+            ::CpiReg.create_reg(params, decry_data)
+            present :error, 0
+          end
         end
       end
     end
