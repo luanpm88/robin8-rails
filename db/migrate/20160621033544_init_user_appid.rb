e1f0a6612073ebf100986f065564e5ccbebc1c93
james <tardis54@163.com>
2016-06-28 12:03:44 +0800

    fix

--- db/migrate/20160621033544_init_user_appid.rb
+++ db/migrate/20160621033544_init_user_appid.rb
@@ -1,6 +1,5 @@
 class InitUserAppid < ActiveRecord::Migration
   def change
-    add_column :users, :appid, :string, :limit => 50
     User.where("appid is null").each do |user|
       user.init_appid
     end
