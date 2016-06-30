9b09f1e50ef832252981d4b5f181cade5f3033bc
huxl <huxl@huxldeMacBook-Pro.local>
2016-06-30 16:38:52 +0800

    fix

--- db/migrate/20160630065616_update_tags.rb
+++ db/migrate/20160630065616_update_tags.rb
@@ -4,6 +4,7 @@ class UpdateTags < ActiveRecord::Migration
     Tag.find_by(:name => 'ce').update_column(:label, '数码电子')
     Tag.where(:name => ['camera', 'mobile', 'airline']).update_all(:enabled => false)
     Tag.create(:name => 'internet_consumption', :label => '电商消费')
+    Tag.create(:name => 'internet', :label => '互联网')
     Tag.create(:name => 'game', :label => '游戏')
     Tag.create(:name => 'fashion', :label => '时尚')
     Tag.create(:name => 'health', :label => '健康')
