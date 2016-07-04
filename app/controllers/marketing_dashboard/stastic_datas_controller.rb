274380ac86ede3c2577793729d8242d09e9fc035
huxl <huxl@huxldeMacBook-Pro.local>
2016-07-04 11:56:33 +0800

    improve

--- app/controllers/marketing_dashboard/stastic_datas_controller.rb
+++ app/controllers/marketing_dashboard/stastic_datas_controller.rb
@@ -29,14 +29,15 @@ class MarketingDashboard::StasticDatasController < MarketingDashboard::BaseContr
   def new_kol
     day_count = 5
     _start = (day_count + 1).days.ago
-    @total = Kol.where("mobile_number is not null").where("created_at > '#{_start}'").select("DATE(created_at) as created, count(*) as count").order("DATE(created_at) asc").group("DATE(created_at)")
+    @bind_mobiles = Kol.where("mobile_number is not null").where("created_at > '#{_start}'").select("DATE(created_at) as created, count(*) as count").order("DATE(created_at) asc").group("DATE(created_at)")
+    @unbind_mobiles = Kol.where("mobile_number is null").where("created_at > '#{_start}'").select("DATE(created_at) as created, count(*) as count").order("DATE(created_at) asc").group("DATE(created_at)")
     @wechat = Identity.where(:provider => 'wechat').where("created_at > '#{_start}'").select("DATE(created_at) as created, count(distinct(kol_id)) as count").order("DATE(created_at) asc").group("DATE(created_at)")
     @weibo = Identity.where(:provider => 'weibo').where("created_at > '#{_start}'").select("DATE(created_at) as created, count(distinct(kol_id)) as count").order("DATE(created_at) asc").group("DATE(created_at)")
     @qq = Identity.where(:provider => 'qq').where("created_at > '#{_start}'").select("DATE(created_at) as created, count(distinct(kol_id)) as count").order("DATE(created_at) asc").group("DATE(created_at)")
     @collect =  []
     today = Date.today
     day_count.times.each do |index|
-      @collect << {:date => (today - index.days), :total => (@total[index].count rescue nil), :wechat => (@wechat[index].count rescue nil),
+      @collect << {:date => (today - index.days), :bind_mobiles => (@bind_mobiles[index].count rescue nil), :unbind_mobiles => (@unbind_mobiles[index].count rescue nil), :wechat => (@wechat[index].count rescue nil),
                    :weibo => (@weibo[index].count rescue nil), :qq => (@qq[index].count rescue nil)}
     end
   end
