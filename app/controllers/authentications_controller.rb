205e395b75b2466e3d8cac217987df0f1ce3ebf3
james <tardis54@163.com>
2016-07-07 12:14:51 +0800

    config omniauth qq

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,6 +3,7 @@ class AuthenticationsController < ApplicationController
   before_action :handle_omniauth_callback, only: [ :weibo, :wechat ]
 
   def weibo
+    binding.pry
     identity = Identity.find_by(:provider => params[:provider], :uid => params[:uid])
     identity = Identity.find_by(:provider => params[:provider], :unionid => params[:unionid]) if identity.blank? and params[:unionid]
 
@@ -24,7 +25,25 @@ class AuthenticationsController < ApplicationController
   # def wechat
   # end
 
+  def qq_connect
+    binding.pry
+    identity = Identity.find_by(:provider => params[:provider], :uid => params[:uid])
+    identity = Identity.find_by(:provider => params[:provider], :unionid => params[:unionid]) if identity.blank? and params[:unionid]
 
+    if identity.blank?
+      # create identity, redirect to register path
+      identity_params = params.merge(:from_type => 'web')
+      identity_params.merge!(kol_id: current_kol.id) if current_kol
+      identity = Identity.create_identity_from_app(identity_params)
+      redirect_to cookies.delete(:return_url) || root_path(identity_code: identity.id) #重定向到 注册页面
+    else
+      # sign in and set union token
+      user = identity.kol.find_or_create_brand_user
+      sign_in user # maybe change later !
+      set_union_access_token
+      redirect_to cookies.delete(:return_url) || root_path
+    end
+  end
 
   def failure
     p params
@@ -35,7 +54,11 @@ class AuthenticationsController < ApplicationController
   private
 
   def handle_omniauth_callback
+    binding.pry
     auth = request.env['omniauth.auth']
+    Rails.logger.alipay.info "-------- handle_omniauth_callback  --------------"
+    Rails.logger.alipay.info "#{auth}"
+
     params[:uid] = auth.uid
     params[:provider] = auth.provider
     params[:token] = auth.credentials.token
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :weibo, Rails.application.secrets.weibo[:app_key], Rails.application.secrets.weibo[:app_secret]
   provider :wechat, Rails.application.secrets.wechat[:app_id], Rails.application.secrets.wechat[:app_secret]
+  provider :qq_connect, Rails.application.secrets.qq[:app_id], Rails.application.secrets.qq[:app_key]
 
   on_failure { |env| AuthenticationsController.action(:failure).call(env) }
 end
