ea67e67812c9071e9a75eb4dc53a017c95b5a7fc
james <tardis54@163.com>
2016-07-11 16:39:45 +0800

    fix bug

--- lib/api/v1_5/qr_code_login.rb
+++ lib/api/v1_5/qr_code_login.rb
@@ -14,9 +14,13 @@ module API
       post '/scan_qr_code_and_login' do
         login_token = declared(params)[:login_token]
         id = current_kol.id
-        $redis.set token, id
-        ActionCable.server.broadcast "uuid_#{token}", result: "success", token: login_token, id: id
-        present :error, 0
+        $redis.set login_token, id
+        if $redis.get "login_uuid_#{login_token}"
+          ActionCable.server.broadcast "uuid_#{login_token}", {result: "success", token: login_token, id: id}
+          present :error, 0
+        else
+          present :error, 1
+        end
       end
     end
 
