13d5bba9a3a947d9a090163004c5b71f1f92ffd0
AndyHu <meeasyhappy@gmail.com>
2016-07-13 17:35:05 +0800

    处理图片

--- app/workers/campaign_img_worker.rb
+++ app/workers/campaign_img_worker.rb
@@ -4,17 +4,15 @@ class CampaignImgWorker
   def perform(campaign_id, img_url)
     begin
       file_name = URI(img_url).path.downcase[1..-1]
-      if file_name.end_with?("png")
-        local_path = "#{Rails.root}/public/assets/#{file_name}"
-        system("wget #{img_url} -O #{local_path}")
-        if(File.size?(local_path))
-          coverted_image = MiniMagick::Image.open(local_path)
-          coverted_image.format "jpg"
-          image = Image.new
-          image.avatar = coverted_image
-          image.save
-          Campaign.find(campaign_id).update_column(:img_url, image.avatar_url)
-        end
+      local_path = "#{Rails.root}/public/assets/#{file_name}"
+      system("wget #{img_url} -O #{local_path}")
+      if(File.size?(local_path))
+        coverted_image = MiniMagick::Image.open(local_path)
+        coverted_image.format "jpg"
+        image = Image.new
+        image.avatar = coverted_image
+        image.save
+        Campaign.find(campaign_id).update_column(:img_url, image.avatar_url)
       end
     rescue Exception => e
     end
