1bf41e5efb3c6244d1864b13946951418a38b052
james <tardis54@163.com>
2016-07-15 11:01:30 +0800

    fix bug

--- app/api/property/v1/identity.rb
+++ app/api/property/v1/identity.rb
@@ -12,7 +12,7 @@ module Property
           requires :id, type: Integer
         end
         delete "/unbind" do
-          @identity = current_kol.identities.find_by(declared(params)[:id]).try(:delete)
+          @identity = current_kol.identities.find_by(id: declared(params)[:id]).try(:delete)
           return present :message, "第三方帐号解除绑定成功" if @identity
           error!({error: '解绑的第三方账号不存在'}, 400)
         end
