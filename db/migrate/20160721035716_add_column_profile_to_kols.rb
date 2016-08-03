a9978fe9d6447ed4535287d0f7918de9547041ca
huxl <huxl@huxldeMacBook-Pro.local>
2016-08-03 16:23:57 +0800

    add

--- db/migrate/20160721035716_add_column_profile_to_kols.rb
+++ db/migrate/20160721035716_add_column_profile_to_kols.rb
@@ -6,6 +6,7 @@ class AddColumnProfileToKols < ActiveRecord::Migration
     add_column :kols, :role_apply_status, :string, :default => 'pending'
     add_column :kols, :role_apply_time, :datetime
     add_column :kols, :role_check_time, :datetime
+    add_column :kols, :role_apply_remark, :string
 
     add_column :kols, :is_hot, :boolean, :default => false
 
