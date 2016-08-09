6b4c94d5d5c0f0e5541a718666618ec32dd7cda5
huxl <huxl@huxldeMacBook-Pro.local>
2016-08-09 17:19:39 +0800

    improve materials api

--- lib/api/v1_6/campaigns.rb
+++ lib/api/v1_6/campaigns.rb
@@ -4,10 +4,15 @@ module API
       resources :campaigns do
         #获取活动素材
         params do
-          requires :id, type: Integer
+          optional :campaign_id, type: Integer
+          optional :campaign_invite_id, type: Integer
         end
-        get ':id/materials' do
-          campaign = Campaign.find(params[:id])            rescue nil
+        get 'materials' do
+          if params[:campaign_id]
+            campaign = Campaign.find(params[:campaign_id])            rescue nil
+          else
+            campaign = CampaignInvite.find(params[:campaign_invite_id]).campaign            rescue nil
+          end
           if campaign.blank?
             return error_403!({error: 1, detail: '该活动不存在' })
           else
