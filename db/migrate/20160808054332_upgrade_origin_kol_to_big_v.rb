f4d3418220769e98a6a4692d90ea7ada53884769
huxl <huxl@huxldeMacBook-Pro.local>
2016-08-12 19:28:08 +0800

    有作弊嫌疑的 不会自动审核通过

--- db/migrate/20160808054332_upgrade_origin_kol_to_big_v.rb
+++ db/migrate/20160808054332_upgrade_origin_kol_to_big_v.rb
@@ -1,7 +1,8 @@
 class UpgradeOriginKolToBigV < ActiveRecord::Migration
   def change
-    Kol.where("name is not null and gender != 0 and app_city is not null and weixin_friend_count is not null and (avatar is not null or avatar is not null)").includes(:tags).each do |kol|
+    Kol.where("mobile_number is not null and name is not null and gender != 0 and app_city is not null and weixin_friend_count is not null and (avatar is not null or avatar_url is not null)").includes(:tags).each do |kol|
       next if kol.tags.size == 0
+      next if kol.social_accounts.size > 0
       kol.kol_role = 'big_v'
       kol.role_apply_status = 'passed'
       kol.role_check_remark = 'background_auto'
@@ -13,28 +14,6 @@ class UpgradeOriginKolToBigV < ActiveRecord::Migration
             kol.social_accounts.build(provider: 'wechat', username: identity.name,  avatar_url: identity.avatar_url )
           elsif identity.provider == 'qq'
               kol.social_accounts.build(provider: 'qq', username: identity.name,  avatar_url: identity.avatar_url, followers_count: kol.weixin_friend_count  )
-          elsif identity.provider == 'weibo'
-            social_account = kol.social_accounts.build(provider: 'weibo', :homepage => "http://weibo.com/u/#{identity.uid}" )
-            social_account.auto_complete_info
-          end
-        end
-      end
-      kol.save!
-    end
-
-    Kol.where(:id => 59464).includes(:tags).each do |kol|
-      next if kol.tags.size == 0
-      kol.kol_role = 'big_v'
-      kol.role_apply_status = 'passed'
-      kol.role_check_remark = 'background_auto'
-      if kol.identities.size == 0
-        kol.social_accounts.build(provider: 'wechat', username: kol.name, avatar_url: kol.avatar.url , followers_count: kol.weixin_friend_count)
-      elsif kol.social_accounts.size == 0
-        kol.identities.each do |identity|
-          if identity.provider == 'wechat'
-            kol.social_accounts.build(provider: 'wechat', username: identity.name,  avatar_url: identity.avatar_url )
-          elsif identity.provider == 'qq'
-            kol.social_accounts.build(provider: 'qq', username: identity.name,  avatar_url: identity.avatar_url, followers_count: kol.weixin_friend_count  )
           elsif identity.provider == 'weibo'
             social_account = kol.social_accounts.build(provider: 'weibo', :homepage => "http://weibo.com/u/#{identity.uid}" )
             social_account.auto_complete_info
--- lib/import_kols/complete_kol_info.rb
+++ lib/import_kols/complete_kol_info.rb
@@ -6,7 +6,7 @@ module ImportKols
         #   kol.delete
         # else
           kol.social_accounts.each do |social_account|
-            kol.mobile_number = 10000000000 + kol.id if kol.mobile_number.blank?
+            kol.mobile_number = 10000000000 + kol.id      if kol.kol_role == 'mcn_big_v' && kol.mobile_number.blank?
             kol.name = social_account.username            if kol.name.blank?   && social_account.username.present?
             kol.avatar_url = social_account.avatar_url    if kol.avatar_url.blank?   && social_account.avatar_url.present?
             kol.desc = social_account.brief               if (kol.desc.blank?  rescue true)
