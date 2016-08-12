307e700483e83b34f1e9bdb6c64c768b6d891d69
huxl <huxl@huxldeMacBook-Pro.local>
2016-08-12 16:25:05 +0800

    fix

--- app/models/concerns/kol_campaign.rb
+++ app/models/concerns/kol_campaign.rb
@@ -146,7 +146,11 @@ module Concerns
     end
 
     def sync_campaigns
-      return if self.kol_role != 'public'
+      if Rails.env.produciton?
+        return if self.kol_role == 'mcn_big_v'
+      else
+        return if self.kol_role != 'big_v'
+      end
       Campaign.where(:status => [:agreed, :executing]).each do |campaign|
         next if campaign.specified_kol_targets.size > 0
         if campaign.is_recruit_type?
