18ef4708da53b9142d1bc26452095be2d026deae
huxl <huxl@huxldeMacBook-Pro.local>
2016-08-12 10:59:05 +0800

    improve

--- app/models/kol.rb
+++ app/models/kol.rb
@@ -80,6 +80,7 @@ class Kol < ActiveRecord::Base
   scope :ios, ->{ where("app_platform = 'IOS'") }
   scope :by_date, ->(date){where("created_at > '#{date.beginning_of_day}' and created_at < '#{date.end_of_day}' ") }
   scope :order_by_hot, ->{order("is_hot desc, created_at desc")}
+  scope :order_by_created, ->{order("created_at desc")}
   scope :big_v, ->{ where("kol_role = 'mcn_big_v' or kol_role = 'big_v'") }
 
   before_save :set_kol_kol_role
