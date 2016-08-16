8c0b11ce65737e6025ded50a8dee5bb8b106dafa
huxl <huxl@huxldeMacBook-Pro.local>
2016-08-16 17:25:50 +0800

    fix

--- app/models/concerns/kol_campaign.rb
+++ app/models/concerns/kol_campaign.rb
@@ -147,11 +147,11 @@ module Concerns
     end
 
     def sync_campaigns
-      if Rails.env.production?
-        return if self.kol_role == 'mcn_big_v'
-      else
-        return if self.kol_role != 'big_v'
-      end
+      # if Rails.env.production?
+      #   return if self.kol_role == 'mcn_big_v'
+      # else
+      #   return if self.kol_role != 'big_v'
+      # end
       Campaign.where(:status => [:agreed, :executing]).each do |campaign|
         next if campaign.specified_kol_targets.size > 0
         self.add_campaign_id(campaign.id)  if campaign.newbie_kol_target.present?
