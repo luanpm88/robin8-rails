783405154743d34390d7fbcf5a49c330c47a7be4
jyfcrw <jyfcrw@gmail.com>
2016-08-22 18:28:32 +0800

    调整运营后台截图管理

--- app/controllers/marketing_dashboard/campaign_invites_controller.rb
+++ app/controllers/marketing_dashboard/campaign_invites_controller.rb
@@ -2,73 +2,42 @@ class MarketingDashboard::CampaignInvitesController < MarketingDashboard::BaseCo
   before_action :set_campaign_invite, only: [:pass, :reject]
 
   def index
-    base_invites = CampaignInvite.where.not(:screenshot => '')
-    if search_key
-      if params[:search_helper][:item_type] == "kol"
-        kol = Kol.where("id=? or mobile_number=?", params[:search_helper][:key], params[:search_helper][:key]).first
-        if kol
-          base_invites = base_invites.where(:kol_id => kol.id)
-        end
-      elsif params[:search_helper][:item_type] == "campaign"
-        base_invites = base_invites.where(:campaign_id => params[:search_helper][:key])
-      end
-    end
-    
-    if params[:campaign_id]
-      @campaign_invites = Campaign.find(params[:campaign_id]).campaign_invites.order('created_at DESC').paginate(paginate_params)
-    else
-      @campaign_invites = base_invites.order('created_at DESC').paginate(paginate_params)
-    end
+    @campaign_invites = CampaignInvite.where.not(:screenshot => '')
+    @campaign_invites = @campaign_invites.where(campaign_id: params[:campaign_id]) if params[:campaign_id]
+
+    @q = @campaign_invites.ransack(params[:q])
+    @campaign_invites = @q.result.order('created_at DESC').paginate(paginate_params)
   end
 
   def pending
+    @campaign_invites = CampaignInvite.where("screenshot is not NULL").where(:img_status => :pending)
     # valid_period = Campaign::SettleWaitTimeForBrand
-    # @campaign_invites = CampaignInvite.joins(:campaign, :kol).where.not(:screenshot => "").where(:img_status => :pending).where(:status => ['approved', 'finished']).where("campaigns.deadline > ?", Time.now-valid_period).paginate(paginate_params)
-    base_invites = CampaignInvite.where.not(:screenshot => '').where(:img_status => :pending)
-    if search_key
-      if params[:search_helper][:item_type] == "kol"
-        kol = Kol.where("id=? or mobile_number=?", params[:search_helper][:key], params[:search_helper][:key]).first
-        if kol
-          base_invites = base_invites.where(:kol_id => kol.id)
-        end
-      elsif params[:search_helper][:item_type] == "campaign"
-        base_invites = base_invites.where(:campaign_id => params[:search_helper][:key])
-      end
-    end
+    # @campaign_invites = CampaignInvite.joins(:campaign, :kol)
+    #   .where.not(:screenshot => "").where(:img_status => :pending)
+    #   .where(:status => ['approved', 'finished'])
+    #   .where("campaigns.deadline > ?", Time.now-valid_period)
+    #   .paginate(paginate_params)
+    @campaign_invites = @campaign_invites.where(:kol_id => params[:kol_id]) if params[:kol_id]
 
-    if params[:kol_id]
-      base_invites = base_invites.where(:kol_id => params[:kol_id])
-    end
-    params[:per_page] = 12
-    if params[:per_action_type] == "post"
-      base_invites = base_invites.joins(:campaign).where("per_budget_type='post'")
-    end
+    @q = @campaign_invites.ransack(params[:q])
+    @campaign_invites = @q.result.order('created_at DESC').paginate(paginate_params)
 
-    if params[:observer_status].to_i == 2
-      @campaign_invites = base_invites.where(:observer_status => 2).where("screenshot is not NULL").order('created_at DESC').paginate(paginate_params)
-    elsif params[:observer_status].to_i == 1
-      @campaign_invites = base_invites.where(:observer_status => 1).where("screenshot is not NULL").order('created_at DESC').paginate(paginate_params)
-    elsif params[:total_click]  and params[:total_click].to_i == 1 
-      campaign_ids = Campaign.where(:status => "executed").map(&:id)
-      base_invites = CampaignInvite.where.not(:screenshot => '').where(:img_status => :pending).where(:campaign_id => campaign_ids)
-      @campaign_invites = base_invites.where(:total_click => 0).where("screenshot is not NULL").order('created_at DESC').paginate(paginate_params)
-    elsif params[:total_click]  and params[:total_click].to_i == 100
-      @campaign_invites = CampaignInvite.where("total_click > 100").where("screenshot is not NULL").order('created_at DESC').paginate(paginate_params)
-      render :index and return
-    else
-      @campaign_invites = base_invites.where("screenshot is not NULL").order('created_at DESC').paginate(paginate_params)
-    end
-    render :pending_new
+    render :index
   end
 
   def passed
-    @campaign_invites = CampaignInvite.where(:img_status => 'passed').order('created_at DESC').paginate(paginate_params)
+    @campaign_invites = CampaignInvite.where(:img_status => 'passed')
+    @q = @campaign_invites.ransack(params[:q])
+    @campaign_invites = @q.result.order('created_at DESC').paginate(paginate_params)
 
     render 'index'
   end
 
   def rejected
-    @campaign_invites = CampaignInvite.where(:img_status => 'rejected').order('created_at DESC').paginate(paginate_params)
+    @campaign_invites = CampaignInvite.where(:img_status => 'rejected')
+    @q = @campaign_invites.ransack(params[:q])
+    @campaign_invites = @q.result.order('created_at DESC').paginate(paginate_params)
+
     render 'index'
   end
 
