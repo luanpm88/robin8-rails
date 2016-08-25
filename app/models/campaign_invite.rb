d2ab41ff305a5f4d95c3de06442c7aac2e9d3884
AndyHu <meeasyhappy@gmail.com>
2016-08-25 18:23:23 +0800

    No message.

--- app/models/campaign_invite.rb
+++ app/models/campaign_invite.rb
@@ -9,7 +9,7 @@ class CampaignInvite < ActiveRecord::Base
 
 
   STATUSES = ['pending', 'running', 'applying', 'approved', 'finished', 'rejected', "settled"]
-  CommonRejectedReason = ["不在朋友圈/该条信息详细页", "截图不完整", "不足30分钟", "评论涉嫌欺诈", "含有诱导点击文字", "分组可见", "朋友圈过多悬赏活动，影响效果", "系统检测到有作弊嫌疑", "一次点击都没有"]
+  CommonRejectedReason = ["朋友圈截图错误", "朋友圈截图不完整", "活动保留时间不足30分钟", "活动评论有诱导嫌疑", "活动含诱导点击文字", "活动被设置分组了","朋友圈过多活动影响效果","系统检测到有作弊嫌疑","活动一次点击都没有"]
   # observer_status 0 表示 未计算, 1 表示 正常, 2 表示 存在作弊嫌疑
   ImgStatus = ['pending','passed', 'rejected']
   OcrStatus = ['pending', 'passed','failure']
