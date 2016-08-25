64b89e4dd74a92f0891eb63c8391e45cef43e0e5
AndyHu <meeasyhappy@gmail.com>
2016-08-25 17:25:10 +0800

    创建文章

--- lib/api/v1_7/carticles.rb
+++ lib/api/v1_7/carticles.rb
@@ -2,6 +2,9 @@ module API
   module V1_7
     class Carticles < Grape::API
       resources :carticles do
+        before do
+          authenticate!
+        end
         #获取活动素材
         params do
           optional :content, type: String
@@ -9,7 +12,7 @@ module API
         post '/' do
           article = Carticle.new(:body => params[:content])
           article.save
-          
+          present :article, article, with: API::V1_7::Entities::Carticles::Detail
         end
       end
     end
