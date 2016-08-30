bc4834986c8f031588482dc73f3b82b1d55f6786
james <tardis54@163.com>
2016-08-30 10:03:02 +0800

    修改新建推广活动选择页面文案

--- client/app/bundles/Robin8/components/SelectCampaignPartial.jsx
+++ client/app/bundles/Robin8/components/SelectCampaignPartial.jsx
@@ -26,7 +26,8 @@ class SelectCampaignPartial extends Component {
                 <p>KOL分享文章到朋友圈后，</p>
                 <p>[CPC]按照好友有效点击数付费</p>
                 <p>[CPP]按照KOL转发一次性付费</p>
-                <p>[CPA]按照KOL分享后效果付费</p>
+                <p>[CPA]按照好友点击指定链接付费</p>
+                <p>[CPI]按照KOL下载APP一次性付费</p>
               </div>
               <div className="at-box-bt at-box-bt-left">
                 <Link to="/brand/campaigns/new" className="btn">
