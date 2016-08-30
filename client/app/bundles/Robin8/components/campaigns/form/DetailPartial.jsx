0a8724eb6be89032c868cf9c4b0cf813cf36d973
james <tardis54@163.com>
2016-08-30 17:49:50 +0800

    修改奖励模式选择样式并增加simple_cpi最小金额验证

--- client/app/bundles/Robin8/components/campaigns/form/DetailPartial.jsx
+++ client/app/bundles/Robin8/components/campaigns/form/DetailPartial.jsx
@@ -56,31 +56,18 @@ export default class DetailPartial extends React.Component {
 
   _listenPerBudgetTypeChange() {
     $("input[name='action_type']").change(function(){
-      const { per_action_budget, per_budget_type, per_budget_collect_type } = this.props;
+      const { per_action_budget, per_budget_type } = this.props;
       if(per_budget_type.value == 'post') {
         per_action_budget.onChange("2.0")
       }
       if(per_budget_type.value == 'click') {
         per_action_budget.onChange("0.2")
       }
+      if(per_budget_type.value == 'simple_cpi') {
+        per_action_budget.onChange("2.0")
+      }
       per_action_budget.onFocus();
       per_action_budget.onBlur();
-
-      // console.log("变化后的 %s")
-      // console.log(per_budget_type.value)
-      // if(per_budget_type.value == "post" || per_budget_type.value == "click"){
-      //   per_budget_collect_type.onChange('');
-      // }else{
-      //   per_budget_collect_type.onChange('cpa_cpi');
-      // }
-    }.bind(this))
-
-
-    $("input[name='cap_cpi-collect-action_type']").click(function(){
-      const { per_action_budget, per_budget_type, per_budget_collect_type } = this.props;
-      if(per_budget_type.value == "post" || per_budget_type.value == "click"){
-        per_budget_type.onChange("cpa");
-      }
     }.bind(this))
 
     $(".commonPerBudgetType").click(function(){
@@ -105,12 +92,11 @@ export default class DetailPartial extends React.Component {
                  <p>2.&nbsp;按照点击奖励KOL: KOL分享后按照好友有效点击数付费。\
                  <p>3.&nbsp;按照活动效果奖励KOL: KOL分享后按照活动效果付费。\
                  "
-                 //<p>3.&nbsp;按照行动奖励KOL: KOL必须完成指定的操作流程才可获得奖励，例如点击长文中的某个链接等等。
     return tip
   }
 
   render() {
-    const { per_budget_type, per_budget_collect_type, action_url, action_url_identifier, short_url, per_action_budget} = this.props
+    const { per_budget_type, action_url, action_url_identifier, short_url, per_action_budget} = this.props
 
     return (
       <div className="creat-activity-form creat-content-sources">
@@ -131,28 +117,20 @@ export default class DetailPartial extends React.Component {
                 <input {...per_budget_type} type="radio" name="action_type" className="commonPerBudgetType" value="post" onChange={per_budget_type.onChange} checked={per_budget_type.value === "post"} />
                 按照转发奖励KOL
               </label>
-
+            </div>
+            <div className="sources-check radio">
               <label>
-                <input {...per_budget_collect_type} type="radio" className="cap_cpi-collect-action_type" name="cap_cpi-collect-action_type" value="cpa_cpi" onChange={per_budget_collect_type.onChange} checked={(per_budget_type.value === "cpa" || per_budget_type.value === "simple_cpi" || per_budget_collect_type.value === "cpa_cpi")} />
-                按照活动效果奖励KOL
+                <input {...per_budget_type} type="radio" name="action_type" value="simple_cpi" onChange={per_budget_type.onChange} checked={per_budget_type.value === "simple_cpi"} />
+                按照下载APP奖励KOL
               </label>
 
+              <label className="cpa-label" >
+                <input {...per_budget_type} type="radio" name="action_type" value="cpa" onChange={per_budget_type.onChange} checked={per_budget_type.value === "cpa"} />
+                按照点击指定链接奖励KOL
+              </label>
             </div>
 
-            <div className="action-url-group" style={ (per_budget_collect_type.value == "cpa_cpi" || (per_budget_type && (per_budget_type.value == 'simple_cpi' || per_budget_type.value == 'cpa'))) ? {display: 'block'} : {display: 'none'} }>
-              <div className="cpa-cpi-select-img">
-                <img src={require("cpa-cpi-background.png")} />
-              </div>
-              <div className="sources-check cpa-cpi-select radio">
-                <label>
-                    <input {...per_budget_type} type="radio" name="action_type" value="cpa" onChange={per_budget_type.onChange} checked={per_budget_type.value === "cpa"} />
-                  按链接点击次数付费(仅支持链接类型)
-                </label>
-                <label>
-                    <input {...per_budget_type} type="radio" name="action_type" value="simple_cpi" onChange={per_budget_type.onChange} checked={per_budget_type.value === "simple_cpi"} />
-                  按下载次数付费(用于APP推广)
-                </label>
-              </div>
+            <div className="action-url-group" style={(per_budget_type && (per_budget_type.value == 'simple_cpi' || per_budget_type.value == 'cpa')) ? {display: 'block'} : {display: 'none'} }>
               {
                 do {
                   if(per_budget_type.value == "cpa"){
