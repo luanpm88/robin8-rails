4921688a3bc56c888c3fb9c6759f4a46b1190205
james <tardis54@163.com>
2016-09-05 14:54:46 +0800

    添加engine migration

--- db/migrate/20160905063105_create_crm_sellers.crm.rb
+++ db/migrate/20160905063105_create_crm_sellers.crm.rb
@@ -1,16 +1,16 @@
 # This migration comes from crm (originally 20160831083847)
 class CreateCrmSellers < ActiveRecord::Migration
   def change
-    # create_table :crm_sellers do |t|
-    #   t.string :mobile_number
-    #   t.string :password_digest
-    #   t.string :name
-    #   t.string :department
-    #   t.string :avatar
-    #   t.string :invite_code
-    #   t.string :private_token
-    # 
-    #   t.timestamps null: false
-    # end
+    create_table :crm_sellers do |t|
+      t.string :mobile_number
+      t.string :password_digest
+      t.string :name
+      t.string :department
+      t.string :avatar
+      t.string :invite_code
+      t.string :private_token
+
+      t.timestamps null: false
+    end
   end
 end
--- db/migrate/20160905063109_add_ch_name_to_crm_seller.crm.rb
+++ db/migrate/20160905063109_add_ch_name_to_crm_seller.crm.rb
@@ -1,6 +1,6 @@
 # This migration comes from crm (originally 20160905023516)
 class AddChNameToCrmSeller < ActiveRecord::Migration
   def change
-    # add_column :crm_sellers, :ch_name, :string
+    add_column :crm_sellers, :ch_name, :string
   end
 end
