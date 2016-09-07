0c5c2a100c548a755145f82e6031959e9703023e
james <tardis54@163.com>
2016-09-07 14:50:41 +0800

    线下充值添加销售人员

--- app/controllers/marketing_dashboard/manual_recharges_controller.rb
+++ app/controllers/marketing_dashboard/manual_recharges_controller.rb
@@ -9,4 +9,14 @@ class MarketingDashboard::ManualRechargesController < MarketingDashboard::BaseCo
     @q = @transactions.ransack(params[:q])
     @transactions = @q.result.order('created_at DESC').paginate(paginate_params)
   end
+
+  def add_seller
+    @transaction = Transaction.find(params[:id])
+    if request.get?
+      render :add_seller
+    else
+      @transaction.account.update(seller_id: params[:seller_id])
+      redirect_to :back, notice: '添加成功'
+    end
+  end
 end
