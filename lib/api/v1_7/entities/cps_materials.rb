e4a089c55c8078cd84b4932ec986612d82ec4f88
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-07 16:41:13 +0800

    fix

--- lib/api/v1_7/entities/cps_materials.rb
+++ lib/api/v1_7/entities/cps_materials.rb
@@ -3,7 +3,7 @@ module API
     module Entities
       module CpsMaterials
         class Summary < Grape::Entity
-          expose :sku_id, :img_url, :material_url, :goods_name, :shop_id, :unit_price, :start_date, :end_date,
+          expose :id, :sku_id, :img_url, :material_url, :goods_name, :shop_id, :unit_price, :start_date, :end_date,
                  :kol_commision_wl, :category
           expose :category_label do  |material|
             CpsMaterial::Categories[material.category.to_sym]   rescue nil
