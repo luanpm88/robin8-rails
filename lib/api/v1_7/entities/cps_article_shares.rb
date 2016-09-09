577accbdfd278b4bf5a33ea1367c42a4f86a4384
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-09 19:04:23 +0800

    fix

--- lib/api/v1_7/entities/cps_article_shares.rb
+++ lib/api/v1_7/entities/cps_article_shares.rb
@@ -8,7 +8,15 @@ module API
           with_options(format_with: :iso_timestamp) do
             expose :created_at
           end
-          expose :kol, using: API::V1::Entities::KolEntities::InviteeSummary
+          expose :kol_id do
+            kol.id
+          end
+          expose :kol_name do
+            kol.name
+          end
+          expose :kol_avatar_url do
+            kol.avatar_url
+          end
         end
 
         class Summary < ForList
