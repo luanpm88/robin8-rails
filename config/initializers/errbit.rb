4498f295dd38cc101807f5b33c3c79098491d02b
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-09 11:18:23 +0800

    fix

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,10 +2,8 @@ if Rails.env.staging?
   Airbrake.configure do |config|
     config.project_key = 'dee3de294163aac01d95fd126150626f'
     config.project_id = -1
-    config.host = 'bug.robin8.net'
-    config.port = 80
+    config.host = 'http://bug.robin8.net'
     config.secure = config.port == 443
-    config.ignore << 'ActionController::InvalidCrossOriginRequest'
   end
 end
 
@@ -13,9 +11,7 @@ if Rails.env.production?
   Airbrake.configure do |config|
     config.project_key = '0dc2bc446c92ca946389c5b7da71895f'
     config.project_id = -1
-    config.host = 'bug.robin8.net'
-    config.port = 80
+    config.host = 'http://bug.robin8.net'
     config.secure = config.port == 443
-    config.ignore << 'ActionController::InvalidCrossOriginRequest'
   end
 end
