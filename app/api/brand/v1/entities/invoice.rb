59425cc5b454f4df2bc7c5f29bb7bd87b8762172
james <tardis54@163.com>
2016-09-09 16:46:31 +0800

    fix bug

--- app/api/brand/v1/entities/invoice.rb
+++ app/api/brand/v1/entities/invoice.rb
@@ -2,7 +2,7 @@ module Brand
   module V1
     module Entities
       class Invoice < Entities::Base
-        expose :title, :invoice_type, :taxpayer_id, :company_name, :company_address, :company_mobile, :bank_name,
+        expose :title, :invoice_type, :taxpayer_id, :company_address, :company_mobile, :bank_name,
                :bank_account
       end
     end
