63368c07a2f90e4f239570c69fa1bc7e2339b561
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-12 14:02:03 +0800

    fix

--- lib/api/v1_7/entities/cps_articles.rb
+++ lib/api/v1_7/entities/cps_articles.rb
@@ -6,8 +6,15 @@ module API
           expose :id, :title, :cover, :content, :show_url, :status, :check_remark, :material_total_price,
                  :writing_forecast_commission, :writing_settled_commission
           expose :author, using: API::V1::Entities::KolEntities::InviteeSummary
+          expose :cps_article_share_count do |cps_article|
+            cps_article.cps_article_shares.count
+          end
+        end
+
+        class WithShareDetail < Summary
           expose :cps_article_shares, using: API::V1_7::Entities::CpsArticleShares::Summary
         end
+
       end
     end
   end
