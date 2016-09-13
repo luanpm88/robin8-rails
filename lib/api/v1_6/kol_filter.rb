6b9bf21587e61635f08d828ea0e552a8713c8deb
james <tardis54@163.com>
2016-09-13 14:33:48 +0800

    improve kol filter

--- lib/api/v1_6/kol_filter.rb
+++ lib/api/v1_6/kol_filter.rb
@@ -9,10 +9,20 @@ module API
         get '/kol_count' do
           @kols = Kol.active.personal_big_v
           if params[:region] && params[:region] != "全部"
-            @kols = @kols.ransack({app_city_in: ['nanjing', 'shanghai']}).result
+            cities = params[:region].split(',').collect { |name| ::City.where("name like '#{name}%'").first.name_en }
+            @kols = @kols.ransack({app_city_in: cities}).result
           end
           if params[:tags] && params[:tags] != '全部'
-            @kols = @kols.ransack({kol_tags_tag_id_in: [1, 2]}).result
+            tags = params[:tags].split(',').collect { |name| ::Tag.get_name_by_label(name) }
+            @kols = @kols.ransack({kol_tags_tag_id_in: tags}).result
+          end
+
+          if params[:age] && params[:age] != '全部'
+            @kols = @kols.ransack({age_in: params[:age].split(',').map(&:to_i)}).result
+          end
+
+          if params[:gender] && params[:gender] != '全部'
+            @kols = @kols.ransack({gender_eq: params[:gender].to_i}).result
           end
 
           present error: 0
