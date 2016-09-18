40fb93deb435171c68123d31268ce9f561109ea5
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-18 11:29:42 +0800

    fix

--- lib/api/v1/entities/campaign_entities.rb
+++ lib/api/v1/entities/campaign_entities.rb
@@ -12,7 +12,7 @@ module API
               campaign.per_budget_type
             end
           end
-          expose :per_action_type do |campaign|
+          expose :per_action_type, :sub_sub_message_type do |campaign|
             if campaign.is_cpi_type?
               'cpi'
             else
