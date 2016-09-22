0c0e47519714b5499baee54cc172230274371d67
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-22 15:57:08 +0800

    优化代码

--- app/controllers/campaign_show_controller.rb
+++ app/controllers/campaign_show_controller.rb
@@ -44,17 +44,10 @@ class CampaignShowController < ApplicationController
     other_options = {}
     other_options[:step] = (uuid_params[:step] || 1).to_i
 
-    if ["unpay", "unexecute", "agreed"].include?(@campaign.status)
-      if other_options[:step] == 1
-        redirect_to @campaign.url
-      else
-        @campaign_action_url = CampaignActionUrl.find_by :identifier => uuid_params[:campaign_action_url_identifier]
-        redirect_to (@campaign_action_url.try(:action_url) || "http://robin8.net")
-      end
-      return
+    unless ["unpay", "unexecute", "agreed"].include?(@campaign.status)
+      CampaignShowWorker.perform_async(params[:uuid], cookies[:_robin8_visitor], request.remote_ip, request.user_agent, request.referer, request.env['HTTP_X_FORWARDED_FOR'], request.url, openid, other_options)
     end
 
-    CampaignShowWorker.perform_async(params[:uuid], cookies[:_robin8_visitor], request.remote_ip, request.user_agent, request.referer, request.env['HTTP_X_FORWARDED_FOR'], request.url, openid, other_options)
     if other_options[:step] == 1
       redirect_to @campaign.url
     else
