380e80ad8a219aefc1e6493ed667384926ba0920
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-22 15:27:24 +0800

    更改商品库

---
+++ db/migrate/20160922061633_add_column_to_cps_materials.rb
@@ -0,0 +1,7 @@
+class AddColumnToCpsMaterials < ActiveRecord::Migration
+  def change
+    add_column :cps_materials, :is_hot, :boolean, :default => false
+
+    CpsMaterial.update_all(:is_hot, true)
+  end
+end
