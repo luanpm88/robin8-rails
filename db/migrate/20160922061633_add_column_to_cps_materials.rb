7081358e06966319185d8198cde4fc6b0dbb3b67
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-22 15:31:49 +0800

    revert

--- db/migrate/20160922061633_add_column_to_cps_materials.rb
+++ db/migrate/20160922061633_add_column_to_cps_materials.rb
@@ -1,6 +1,6 @@
 class AddColumnToCpsMaterials < ActiveRecord::Migration
   def change
-    # add_column :cps_materials, :is_hot, :boolean, :default => false
+    add_column :cps_materials, :is_hot, :boolean, :default => false
 
     CpsMaterial.update_all(:is_hot => true)
   end
