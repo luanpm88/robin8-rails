67c47f5b4ee9e35b8082491f04802be10123925f
huxl <huxl@huxldeMacBook-Pro.local>
2016-09-26 12:18:40 +0800

    add end_date for cps_artciles

--- lib/api/v1_7/entities/cps_articles.rb
+++ lib/api/v1_7/entities/cps_articles.rb
@@ -4,7 +4,7 @@ module API
       module CpsArticles
         class Summary < Grape::Entity
           expose :id, :title, :cover, :content, :show_url, :status, :check_remark, :material_total_price,
-                 :writing_forecast_commission, :writing_settled_commission
+                 :writing_forecast_commission, :writing_settled_commission, :end_date
           expose :author, using: API::V1::Entities::KolEntities::InviteeSummary
           expose :cps_article_share_count do |cps_article|
             cps_article.cps_article_shares.count
