8c08ca9139ae9078ddc0513fd3045cad5e2e17de
jyfcrw <jyfcrw@gmail.com>
2016-10-18 16:09:13 +0800

    fix bug

--- app/models/concerns/campaigns/campaign_invite_analysis.rb
+++ app/models/concerns/campaigns/campaign_invite_analysis.rb
@@ -67,7 +67,7 @@ module Campaigns
           province_name: province.name,
           province_code: province.name_en
         }
-      end.sort_by { |c| c[:province_code] }.compact    rescue nil
+      end.sort_by { |c| c[:province_code] }.compact rescue []
     end
   end
 end
