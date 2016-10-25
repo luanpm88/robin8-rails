eeb5231c776e4c3e8606bdc2fb3edde67e95a4ed
jyfcrw <jyfcrw@gmail.com>
2016-10-25 12:32:40 +0800

    fix bug

--- app/models/province.rb
+++ app/models/province.rb
@@ -5,4 +5,7 @@ class Province < ActiveRecord::Base
   has_many :cities, dependent: :destroy
   has_many :districts, through: :cities
 
+  def short_name
+    @short_name ||= name.gsub(/市|省|回族自治区|壮族自治区|维吾尔自治区|自治区|特别行政区/, '')
+  end
 end
