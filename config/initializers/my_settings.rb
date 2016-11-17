8683446078516615936d631b7ae9bf0ebb2d7b25
huxl <huxl@huxldeMacBook-Pro.local>
2016-11-17 12:36:19 +0800

    comment

--- config/initializers/my_settings.rb
+++ config/initializers/my_settings.rb
@@ -1,2 +1,2 @@
-MySettings.campaign_min_budget = 300  if MySettings.campaign_min_budget.blank?
-MySettings.recharge_min_budget = 3000  if MySettings.recharge_min_budget.blank?
+# MySettings.campaign_min_budget = 300  if MySettings.campaign_min_budget.blank?
+# MySettings.recharge_min_budget = 3000  if MySettings.recharge_min_budget.blank?
