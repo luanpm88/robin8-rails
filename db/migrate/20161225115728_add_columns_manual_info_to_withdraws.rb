92b9054e092a431a8f95d1b3575342d08a62db05
huxl <huxl@huxldeMacBook-Pro.local>
2016-12-26 15:23:40 +0800

    fix

--- db/migrate/20161225115728_add_columns_manual_info_to_withdraws.rb
+++ db/migrate/20161225115728_add_columns_manual_info_to_withdraws.rb
@@ -2,5 +2,7 @@ class AddColumnsManualInfoToWithdraws < ActiveRecord::Migration
   def change
     add_column :withdraws, :user_id, :integer
     add_column :withdraws, :operate_admin, :string
+
+    change_column :withdraws, :credits, :float
   end
 end
