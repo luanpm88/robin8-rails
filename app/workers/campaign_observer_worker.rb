6ea4e6c6bb3b8b7dda6d7c9f7a07a7353e8b8995
Bart <bartlomiej.grasza@gmail.com>
2017-05-18 17:10:39 +0800

    small improvements to CampaignObserver

--- app/workers/campaign_observer_worker.rb
+++ app/workers/campaign_observer_worker.rb
@@ -3,9 +3,17 @@ class CampaignObserverWorker
   sidekiq_options  :queue => :campaign_observer_worker
 
   def perform(campaign_id)
+    campaign = Campaign.where(:id => campaign_id, :status => ['executed','settled']).first rescue nil
+    if campaign
+      campaign_invite campaign_id
+    else
+      ::NewRelic::Agent.record_metric("CampaignObserverWorker / campaign_id:#{campaign_id} not found. Worker not running", campaign_id)
+      return nil
+    end
+  end
+
+  def campaign_invite campaign_id
     CampaignInvite.where(:campaign_id => campaign_id).map do |invite|
-      campaign = Campaign.where(:id => invite.campaign_id, :status => ['executed','settled']).first
-      next unless campaign
       if invite.observer_status != 0
         next
       end
