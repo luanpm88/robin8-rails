dadebd74ba3424b41f871af5930de1e0194437b5
Bart <bartlomiej.grasza@gmail.com>
2017-05-24 11:18:38 +0800

    added ability to deploy to qa server from different git branch

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -17,7 +17,7 @@
 
 
 server '139.196.36.27', user: 'deployer', roles: %w{web app db master}
-set :branch, 'qa'
+set :branch, ENV['BRANCH_NAME'] || 'qa'
 set :server_name, 'qa.robin8.net'
 
 set :stage, 'qa'
