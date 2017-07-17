cf784a4a2055b22d9d70e74b8215339f62127180
Bart <bartlomiej.grasza@gmail.com>
2017-07-17 18:09:53 +0800

    changes in talking data /login api endopoint

--- app/api/property/v1/talking_data.rb
+++ app/api/property/v1/talking_data.rb
@@ -4,12 +4,22 @@ module Property
       resource :talking_data do
         params do
           requires :channel_id, type: String
-          requires :idfa, type: String
+          optional :idfa, type: String
+          optional :imei, type: String
+          at_least_one_of :idfa, :imei
+          # requires :idfa, type: String
         end
         get '/login' do
-          kol = Kol.find_by IDFA: params[:idfa]
-          if kol and !kol.talkingdata_channel_id
-            kol.update_attributes(talkingdata_channel_id: params[:channel_id])
+          if params[:idfa]
+            kol = Kol.find_by IDFA: params[:idfa]
+            if kol and !kol.talkingdata_channel_id
+              kol.update_attributes(talkingdata_channel_id: params[:channel_id])
+            end
+          else
+            kol = Kol.find_by IMEI: params[:imei]
+            if kol and !kol.talkingdata_channel_id
+              kol.update_attributes(talkingdata_channel_id: params[:channel_id])
+            end
           end
           present :error, 0
         end
