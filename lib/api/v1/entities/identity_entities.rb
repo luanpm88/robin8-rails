d3f19e5f40592c0efb994290d683bed6df86b429
yin <yin@yindeMacBook-Pro.local>
2017-07-25 16:24:59 +0800

    fixed of bind

--- lib/api/v1/entities/identity_entities.rb
+++ lib/api/v1/entities/identity_entities.rb
@@ -3,9 +3,7 @@ module API
     module Entities
       module IdentityEntities
         class Summary < Grape::Entity
-          unless :uid.blank?
             expose :provider, :uid, :token, :name, :url, :avatar_url, :desc 
-          end
         end
       end
     end
