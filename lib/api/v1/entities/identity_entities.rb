02a4c77c4d4c3a162799911a0dd99b7a9decdce1
yin <yin@yindeMacBook-Pro.local>
2017-07-25 15:40:52 +0800

    fixed

--- lib/api/v1/entities/identity_entities.rb
+++ lib/api/v1/entities/identity_entities.rb
@@ -3,7 +3,7 @@ module API
     module Entities
       module IdentityEntities
         class Summary < Grape::Entity
-          expose :provider, :uid, :token, :name, :url, :avatar_url, :desc  if :token
+          expose :provider, :uid, :token, :name, :url, :avatar_url, :desc 
         end
       end
     end
