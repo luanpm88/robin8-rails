255eb30ab97622b9c7143bcf3cbda09296692cfe
yin <yin@yindeMacBook-Pro.local>
2017-07-25 18:47:19 +0800

    fix foe bind

--- lib/api/v1_6/entities/social_account_entities.rb
+++ lib/api/v1_6/entities/social_account_entities.rb
@@ -3,11 +3,10 @@ module API
     module Entities
       module SocialAccountEntities
         class Summary < Grape::Entity
-          if username
-            expose :id, :provider, :uid, :username, :homepage, :avatar_url, :brief, :followers_count, :friends_count, :like_count, :reposts_count, :statuses_count, :price, :search_kol_id
-            expose :provider_name do |social_account|
-              SocialAccount::Providers[social_account.provider]
-            end
+          expose :id, :provider, :uid, :username, :homepage, :avatar_url, :brief, :followers_count, 
+                 :friends_count, :like_count, :reposts_count, :statuses_count, :price, :search_kol_id, unless: :username.blank?
+          expose :provider_name do |social_account|
+            SocialAccount::Providers[social_account.provider]
           end
         end
       end
