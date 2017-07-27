3713b374d51df8f80acf22588206f24cc7c3f98f
yin <yin@yindeMacBook-Pro.local>
2017-07-27 15:33:09 +0800

    fix bug of unbind_social_account

--- lib/api/v1_6/big_v.rb
+++ lib/api/v1_6/big_v.rb
@@ -95,7 +95,7 @@ module API
           kol = Kol.find(params[:kol_id]) rescue nil
           if social_account and social_account.kol_id == kol.id
             unbind_record = BindRecord.find_by(:kol_id => params[:kol_id] , :provider =>  params[:provider])
-            if unbind_timestamp.unbind_count == true
+            if unbind_record.unbind_count == true
               social_account.delete
               unbind_record.update( :unbind_at => Time.now , :unbind_count => false)
               present :error, 0
