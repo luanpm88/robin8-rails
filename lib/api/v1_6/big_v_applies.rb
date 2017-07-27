0acb14327b1e7e3699f52cd087bccd90f0db20ef
yin <yin@yindeMacBook-Pro.local>
2017-07-27 14:08:39 +0800

    fix bug of update_social

--- lib/api/v1_6/big_v_applies.rb
+++ lib/api/v1_6/big_v_applies.rb
@@ -51,7 +51,7 @@ module API
           provider = SocialAccount::Providers.invert[params[:provider_name]]
           bind_record = BindRecord.find_by(:kol_id => current_kol.id , :provider => provider)
           bind_count = bind_record.bind_count
-          if unbind_record.bind_count > 0
+          if bind_count > 0
             social_account = SocialAccount.find_or_initialize_by(:kol_id => current_kol.id, :provider => provider)
             social_account.homepage = params[:homepage]  if params[:homepage].present?
             if provider == 'weibo' && social_account.homepage.blank?
@@ -68,7 +68,7 @@ module API
             social_account.save
             current_kol.update_columns(:role_apply_status => 'applying', :role_apply_time => Time.now)   if current_kol.is_big_v?
             bind_count = bind_count - 1
-            unbind_record.update(:bind_count => bind_count)
+            bind_record.update(:bind_count => bind_count)
             present :error, 0
           else
             return error_403!({erroe: 1, detail: '本月无法再次绑定'})
