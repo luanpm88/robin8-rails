50204e0452ec807a33996f7270873728c633d0c9
Bart <bartlomiej.grasza@gmail.com>
2017-08-07 17:52:22 +0800

    added wechat campaigns allow to login for exising KOLs

--- app/controllers/wechat_campaign_controller.rb
+++ app/controllers/wechat_campaign_controller.rb
@@ -3,8 +3,6 @@ class WechatCampaignController < ApplicationController
   before_action :set_campaign, only: [:campaign_page, :kol_register, :campaign_details]
 
   def campaign_page
-    # TODO: different message for non-executing campaigns
-    # TODO: test on non-'executing' campaign
     render :layout => false
   end
 
@@ -21,8 +19,8 @@ class WechatCampaignController < ApplicationController
     mobile_number = params[:mobile_number]
     return render json: {error: "必须填写手机号"} if mobile_number.blank?
 
-    kol_exists = Kol.find_by(mobile_number: mobile_number)
-    return render json: {error: "Kol already exists"} if kol_exists
+    # kol_exists = Kol.find_by(mobile_number: mobile_number)
+    # return render json: {error: "用户已注册"} if kol_exists #Kol already exists
 
     total_send_key = "wechat_campaign_kol_register_send_sms_count"
     send_count =  Rails.cache.fetch(total_send_key).to_i || 1
@@ -44,29 +42,45 @@ class WechatCampaignController < ApplicationController
     return render json: {error: "短信验证码错误" } unless verify_code == params[:sms_code]
 
     kol_exists = Kol.find_by(mobile_number: params[:mobile_number])
-    return render json: {error: "Kol already exists"} if kol_exists
+    # return render json: {error: "Kol already exists"} if kol_exists
 
-    ip = (request.remote_ip rescue nil) || request.ip
-    kol = Kol.new(mobile_number: params[:mobile_number],
-                  name: Kol.hide_real_mobile_number(params[:mobile_number]),
-                  current_sign_in_ip: ip)
-
-    if kol.save
+    if kol_exists
       campaign_id = params[:campaign_id]
       campaign = Campaign.find(campaign_id) rescue nil
-      token = tmp_token kol, campaign
+      token = tmp_token kol_exists, campaign
       cookies[:_robin8_wechat_campaign] = token
-      cookies[:_robin8_wechat_campaign_id] = kol.id
-      cache_key = "wechat-campaign-token-#{kol.id}-#{campaign_id}"
+      cookies[:_robin8_wechat_campaign_id] = kol_exists.id
+      cache_key = "wechat-campaign-token-#{kol_exists.id}-#{campaign_id}"
       Rails.cache.write(cache_key, token , :expires_in => 1.hour)
       if campaign and campaign.status == 'executing'
-        kol.add_campaign_id campaign_id
-        kol.approve_campaign(campaign_id)
+        kol_exists.add_campaign_id campaign_id
+        kol_exists.approve_campaign(campaign_id)
       end
       return render json: {url: wechat_campaign_campaign_details_path(campaign_id: campaign_id) }
     else
-      return render json: {error: 'Campaign not found'}
+      ip = (request.remote_ip rescue nil) || request.ip
+      kol = Kol.new(mobile_number: params[:mobile_number],
+                    name: Kol.hide_real_mobile_number(params[:mobile_number]),
+                    current_sign_in_ip: ip)
+
+      if kol.save
+        campaign_id = params[:campaign_id]
+        campaign = Campaign.find(campaign_id) rescue nil
+        token = tmp_token kol, campaign
+        cookies[:_robin8_wechat_campaign] = token
+        cookies[:_robin8_wechat_campaign_id] = kol.id
+        cache_key = "wechat-campaign-token-#{kol.id}-#{campaign_id}"
+        Rails.cache.write(cache_key, token , :expires_in => 1.hour)
+        if campaign and campaign.status == 'executing'
+          kol.add_campaign_id campaign_id
+          kol.approve_campaign(campaign_id)
+        end
+        return render json: {url: wechat_campaign_campaign_details_path(campaign_id: campaign_id) }
+      else
+        return render json: {error: 'Campaign not found'}
+      end
     end
+
   end
 
   def campaign_details
