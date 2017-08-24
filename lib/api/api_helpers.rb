fca29f1b375ec811913f2dd9ef092a4b3b4f7e7c
yin <yin@yindeMacBook-Pro.local>
2017-08-24 11:07:17 +0800

    fix bug

--- lib/api/api_helpers.rb
+++ lib/api/api_helpers.rb
@@ -139,7 +139,7 @@ module API
       campaigns.each do |t|
         target = CampaignTarget.where("campaign_id" => t[:id])
         target.each do |i|
-          filter = 0 unless i.get_matching_kols_ids(current_kol,t[:target_type]).index(current_kol[:id])
+          filter = 0 unless i.get_matching_kol_ids(current_kol,t[:target_type]).index(current_kol[:id])
         end
         campaigns_filter.push(t) if filter = 1
       end
