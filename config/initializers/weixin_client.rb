6909c5e8dc5d99bac989a4ba84bb873530d86477
Bart <bartlomiej.grasza@gmail.com>
2017-09-01 17:17:21 +0800

    testing new redis wechat settings

--- config/initializers/weixin_client.rb
+++ config/initializers/weixin_client.rb
@@ -17,5 +17,12 @@ unless Rails.env.test?
 
   $weixin_client ||= WeixinAuthorize::Client.new(Rails.application.secrets.wechat[:app_id],
                                                  Rails.application.secrets.wechat[:app_secret])
+
   puts "======weixin_client init===#{$weixin_client.is_valid?}"
+  Rails.logger.wechat_campaign.info "======weixin_client init===#{$weixin_client.is_valid?}"
+  Rails.logger.wechat_campaign.info "======weixin_client access_token===#{$weixin_client.access_token}"
+  Rails.logger.wechat_campaign.info "======weixin_client access_token.expired_at===#{$weixin_client.expired_at}"
+  t = $weixin_client.token_store
+  Rails.logger.wechat_campaign.info "======weixin_client redis_key===#{t.client.redis_key}"
+  Rails.logger.wechat_campaign.info "======weixin_client expired_at===#{t.client.expired_at}"
 end
