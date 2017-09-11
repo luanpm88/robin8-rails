42ecb7171fc9c1d0bfde6f5713c0aa5de2ce121b
yin <yin@yindeMacBook-Pro.local>
2017-09-11 18:20:22 +0800

    update

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,13 +1,18 @@
 require 'roo'
+require 'csv'
 begin
   xlsx = Roo::Spreadsheet.open("#{Rails.root}/public/geometry.xlsx")
+  geometry = File.new("#{Rails.root}/public/#{Time.now}geometry.txt","w+")
   phone = xlsx.column(5)
   phone.each do |t|
-	kol = Kol.find_by(mobile_number: t)
-	if kol.present?
-	  kol.admintags << Admintag.find_or_create_by(tag: 'geometry')
-	end
+    geometry.write("#{t},")
+	  kol = Kol.find_by(mobile_number: t)
+	  if kol.present?
+      geometry.write("#{t},")
+	    kol.admintags << Admintag.find_or_create_by(tag: 'geometry')
+	  end
   end
+  geometry.close
   puts "标签打完了哟"
 rescue
   puts "出错,请重试"
