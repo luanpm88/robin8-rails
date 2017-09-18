c4fa8ed72466452615e479074805eeea4a85e282
yin <yin@yindeMacBook-Pro.local>
2017-09-18 15:53:49 +0800

    add new list of geometry

--- lib/excel.rb
+++ lib/excel.rb
@@ -4,8 +4,7 @@ require 'csv'
 
 xlsx = Roo::Spreadsheet.open("#{Rails.root}/public/geometry.xlsx")
 txt = File.open("#{Rails.root}/public/geometry.txt" ,"r").read.split(",")
- a = 0
- phone = xlsx.column(5)
+phone = xlsx.column(5)
 CSV.open("#{Rails.root}/public/123.csv" ,"wb") do |csv|
 	txt.each do |t|
 	  line = phone.index(t.to_i) + 1
@@ -13,3 +12,14 @@ CSV.open("#{Rails.root}/public/123.csv" ,"wb") do |csv|
 	  csv << row
 	end
 end
+
+# xlsx2 = Roo::Spreadsheet.open("#{Rails.root}/public/geometry2.xlsx")
+# txt2 = File.open("#{Rails.root}/public/geometry2.txt" ,"r").read.split(",")
+# kol_name = xlsx2.column(2)
+# CSV.open("#{Rails.root}/public/345.csv" ,"wb") do |csv|
+# 	txt2.each do |t|
+# 	  line = kol_name.index(t.to_i) + 1
+# 	  row = xlsx2.row(line)
+# 	  csv << row
+# 	end
+# end
