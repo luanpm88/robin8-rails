02b538842eea57bb78481096bb1176fe859d688c
yin <yin@yindeMacBook-Pro.local>
2017-10-09 18:04:15 +0800

    add new list

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,6 +1,6 @@
 require 'roo'
 
-# begin
+begin
   xlsx = Roo::Spreadsheet.open("#{Rails.root}/public/geometry.xlsx")
   # geometry = File.new("#{Rails.root}/public/geometry.txt","w+")
   # geometry2 = File.new("#{Rails.root}/public/geometry2.txt","w+")
@@ -22,6 +22,6 @@ require 'roo'
   puts "=========分割线========="
   puts "未注册用户#{weizhuce}"
   # geometry.close
-# rescue
-#   puts "出错,请重试"
-# end
+rescue
+  puts "出错,请重试"
+end
--- lib/geometry_campaign.rb
+++ lib/geometry_campaign.rb
@@ -1,7 +1,7 @@
 require 'roo'
 begin
-	# xlsx = Roo::Spreadsheet.open("#{Rails.root}/public/campaign.xlsx")
-	# ids = xlsx.column(1)
+	xlsx = Roo::Spreadsheet.open("#{Rails.root}/public/campaign.xlsx")
+	ids = xlsx.column(1)
 	# geometry_campaign = File.new("#{Rails.root}/public/campaign.txt","w+")
 	# ids = []
 	# Admintag.find_by(tag: "geometry").kols.distinct.each do |t|
@@ -16,9 +16,13 @@ begin
 	  	t.img_status = "passed" 
 	    t.save
 	  end
-	  t.kol.amount = t.kol.amount - t.campaign.per_action_budget
-	  t.kol.save
-	  # t.kol.amount = t.kol.amount - t.campaign.per_action_budget unless ids.include?(t.kol_id)
+	  # t.kol.amount = t.kol.amount - t.campaign.per_action_budget
+	  # t.kol.save
+	  unless ids.include?(t.kol_id)
+	    t.kol.amount = t.kol.amount - t.campaign.per_action_budget
+	    t.kol.save
+	  end
+
 
 	  detail = "#{t.kol_id}@#{t.kol.mobile_number}@#{t.campaign_id}@#{t.total_click}@#{t.avail_click}&" + detail
 	  # geometry_campaign.write("#{detail}&")
