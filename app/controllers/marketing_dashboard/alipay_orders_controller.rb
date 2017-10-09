7be4942dd03cb8e9da75688eab417edf4e03e4ca
huazai <470021389@qq.com>
2017-10-09 17:22:27 +0800

    Add csv to marketing_dashboard

--- app/controllers/marketing_dashboard/alipay_orders_controller.rb
+++ app/controllers/marketing_dashboard/alipay_orders_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class MarketingDashboard::AlipayOrdersController < MarketingDashboard::BaseController
   def index
     @alipay_orders = AlipayOrder.all
@@ -7,6 +9,19 @@ class MarketingDashboard::AlipayOrdersController < MarketingDashboard::BaseContr
   def from_pc
     @alipay_orders = AlipayOrder.where(recharge_from: ["pc", nil])
     get_alipay_orders
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        csv_string = CSV.generate do |csv|
+          csv << ["ID", "品牌主ID", "品牌昵称", "品牌主手机", "销售人员", "下单时间", "订单号", "支付宝订单号", "充值金额", "税费", "充值状态", "查看流水（若未付款则为空）"]
+          @alipay_orders.each do |c|
+            csv << [c.id, c.user_id, c.id, c.user.smart_name, c.user.mobile_number,  c.user.seller.name, c.created_at.strftime("%Y-%m-%d %H:%M:%S"), c.trade_no, c.alipay_trade_no, c.credits, c.tax, c.status, "查看流水" ]
+          end
+        end
+        send_data csv_string, :filename => "手动充值列表##{Time.current.strftime("%Y-%m-%d")}.csv"
+      }
+    end
   end
 
   def from_app
@@ -20,6 +35,19 @@ class MarketingDashboard::AlipayOrdersController < MarketingDashboard::BaseContr
 
     @q = @campaigns.ransack(params[:q])
     @campaigns = @q.result.order('created_at DESC').paginate(paginate_params)
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        csv_string = CSV.generate do |csv|
+          csv << ["活动ID", "活动名称", "品牌主ID", "品牌名称", "订单号", "支付宝订单号", "支付金额", "支付状态", "下单时间", "销售人员"]
+          @campaigns.each do |c|
+            csv << [c.id, c.name, c.user.id, c.user.name, c.trade_number, c.alipay_trade_no, c.alipay_amount, c.alipay_status.zero?, c.created_at.strftime("%Y-%m-%d %H:%M") ]
+          end
+        end
+        send_data csv_string, :filename => "支付宝直接付款发起的活动 #{Time.current.strftime("%Y-%m-%d")}.csv"
+    }
+    end
   end
 
   def change_campaign_desc
