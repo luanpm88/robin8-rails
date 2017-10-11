200542d2a0905c252c0c002dceb944af606b7de2
huazai <470021389@qq.com>
2017-10-11 17:01:48 +0800

    can putout phone list

--- lib/campaign_rodrigo.rb
+++ lib/campaign_rodrigo.rb
@@ -1,34 +1,33 @@
 require 'roo'
 
 begin
-  #old_number = Roo::Spreadsheet.open("#{Rails.root}/public/rodrigo.xlsx").column(1)
-  xinyonghu = []
+  old_number = Roo::Spreadsheet.open("#{Rails.root}/public/rodrigo.xlsx").column(1)
 
   #number = []
   #找出10月6号和7号这两天新注册的用户
-  kols = Kol.where("created_at > ? and created_at < ?","2017-10-06 00:00:00" ,"2017-10-07 23:59:59")
-
+  kols = Kol.where("created_at > ? and created_at < ?","2015-10-15 00:00:00" ,"2016-10-15 23:59:59")
+  #puts kols
+  ids = []
   kols.each do |t|
     #判断这两天新注册的用户是否参加了4221这个活动
-    cam = CampaignInvite.find_by(campaign_id: 4221 , kol_id: t.id)
-    if cam.present?
-      xinyonghu.push t.mobile_number
-      t.admintags << Admintag.find_or_create_by(tag:'Rodrigo') if t.admintags.blank?
+    cam = CampaignInvite.find_by(campaign_id: 122, kol_id: t.id)
+    if cam.present? && t.admintags.blank?
+      t.admintags << Admintag.find_or_create_by(tag:'Rodrigo')
+      ids.push t.mobile_number.to_i unless old_number.include?(t.mobile_number)
     end
   end
 
-
   #kols = CampaignInvite.where(campaign_id: 4221 ).kols.where("created_at >= ? and created_at <= ?","2017-10-06 00:00:00" ,"2017-10-07 23:59:59")
-
-  #all = kols.map {|t| t.kol.mobile_number.to_i}
+  #
+  # all = kols.map {|t| t.kol.mobile_number.to_i}
   # kols.each do |t|
   # #  t.kol.admintags << Admintag.find_or_create_by(tag:'Rodrigo') if t.kol.admintags.blank?
-  #   xinyonghu.push t.kol.mobile_number unless old_number.include?(t.kol.mobile_number)
+  #   # xinyonghu.push t.kol.mobile_number unless old_number.include?(t.kol.mobile_number)
   # end
 
   #puts "所有用户#{all}"
   #puts "=========分割线========="
-  puts "新用户#{xinyonghu}"
+  puts "新用户#{ids}"
 rescue
   puts '出错了，请检查'
 end
