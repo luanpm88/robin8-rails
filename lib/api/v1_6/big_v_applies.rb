1f2145205e971b49a4bb2e95bcb00330f7104dd7
yin <yin@yindeMacBook-Pro.local>
2017-10-12 12:05:23 +0800

    fixed bug

--- lib/api/v1_6/big_v_applies.rb
+++ lib/api/v1_6/big_v_applies.rb
@@ -29,6 +29,7 @@ module API
                                      :desc => params[:desc], :gender => gender, :age => params[:age])
           current_kol.tags  = Tag.where(:name => params[:tag_names].split(",")) rescue nil
           current_kol.avatar = params[:avatar]  if params[:avatar].present?
+          return error_403!({erroe: 1, detail: '请先绑定手机号'}) unless current_kol.mobile_number
           # current_kol.cover_images = [Image.create!(:referable => current_kol, :avatar => params[:avatar], :sub_type => 'cover')]
           current_kol.save
           current_kol.update_columns(:role_apply_status => 'applying', :role_apply_time => Time.now)   if current_kol.is_big_v?
