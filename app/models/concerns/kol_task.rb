81be025b9d77cf6984d0cad35d66a9e507316fba
huazai <470021389@qq.com>
2017-11-13 19:10:33 +0800

    add callback on invite_reward function

--- app/models/concerns/kol_task.rb
+++ app/models/concerns/kol_task.rb
@@ -7,8 +7,8 @@ module Concerns
       # after_create :generate_invite_code
 
       # Kol's inviter is rewarded only after Kol gets approved
-      #after_create :generate_invite_task_record
-      after_update :generate_invite_task_record
+      after_create :generate_invite_task_record
+      #after_update :generate_invite_task_record
     end
 
     class_methods do
@@ -45,12 +45,13 @@ module Concerns
     #兼容cpi  如果不是邀请好友注册，此时还好判断该用户是否通过cpi活动注册
     def generate_invite_task_record
       # Inviter isn't rewarded unless Kol got approved in admin panel
-      return unless self.role_apply_status == 'passed'
+      #return unless self.role_apply_status == 'passed'
 
+      #device_exist如果为真，说明此用户有重复
       if self.IMEI.present?
         device_exist = Kol.where(:IMEI => self.IMEI).where("mobile_number != '#{Kol::TouristMobileNumber}'").size > 1
       elsif self.IDFA.present?
-        device_exist = Kol.where(:IDFA => self.IDFA).where("mobile_number != '#{Kol::TouristMobileNumber}'").size > 1
+        device_exist = Kol.where(:IDFA => self.IDFA).where("mobile_number != '#{Kol::TouristMobileNumber}'").size > 20
       else
         device_exist = true
       end
@@ -71,7 +72,8 @@ module Concerns
         invitation.update!(status: 'completed', invitee_id: self.id, registered_at: Time.now)
 
         task_record = inviter.task_records.create(:task_type => RewardTask::InviteFriend, :status => 'active', :invitees_id => self.id)
-        task_record.sync_to_transaction  #  if inviter.today_invite_count <= 5
+        task_record.sync_to_transaction
+        # if inviter.today_invite_count <= 5
       end
 
       # download_invitation = DownloadInvitation.find_invation(self)
