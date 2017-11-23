a50a312b144f21b057ce0069f884c75d6e45b814
yin <zyin@robin8.net>
2017-11-23 18:29:38 +0800

    fix bug XD

--- lib/api/v1/campaign_invites.rb
+++ lib/api/v1/campaign_invites.rb
@@ -68,8 +68,8 @@ module API
         end
         get 'my_campaigns' do
           if params[:state] == 'pending'
-            # kol_campaigns = current_kol.campaign_invites.where(status: ['running','finished']).order(updated_at: :desc).page(params[:page]).per_page(10)
-            kol_campaigns = current_kol.campaign_invites.where.not(status: ['settled','rejected']).order(updated_at: :desc).page(params[:page]).per_page(10)
+            # kol_campaigns = current_kol.campaign_invites.joins(:campaign).where("campaign_invites.status = 'running' OR campaign_invites.status = 'finished' AND campaigns.status !=  'finished' ") 
+            kol_campaigns = current_kol.campaign_invites.where(status: ['running','finished','approved']).order(updated_at: :desc).page(params[:page]).per_page(10)
           else
             kol_campaigns = current_kol.campaign_invites.where(status: ['settled','rejected'] , img_status: params[:status]).order(updated_at: :desc).page(params[:page]).per_page(10)
           end
