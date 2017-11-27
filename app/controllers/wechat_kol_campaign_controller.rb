4633bdc0c22c1bf9f7d67e6d2d6710a9c16eac1f
yin <zyin@robin8.net>
2017-11-27 14:10:40 +0800

    update h5

--- app/controllers/wechat_kol_campaign_controller.rb
+++ app/controllers/wechat_kol_campaign_controller.rb
@@ -3,16 +3,10 @@ class WechatKolCampaignController < ApplicationController
 
   def campaign
     Rails.logger.wechat_kol_campaign.info "--checked: #{params}"
-    kol = Kol.find_or_create_by(channel: params[:cid, cid: params[:cid])
-    kol.add_campaign_id @campaign.id
-    kol.approve_campaign @campaign_id
+    kol = Kol.find_or_create_by(channel: params[:channel_id], cid: params[:cid])
+    kol.add_campaign_id params[:campaign_id]
+    kol.approve_campaign params[:campaign_id]
     @campaign_invite = @campaign.get_campaign_invite(kol.id) rescue nil
-    if @campaign_invite
-      campaign_invite_uuid = @campaign_invite.uuid
-      Rails.logger.wechat_kol_campaign.info "--campaign_details: campaign_invite_uuid #{campaign_invite_uuid}"
-      @share_url = @campaign_invite.visit_url if campaign_invite_uuid
-      Rails.logger.wechat_kol_campaign.info "--campaign_details: @share_url #{@share_url}"
-    end
     @share_url ||= "#{Rails.application.secrets.domain}/campaign_visit?campaign_id=#{campaign.id}" rescue ''
     Rails.logger.wechat_kol_campaign.info "--campaign_details: @share_url #{@share_url}"
     @app_download_url = Rails.application.secrets[:download_url]
@@ -22,12 +16,17 @@ class WechatKolCampaignController < ApplicationController
   private
 
   def valid_request?
-    key = "RANDOM_KEY"
-    data = "id=#{params[:id]}&channel_id=#{params[:channel_id]}&cid=#{params[:cid]}&nonce=#{params[:nonce]}&timestamp=#{params[:timestamp]}"
-    digest = OpenSSL::Digest.new('sha1')
-    hmac = OpenSSL::HMAC.hexdigest(digest, key, data)
+    # key = "RANDOM_KEY"
+    # data = "id=#{params[:id]}&channel_id=#{params[:channel_id]}&cid=#{params[:cid]}&nonce=#{params[:nonce]}&timestamp=#{params[:timestamp]}"
+    # digest = OpenSSL::Digest.new('sha1')
+    # hmac = OpenSSL::HMAC.hexdigest(digest, key, data)
+    # @campaign = Campaign.find(params[:campaign_id]) rescue nil
+    # unless hmac == params[:signature] || @campaign.present?
+    #   Rails.logger.wechat_kol_campaign.info "--check-error: #{params}"
+    #   render text: "error", status: "422" and return
+    # end
     @campaign = Campaign.find(params[:campaign_id]) rescue nil
-    if hmac != params[:signature] || @campaign
+    unless  @campaign.present?
       Rails.logger.wechat_kol_campaign.info "--check-error: #{params}"
       render text: "error", status: "422" and return
     end
