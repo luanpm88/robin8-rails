642c613e5a779ebf04a5aed1a4122247d588dd71
SoonKhen OwYong <dude@owyong.sk>
2017-11-28 04:23:22 +0000

    addon on zhendong channel_h5, remove some unused html

--- app/controllers/partner_campaign_controller.rb
+++ app/controllers/partner_campaign_controller.rb
@@ -2,21 +2,21 @@ class PartnerCampaignController < ApplicationController
   before_action :valid_signature?
 
   def campaign
-    @campaign = Campaign.find(params[:id])
-    Rails.logger.wechat_kol_campaign.info "--checked: #{params}"
-    kol = Kol.find_or_create_by(channel: params[:channel_id], cid: params[:cid])
-    kol.add_campaign_id params[:campaign_id]
-    kol.approve_campaign params[:campaign_id]
+    @campaign = Campaign.find(params[:id]) rescue nil
+    Rails.logger.partner_campaign.info "--checked: #{params}"
+    kol = Kol.find_or_create_by(channel: params.require(:channel_id),
+                                cid:     params.require(:cid))
+    kol.add_campaign_id  params[:id]
+    kol.approve_campaign params[:id]
     @campaign_invite = @campaign.get_campaign_invite(kol.id) rescue nil
-    if campaign_invite
+    if @campaign_invite
       campaign_invite_uuid = @campaign_invite.uuid
-      Rails.logger.wechat_campaign.info "--campaign_details: campaign_invite_uuid #{campaign_invite_uuid}"
+      Rails.logger.partner_campaign.info "--campaign_details: campaign_invite_uuid #{campaign_invite_uuid}"
       @share_url = @campaign_invite.visit_url if campaign_invite_uuid
-      Rails.logger.wechat_campaign.info "--campaign_details: @share_url #{@share_url}"
+      Rails.logger.partner_campaign.info "--campaign_details: @share_url #{@share_url}"
     end
-    @share_url ||= "#{Rails.application.secrets.domain}/campaign_visit?campaign_id=#{campaign.id}" rescue ''
-    Rails.logger.wechat_kol_campaign.info "--campaign_details: @share_url #{@share_url}"
-    @app_download_url = Rails.application.secrets[:download_url]
+    @share_url ||= "#{Rails.application.secrets.domain}/campaign_visit?campaign_id=#{@campaign.id}" rescue ''
+    Rails.logger.partner_campaign.info "--campaign_details: @share_url #{@share_url}"
     render :layout => false
   end
 
@@ -30,7 +30,7 @@ class PartnerCampaignController < ApplicationController
     digest    = OpenSSL::Digest.new('sha1')
     hmac      = OpenSSL::HMAC.hexdigest(digest, key, data)
     unless hmac == params[:signature]
-      Rails.logger.wechat_kol_campaign.info "--check-error: #{params}"
+      Rails.logger.partner_campaign.info "--check-error: #{params}"
       render text: "error", status: "422" and return
     end
   end
