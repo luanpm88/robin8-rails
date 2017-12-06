3b441e39cea811ee34109f0a60489f77b215083f
SoonKhen OwYong <dude@owyong.sk>
2017-12-06 14:48:42 +0000

    enhancements to alizhongbao partner - added rqrcode to generate QR code, uses suo.im for short URL - uses caching to save the QR code generated - added a complete share for a callback to alizhongbao server

--- app/controllers/partner_campaign_controller.rb
+++ app/controllers/partner_campaign_controller.rb
@@ -1,13 +1,20 @@
+require 'rqrcode'
+
 class PartnerCampaignController < ApplicationController
-  before_action :valid_signature? , :set_kol
-  before_action :set_campaign, only: [:campaign, :show, :complete_share]
+  before_action :valid_signature? , :set_kol, except: [:complete_share]
+  before_action :set_campaign, only: [:campaign, :show]
   layout :false
 
   def campaign
     Rails.logger.partner_campaign.info "--checked: #{params}"
     @campaign_invite , @share_url = @campaign.create_share_url(@kol)
     respond_to do |format|
-      format.html
+      format.html do
+        @qr = Rails.cache.fetch("campaign_invites/#{@campaign_invite.id}/request_url") do
+          resp = HTTParty.get("http://suo.im/api.php?format=json&url=#{CGI.escape(request.url)}").parsed_response
+          RQRCode::QRCode.new(JSON.parse(resp)["url"], size: 3, level: :h).as_svg
+        end
+      end
       format.json do
         render :json => {click: @campaign_invite.get_avail_click(true) , earn_money: @campaign_invite.earn_money , share_url: @share_url}.to_json
       end
@@ -32,6 +39,10 @@ class PartnerCampaignController < ApplicationController
   end
 
   def complete_share
+    campaign_invite = CampaignInvite.find(params[:partner_campaign_id])
+    if campaign_invite
+      AlizhongbaoCompleteShareWorker.perform_async(campaign_invite.id)
+    end
     render json: {status: '200' }.to_json
   end
 
@@ -45,8 +56,7 @@ class PartnerCampaignController < ApplicationController
       hmac      = OpenSSL::HMAC.hexdigest(digest, key, data)
       unless hmac == params[:signature]
         Rails.logger.partner_campaign.info "--check-error: #{params}"
-        # render text: "Params error", status: "422" and return
-        binding.pry
+        render text: "Params error", status: "422" and return
       end
     end
   end
@@ -55,8 +65,7 @@ class PartnerCampaignController < ApplicationController
     @campaign = Campaign.find_by(id: params[:id], channel: ['all' , params[:channel_id]]) rescue nil
     if @campaign.nil?
       Rails.logger.partner_campaign.info "--can't find campaign: #{params}"
-      # render text: "Params error", status: "422" and return
-      binding.pry
+      render text: "Params error", status: "422" and return
     end
   end
 
