6c7afdc7470edab8d6ca2161f19cb8b24174cf6c
huazai <470021389@qq.com>
2017-12-18 11:50:35 +0800

    remoce method has_any_unread_messages and optimize it

--- lib/api/v1_6/my.rb
+++ lib/api/v1_6/my.rb
@@ -12,9 +12,8 @@ module API
           present :detail, 15
           present :kol, current_kol, with: API::V1_6::Entities::BigVEntities::My
           present :is_open_indiana, true
-          present :has_any_unread_message, current_kol.has_any_unread_message?
+          present :has_any_unread_message, current_kol.unread_messages.any?
         end
-
 
         params do
           optional :page, type: Integer
