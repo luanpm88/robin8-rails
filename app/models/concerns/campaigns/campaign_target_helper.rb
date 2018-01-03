5ec4b57f6026dc528f8a18f6e51d9cde34e0f914
huazai <470021389@qq.com>
2018-01-03 15:50:31 +0800

    repair sql injection loophole in app/models/concerns/campaigns/campaign_target_helper.rb line 82

--- app/models/concerns/campaigns/campaign_target_helper.rb
+++ app/models/concerns/campaigns/campaign_target_helper.rb
@@ -79,7 +79,7 @@ module Campaigns
       if self.budget >= 500
         return []
       else
-        CampaignInvite.where("approved_at > '#{Campaign::ReceiveCampaignInterval.ago}'").collect{|t| t.kol_id}
+        CampaignInvite.where("approved_at > ?", '#{Campaign::ReceiveCampaignInterval.ago}').collect{|t| t.kol_id}
       end
     end
 
