484290e8838cdb0406d15db6b1dc7397547508f1
Zhendong Yin <zyin@robin8.com>
2018-01-16 08:23:10 +0000

    settle_partner_worker.rb edited online with Bitbucket

--- app/workers/settle_partner_worker.rb
+++ app/workers/settle_partner_worker.rb
@@ -4,7 +4,6 @@ class SettlePartnerWorker
   def self.perform(id , channel = "azb")
   	CampaignInvite.joins(:kol).where("kols.channel = ? AND campaign_invites.campaign_id = ?" , channel , id).each do |t|
   	  Partners::Alizhongbao.settle_campaign_invite(t.id)
-  	  sleep 0.5
   	end
   end
 end
