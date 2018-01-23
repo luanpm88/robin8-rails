16461e6ca7e511d5a031cc55e95b84cfbbe1a03f
huazai <470021389@qq.com>
2018-01-23 16:21:14 +0800

    remove call smart_root_url method in scan.html.erb

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
+  #config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = [/https:\/\/passport.robin8.net/, 'https://test.robin8.net', 'https://qa.robin8.net']
 
