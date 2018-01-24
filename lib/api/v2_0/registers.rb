d219c3f417f82f2118a5463d964f2919fafc3671
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-01-24 17:20:26 +0800

    update v2_0 register

--- lib/api/v2_0/registers.rb
+++ lib/api/v2_0/registers.rb
@@ -9,8 +9,8 @@ module API
         end
         get 'email_valid' do
           email = params[:email]
-          error_403!({detail: '邮箱格式错误' }) unless format_email(email)
-          error_403!({detail: '邮箱已被注册' }) if Kol.find_by(email: email)
+          error_403!(detail: '邮箱格式错误') unless format_email(email)
+          error_403!(detail: '邮箱已被注册') if Kol.find_by(email: email)
 
           valid_code = $redis.get("reg_#{email}")
 
@@ -24,7 +24,7 @@ module API
           if res.errors.empty?
             present error: 0, alert: '验证码已发送您的邮箱，请在5分钟内进行验证，过期请重新获取'
           else
-            error_403!({detail: res.errors.join('.') })
+            error_403!(detail: res.errors.join('.'))
           end
         end
 
@@ -35,14 +35,16 @@ module API
           requires :valid_code, type: String
         end
         post '/' do
-          error_403!({detail: '参数错误' }) unless [nil, 'mobile', 'email'].include?(params[:reg_type])
+          error_403!(detail: '参数错误') unless [nil, 'mobile', 'email'].include?(params[:reg_type])
+
+          _kol_hash = {password: params[:password]}
 
           if params[:reg_type] == 'email'
-            error_403!({detail: '邮箱验证错误' }) unless $redis.get("reg_#{params[:login]}") == params[:valid_code]
+            error_403!(detail: '邮箱验证错误') unless $redis.get("reg_#{params[:login]}") == params[:valid_code]
 
-            _kol_hash = {email: params[:login], password: params[:password]}
+            _kol_hash[:email] = params[:login]
           else
-            _kol_hash = {mobile_number: params[:login], password: params[:password]}
+            _kol_hash[:mobile_number] = params[:login]
           end
 
           kol = Kol.new(_kol_hash)
@@ -50,7 +52,7 @@ module API
           if kol.save!
             present error: 0, alert: '您已注册成功，请登录'
           else
-            error_403!({detail: kol.errors.join('.') })
+            error_403!(detail: kol.errors.join('.'))
           end
         end
         
