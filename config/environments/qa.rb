0bdd1c5b535539772b23b4914adfc7a18d2f40b0
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-01-29 12:33:52 +0800

    mailer config

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -83,16 +83,18 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: Rails.application.secrets[:host] }
-  ActionMailer::Base.smtp_settings = {
-    :user_name => Rails.application.secrets[:smtp][:user_name],
-    :password => Rails.application.secrets[:smtp][:password],
-    :domain => Rails.application.secrets[:smtp][:domain],
-    :address => Rails.application.secrets[:smtp][:address],
-    :port => Rails.application.secrets[:smtp][:port],
-    :authentication => :plain,
-    :enable_starttls_auto => true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.default_url_options = {host: Rails.application.secrets[:host]}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              Rails.application.secrets[:smtp][:address],
+    port:                 Rails.application.secrets[:smtp][:port],
+    domain:               Rails.application.secrets[:smtp][:domain],
+    user_name:            Rails.application.secrets[:smtp][:user_name],
+    password:             Rails.application.secrets[:smtp][:password],
+    authentication:       'login',
+    enable_starttls_auto: true
   }
-  ActionMailer::Base.delivery_method = :smtp
 end
