56c2e69fcd5f369876d656773f278bdb443ed74e
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-04-03 15:47:45 +0800

    article add and cancel

--- lib/api/v2_0/articles.rb
+++ lib/api/v2_0/articles.rb
@@ -16,6 +16,31 @@ module API
         	present :list, res, with: API::V2_0::Entities::InfluenceEntities::Articles
         end
 
+        params do
+          requires :_action, type: String, values: ElasticArticleAction::ACTIONS
+          requires :post_id, type: String
+        end
+        post 'add' do
+          eaa = current_kol.elastic_article_actions.find_or_initialize_by(_action: params[:_action], post_id: params[:post_id])
+
+          eaa.save if eaa.new_record?
+
+          present :error, 0, alert: '操作成功'
+        end
+
+        params do
+          requires :_action, type: String, values: ElasticArticleAction::ACTIONS
+          requires :post_id, type: String
+        end
+        post 'cancel' do
+          eaa = current_kol.elastic_article_actions.find_by(_action: params[:_action], post_id: params[:post_id])
+
+          eaa.destory if eaa
+
+          present :error, 0, alert: '操作成功'
+        end
+
+
       end
     end
   end
