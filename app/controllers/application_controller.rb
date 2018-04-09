d2f0ff5b4a333c9ae640a84343486e755c66231a
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-04-09 23:06:54 +0800

    update application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  skip_before_filter :verify_authenticity_token
   include Concerns::BrowserRequest
   include Concerns::RequestSmsValidator
   before_filter :set_cookies
