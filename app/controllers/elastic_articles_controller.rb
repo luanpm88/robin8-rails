97a7072b0b3fc2a5069acf2cf7ece941380a427d
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-04-11 17:08:40 +0800

    update forward

--- app/controllers/elastic_articles_controller.rb
+++ app/controllers/elastic_articles_controller.rb
@@ -2,22 +2,22 @@ class ElasticArticlesController < ApplicationController
 	layout false
 
   def forward
-  	@res = $redis.hgetall("elastic_#{params[:post_id]}")
+  	@res = $redis.hgetall("elastic_#{params[:id]}")
 
   	if @res.empty?
     	res = ElasticArticleExtend.get_by_post_ids([params[:id]]).first
     	if res
-    		$redis.hset("elastic_#{params[:post_id]}", 'avatar_url',   res['avatar_url'])
-    		$redis.hset("elastic_#{params[:post_id]}", 'profile_name', res['profile_name'])
-    		$redis.hset("elastic_#{params[:post_id]}", 'title', 			 res['title'])
-    		$redis.hset("elastic_#{params[:post_id]}", 'pic_content',  res['pic_content'])
-    		$redis.hset("elastic_#{params[:post_id]}", 'reads_count',  res['reads_count'])
-    		$redis.hset("elastic_#{params[:post_id]}", 'likes',        res['likes'])
-    		$redis.hset("elastic_#{params[:post_id]}", 'shares',       res['shares'])
+    		$redis.hset("elastic_#{params[:id]}", 'avatar_url',   res['avatar_url'])
+    		$redis.hset("elastic_#{params[:id]}", 'profile_name', res['profile_name'])
+    		$redis.hset("elastic_#{params[:id]}", 'title', 			 res['title'])
+    		$redis.hset("elastic_#{params[:id]}", 'pic_content',  res['pic_content'])
+    		$redis.hset("elastic_#{params[:id]}", 'reads_count',  res['reads_count'])
+    		$redis.hset("elastic_#{params[:id]}", 'likes',        res['likes'])
+    		$redis.hset("elastic_#{params[:id]}", 'shares',       res['shares'])
 
-    		$redis.expire("elastic_#{params[:post_id]}", 36000)
+    		$redis.expire("elastic_#{params[:id]}", 36000)
 
-    		@res = $redis.hgetall("elastic_#{params[:post_id]}")
+    		@res = $redis.hgetall("elastic_#{params[:id]}")
     	else
     		render text: 'not_found'
     	end
