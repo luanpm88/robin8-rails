28e496d746dc5e3b8f1cdd2d1ebda5ebac5c6afe
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-05-07 11:11:48 +0800

    marketing_dashboard elastic_articles index

--- app/controllers/marketing_dashboard/elastic_articles_controller.rb
+++ app/controllers/marketing_dashboard/elastic_articles_controller.rb
@@ -1,6 +1,8 @@
 class MarketingDashboard::ElasticArticlesController < MarketingDashboard::BaseController
 
   def index
+  	@q = ElasticArticle.ransack(params[:q])
+    @elastic_articles = @q.result.order('updated_at DESC').paginate(paginate_params)
   end
 
   def kols
