f0e76f01cc59ccc00a1358bde970de2cd87f9751
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-05-09 15:36:36 +0800

    update marketing_dashboard announcements

--- app/controllers/marketing_dashboard/announcements_controller.rb
+++ app/controllers/marketing_dashboard/announcements_controller.rb
@@ -11,6 +11,11 @@ class MarketingDashboard::AnnouncementsController < MarketingDashboard::BaseCont
     @announcement = Announcement.new
   end
 
+  def edit
+    authorize! :read, Announcement
+    @announcement = Announcement.find(params[:id])
+  end
+
   def create
     authorize! :update, Announcement
     params.permit!
@@ -18,6 +23,20 @@ class MarketingDashboard::AnnouncementsController < MarketingDashboard::BaseCont
     if @announcement.save
       redirect_to :action => :index
     end
+  end
+
+  def update
+    authorize! :update, Announcement
+    @announcement = Announcement.find params[:id]
+
+    if @announcement.update_attributes!(params[:announcement].permit!)
+      flash[:notice] = "修改成功"
+      redirect_to action: :index
+    else
+      flash[:alert] = "修改失败, 请重试"
+      render 'edit'
+    end
+
   end
 
   def destroy
