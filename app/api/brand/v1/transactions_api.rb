e60f45fbf0c84e555a59a28ec0c3ae44bccd4085
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-05-24 15:02:04 +0800

    credit expend

--- app/api/brand/v1/transactions_api.rb
+++ app/api/brand/v1/transactions_api.rb
@@ -9,7 +9,7 @@ module Brand
       resource :transactions do
         paginate per_page: 8
         get "/" do
-          transactions = paginate(Kaminari.paginate_array(current_user.paid_transactions.order('created_at DESC')))
+          transactions = paginate(Kaminari.paginate_array(current_user.paid_transactions.includes(:item).order('created_at DESC')))
           present transactions
         end
       end
