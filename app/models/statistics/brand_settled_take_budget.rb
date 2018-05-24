0b2b0cc444fbe5a58d824c208f8d165e6d66efbd
Fergus Tang <ftang@robin8.com>
2018-05-24 10:32:20 +0800

    update

--- app/models/statistics/brand_settled_take_budget.rb
+++ app/models/statistics/brand_settled_take_budget.rb
@@ -3,46 +3,52 @@ class Statistics::BrandSettledTakeBudget < ActiveRecord::Base
     adminTag = Admintag.find_by(tag: tag_name)
     page_size = 100
     index = 0
-    total = Campaign.count
+    beginTime = Time.new
 
     result = Array.new
     loop do
+      brand_count = Statistics::BrandSettledTakeBudget
+                      .find_by_sql("select user_id, tag, count(id) as total_campaign_count,
+sum(take_budget) as total_take_budget from statistics_campaign_settled_take_budgets group by user_id, tag").count
+
       puts "Looking records for Campaign page size : " + page_size.to_s + " index at: " + index.to_s
-      brand_count = StatCampaignSettledTakeBudget
-      .find_by_sql("select user_id, tag, count(id) as total_campaign_count,
-sum(take_budget) as total_take_budget from stat_campaign_settled_take_budget group by user_id, tag").count
-#        .select("user_id, count(id) as total_campaign_count, sum(take_budget) as total_take_budget")
- #                     .group("tag, user_id").order("total_take_budget desc").limit(page_size).offset(index)
-                   #   .count
 
       puts "brand count " + brand_count.to_s
-      break
-      brand_count = Campaign.order("id").limit(page_size).offset(index).count
-      if campaign_count == 0
+      puts ">>>>>>>>>>> c count " + brand_count.to_s
+
+      if index > brand_count
         # no record anymore
         break
       else
-        campaigns = Campaign.order("id").limit(page_size).offset(index)
-        campaigns.each do |c|
-          puts "c id " + c.id.to_s
-          take_budget = c.take_budget
-          cTakeBudget = Statistics::BrandSettledTakeBudget.new(c.id, take_budget, c.user_id)
-          result[result.length] = cTakeBudget;
 
+        brandSettledTakeBudgets = Statistics::BrandSettledTakeBudget
+          .find_by_sql("select user_id, tag, count(id) as total_campaign_count,
+            sum(take_budget) as total_take_budget from statistics_campaign_settled_take_budgets group by user_id, tag limit " + page_size.to_s + " offset " + index.to_s
+          )
+        puts "brandSettledTakeBudgets size " + brandSettledTakeBudgets.size.to_s
+
+        brandSettledTakeBudgets.each do |c|
+
+          bTakeBudget = Statistics::BrandSettledTakeBudget.new
+          bTakeBudget.tag = c.tag
+          bTakeBudget.user_id = c.user_id
+
+          bTakeBudget.total_campaign_count = c.total_campaign_count
+          bTakeBudget.total_take_budget = c.total_take_budget
+          bTakeBudget.deleted = 0
+          bTakeBudget.created_at = Time.now
+          bTakeBudget.updated_at = Time.now
+          bTakeBudget.created_by = "system"
+          bTakeBudget.updated_by = "system"
+
+          ## result[result.length] = bTakeBudget
+          bTakeBudget.save
         end
+
       end
       index = index + page_size
     end
-    puts "End : " + result.length.to_s
-    result.each do |e|
-      puts " result ==> user id " + e.user_id.to_s + " campaign id " + e.campaign_id.to_s + " take budget " + e.take_budget
-    end
-
-      # Campaign.order("id").limit(page_size).offset(index).
-    # Campaign.all.where("id < 10").map(&:id).each do |c|
-    #   puts "c id= " + c.id.to_s
-    # end
-
-
+    endTime = Time.new
+    puts "End : " + result.length.to_s + " execution time " + (endTime - beginTime).to_s
   end
 end
