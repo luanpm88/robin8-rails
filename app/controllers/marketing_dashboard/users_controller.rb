1c96bd864c9e71dfb878431eb4fed4e08f804df6
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-06-06 14:44:39 +0800

    marketing_dashboard recharge credit and amount

--- app/controllers/marketing_dashboard/users_controller.rb
+++ app/controllers/marketing_dashboard/users_controller.rb
@@ -68,9 +68,39 @@ class MarketingDashboard::UsersController < MarketingDashboard::BaseController
 
     credits, score, expired_at = params[:credits].to_f, params[:score].to_i, nil
 
-    if credits <= 0 || score < 0
-      flash[:alert] = "金额不能为空"
+    if credits <= 0 && score <= 0
+      flash[:alert] = "金额或积分不能都为空"
       return render 'recharge'
+    end
+
+    recharge_record = nil
+
+    if credits > 0
+      recharge_record = RechargeRecord.create(
+        credits:        credits,
+        tax:            0,
+        need_invoice:   false,
+        status:         "pending",
+        receiver_name:  params[:receiver_name],
+        receiver:       @user,
+        operator:       current_admin_user.email,
+        admin_user:     current_admin_user,
+        remark:         params[:remark]
+      )
+
+      if @user.income(credits, 'manual_recharge', recharge_record)
+        recharge_record.update(status: "success")
+        @user.increment!(:appliable_credits, credits)
+        @user.update(seller_id: params[:seller_id]) if params[:seller_id]
+
+        flash[:notice] = '为品牌主充值成功'
+      else
+        recharge_record.update(status: "failed")
+
+        flash[:alert] = '为品牌主充值失败，请联系技术支持'
+
+        return render 'recharge'
+      end
     end
 
     if score > 0
@@ -79,40 +109,8 @@ class MarketingDashboard::UsersController < MarketingDashboard::BaseController
         flash[:alert] = "积分有效时间不能小于当前时间"
         return render 'recharge'
       end
-    end
-    
-    recharge_record = RechargeRecord.create(
-      credits: credits,
-      tax: 0,
-      need_invoice: false,
-      status: "pending",
-      receiver_name: params[:receiver_name],
-      receiver: @user,
-      operator: current_admin_user.email,
-      admin_user: current_admin_user,
-      remark: params[:remark]
-    )
 
-    if @user.income(credits, 'manual_recharge', recharge_record)
-      recharge_record.update(status: "success")
-      @user.increment!(:appliable_credits, credits)
-      @user.update(seller_id: params[:seller_id]) if params[:seller_id]
-      # 送积分{_method, score, owner, resource, expired_at, remark}
-      Credit.gen_record(
-        'recharge',
-        1,
-        score,
-        @user,
-        recharge_record,
-        expired_at,
-        "充#{credits}元, 赠送#{score}积分"
-      ) if score > 0
-
-      flash[:notice] = '为品牌主充值成功'
-    else
-      recharge_record.update(status: "failed")
-
-      flash[:alert] = '为品牌主充值失败，请联系技术支持'
+      Credit.gen_record('recharge', 1, score, @user, recharge_record, expired_at, "充#{credits}元, 赠送#{score}积分")
     end
 
     respond_to do |format|
