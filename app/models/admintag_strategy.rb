71efe77fed8a1f142852dab85e05fb82ff6deae5
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-06-21 20:29:46 +0800

    update model admintag_strategy

--- app/models/admintag_strategy.rb
+++ app/models/admintag_strategy.rb
@@ -8,13 +8,15 @@ class AdmintagStrategy < ActiveRecord::Base
 	# unit_price_rate_for_admin:  default 0   当这个值大于0时，管理员会得其以下成员活动收益的别外一部分
 	# master_income_rate:         default 0   geometry为0,其他为kol默认收益的5%
 
+  # actual_per_action_budget: amount paid to KOL (currently 60% of per_action_budget)
+  # actual_per_action_budget * unit_price_rate_for_kol
 	# 默认活动单价用户抽60%， 活动收益师傅额外得徒弟的5%(系统发放，不抽用户的钱)，成功邀请一个好友奖励2元
 	InitHash = 	{
 							  register_bounty:            0,
 								invite_bounty:              2,
 								invite_bounty_for_admin:    0,
 								first_task_bounty:          0,
-								unit_price_rate_for_kol:    0.6,
+								unit_price_rate_for_kol:    1,
 								unit_price_rate_for_admin:  0,
 								master_income_rate:         0.05,
 							}
@@ -27,8 +29,8 @@ class AdmintagStrategy < ActiveRecord::Base
 			invite_bounty: 					 		invite_bounty,
 			invite_bounty_for_admin: 		invite_bounty_for_admin,
 			first_task_bounty: 					first_task_bounty,
-			unit_price_rate_for_kol: 		unit_price_rate_for_kol * 0.6,
-			unit_price_rate_for_admin: 	unit_price_rate_for_admin * 0.6,
+			unit_price_rate_for_kol: 		unit_price_rate_for_kol,
+			unit_price_rate_for_admin: 	unit_price_rate_for_admin,
 			master_income_rate: 				master_income_rate
 		}
 	end
