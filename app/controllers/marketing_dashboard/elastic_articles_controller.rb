892ceab7bdaf2ec692a2f30e4aa3f748eec00113
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2018-07-09 17:35:53 +0800

    refs: add kols_red_money function to ElasticArticlesController

--- app/controllers/marketing_dashboard/elastic_articles_controller.rb
+++ app/controllers/marketing_dashboard/elastic_articles_controller.rb
@@ -10,5 +10,10 @@ class MarketingDashboard::ElasticArticlesController < MarketingDashboard::BaseCo
   	@q 		= Kol.joins(:elastic_article_actions).where("kols.id=elastic_article_actions.kol_id").ransack(params[:q])
     @kols = @q.result.uniq.order('id DESC').paginate(paginate_params)
   end
+
+  def kols_red_money
+  	@q		= Kol.joins(:transactions).where("transactions.account_type='Kol' and transactions.subject='red_money' and kols.id=transactions.account_id").ransack(params[:q])
+    @kols = @q.result.uniq.order('id DESC').paginate(paginate_params)
+  end
   
 end
