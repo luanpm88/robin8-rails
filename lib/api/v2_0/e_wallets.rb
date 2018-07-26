3016fa49fa1b54241bdf31d29f10e556f342dfe1
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2018-07-26 10:36:35 +0800

    refs: add api/v2_0/e_wallets.rb function

--- lib/api/v2_0/e_wallets.rb
+++ lib/api/v2_0/e_wallets.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require "pems"
+require 'pmes'
 module API
   module V2_0
     class EWallets < Grape::API
@@ -9,18 +9,29 @@ module API
         end
 
         params do
-          requires :password, type: String
+          requires :password, type: String, regexp: /^\d{6}$/
         end
         post 'apply_open' do
           result = PMES.gen_e_wallet(password)
-          token = JSON.parse(result)['token']
-          current_kol.e_wallet_account(token: token)
+          return error_403!({error: 1, detail: 'PMES gen_e_wallet 获取错误'}) unless result
+          e_wallet_account = current_kol.e_wallet_account
+          EWallet::Account.create(token: JSON.parse(result)['token'] , kol_id: current_kol.id) unless e_wallet_account
+
+          present :error, 0
+          present :alert, '创建成功'
         end
 
         params do 
           requires :token, type: String
         end
         get 'info' do
+          result = PMES.get_e_wallet(token)
+          return error_403!({error: 1, detail: 'PMES get_e_wallet 获取错误'}) unless result
+
+          $redis.set("#{current_kol.id}_put_account", JSON.parse(result)['PUT']['amount'])
+
+          present :error, 0
+          present :kol_put_account, $redis.get("#{current_kol.id}_put_account")
         end
 
         params do 
@@ -28,8 +39,6 @@ module API
         end
         get 'transfers_list' do
         end
-
-
 
 
       end
