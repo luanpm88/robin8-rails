4c009996bca36be87b85c73ee92914486106c802
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2018-07-26 15:14:02 +0800

    refs: add user and kol for the e_wattel_account

--- lib/api/v2_0/e_wallets.rb
+++ lib/api/v2_0/e_wallets.rb
@@ -12,20 +12,20 @@ module API
           requires :password, type: String, regexp: /^\d{6}$/
         end
         post 'apply_open' do
+          return error_403!({error: 1, detail: '电子钱包已开通'}) unless current_kol.e_wallet_account
+
           result = PMES.gen_e_wallet(password)
           return error_403!({error: 1, detail: 'PMES gen_e_wallet 获取错误'}) unless result
-          e_wallet_account = current_kol.e_wallet_account
-          EWallet::Account.create(token: JSON.parse(result)['token'] , kol_id: current_kol.id) unless e_wallet_account
+
+          EWallet::Account.create(token: JSON.parse(result)['token'] , kol_id: current_kol.id) 
 
           present :error, 0
           present :alert, '创建成功'
         end
 
-        params do 
-          requires :token, type: String
-        end
+
         get 'info' do
-          result = PMES.get_e_wallet(token)
+          result = PMES.get_e_wallet(current_kol.e_wallet_account.token)
           return error_403!({error: 1, detail: 'PMES get_e_wallet 获取错误'}) unless result
 
           $redis.set("#{current_kol.id}_put_account", JSON.parse(result)['PUT']['amount'])
