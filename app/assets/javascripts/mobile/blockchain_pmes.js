0f7d86d05b8934f92cef54c81721d3c7f5be952e
xingchong <cxing@robin8.com>
2018-08-23 18:31:38 +0800

    fix

--- app/assets/javascripts/mobile/blockchain_pmes.js
+++ app/assets/javascripts/mobile/blockchain_pmes.js
@@ -1,4 +1,5 @@
-var URLHOST = window.location.origin;
+// var URLHOST = window.location.origin;
+var URLHOST = $('#host_url').val();
 
 $(document).ready(function() {
   if ($('body').attr('id') === 'wechat_pages_pmes_demo') {
@@ -127,11 +128,13 @@ $(document).ready(function() {
                       $('#pmes_login_page').show();
                     },
                     error: function(xhr, type) {
+                      createAlert('ruby error');
                       console.log('error');
                     }
                   });
                 },
                 error: function(xhr, type) {
+                  createAlert('pmes post error');
                   console.log('error');
                 }
               });
@@ -145,6 +148,43 @@ $(document).ready(function() {
     }
 
     // 已有帐号
+    // var pwd = "";
+    // var len = 0;
+    // // type=tel input框
+    // var $inputs = $('.ios-password-input > input');
+    // $('#login_password').on('input', function() {
+    //   if (!$(this).val()) { //无值
+    //   }
+    //   if (/^[0-9]*$/g.test($(this).val())) { //有值且只能是数字（正则）
+    //     pwd = $(this).val().trim();
+    //     len = pwd.length;
+    //     for (var i in pwd) {
+    //       $inputs.eq(i).val(pwd[i]);
+    //     }
+    //     $inputs.each(function() { //将有值的当前input 后面的所有input清空
+    //       var index = $(this).index();
+    //       if (index >= len) {
+    //         $(this).val("");
+    //       }
+    //     });
+    //     if (len === 6) {
+    //       //执行付款操作
+    //     }
+
+    //   } else { //清除val中的非数字，返回纯number的value
+    //     var arr = $(this).val().match(/\d/g);
+    //     try {
+    //       $(this).val($(this).val().slice(0, $(this).val().lastIndexOf(arr[arr.length - 1]) + 1));
+    //     } catch(e) {
+    //       // console.log(e.message)
+    //       //清空
+    //       $(this).val("");
+    //     }
+    //   }
+    //   console.log("password:" + pwd);
+    // })
+
+
     $('#pmes_login_btn').click(function(event) {
       var current_date = new Date();
       current_date = current_date.customFormat('#YYYY##MM##DD##hhh##mm#');
