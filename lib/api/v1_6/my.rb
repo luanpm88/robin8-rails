e881f3c984dd613b1907cdd2ffef1feaa98cb17d
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-09-05 17:24:01 +0800

    update put

--- lib/api/v1_6/my.rb
+++ lib/api/v1_6/my.rb
@@ -18,6 +18,7 @@ module API
           present :logo, current_kol.strategy[:logo]
           present :vest_bag_detail, $redis.get('vest_bag_detail')
           present :put_switch, $redis.get('put_switch') # 1:显示， 0:隐藏
+          present :put_count, $redis.get('put_count')
         end
 
         params do
