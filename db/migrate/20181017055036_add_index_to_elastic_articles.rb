adf4cf2037c441a75312a1a309685117b65ec57b
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2018-10-17 14:31:12 +0800

    refs:index name modify

--- db/migrate/20181017055036_add_index_to_elastic_articles.rb
+++ db/migrate/20181017055036_add_index_to_elastic_articles.rb
@@ -1,5 +1,5 @@
 class AddIndexToElasticArticles < ActiveRecord::Migration
   def change
-  	add_index :elastic_articles, [:post_id], name: :index_elastic_articles_post
+  	add_index :elastic_articles, [:post_id], name: :index_eas_on_post_id
   end
 end
