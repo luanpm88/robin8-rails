3790f3e2d7943c304de52d3753e0d2649acdd5fd
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-11-05 15:21:48 +0800

    update v2_1 applying wechat_public_account

--- lib/api/v2_1/base_infos.rb
+++ lib/api/v2_1/base_infos.rb
@@ -9,13 +9,13 @@ module API
 
         get '/' do
           # Rails.cache.delete('api:v2_1:base_infos')
-          # cache(key: "api:v2_1:base_infos") do
+          cache(key: "api:v2_1:base_infos") do
             present :error, 0
             present :circles_list,    Circle.all.order(position: :asc), with: API::V2_1::Entities::BaseInfoEntities::Circle
             present :terraces_list,   Terrace.all, with: API::V2_1::Entities::BaseInfoEntities::Terrace
             present :ages_list,       Creator::AgeRanges.values
             present :weibo_auth_list, WeiboAccount::AuthTypes.values
-          # end
+          end
         end
       end
     end
--- lib/api/v2_1/kols.rb
+++ lib/api/v2_1/kols.rb
@@ -211,7 +211,7 @@ module API
           end
 
           present :error, 0
-          present :kol,   current_kol, with: API::V2_1::Entities::KolEntities::BaseInfo
+          present :kol,   current_kol.reload, with: API::V2_1::Entities::KolEntities::BaseInfo
         end
 
         desc 'get kol base info'
