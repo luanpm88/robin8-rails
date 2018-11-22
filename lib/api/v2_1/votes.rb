e167973164c01af8a1822ba2da27e8db60c7f6e3
evan zhang <evanzhang@evandeMacBook-Pro.local>
2018-11-22 18:37:14 +0800

    update api votes

--- lib/api/v2_1/votes.rb
+++ lib/api/v2_1/votes.rb
@@ -7,7 +7,7 @@ module API
           authenticate! unless @options[:path].join("").match('sms')
         end
 
-        desc '我的爱豆列表'
+        desc 'my idois list'
         params do
         	requires :page, type: Integer
         end
@@ -20,7 +20,7 @@ module API
           present :list, list, with: API::V2_1::Entities::KolEntities::Brief, mobile_number: current_kol.mobile_number
         end
 
-        desc '我的粉丝列表'
+        desc 'my fans list'
         params do
         	requires :page, type: Integer
         end
@@ -31,7 +31,7 @@ module API
           present :list, list, with: API::V2_1::Entities::KolEntities::Voter
         end
 
-        desc '我要报名，我要当网红'
+        desc 'be kol'
         post 'be_kol' do
           current_kol.update_columns(is_hot: 0) unless current_kol.is_hot
 
@@ -43,11 +43,11 @@ module API
           requires :kol_id,  type: Integer
         end
         post 'vote' do
+          error_403!(detail: '24小时内对每个kol不能重复投票') if $redis.get("#{current_kol.mobile_number}_vote_kol_#{params[:kol_id]}")
+
           _kol = Kol.find_by_id(params[:kol_id])
-
+          
           error_403!(detail: '支持的信息有误') unless _kol.try(:is_hot)
-
-          error_403!(detail: '24小时内针对一个kol只能投一次票') if $redis.get("#{current_kol.mobile_number}_vote_kol_#{_kol.id}")
 
           voter_ship = VoterShip.find_or_initialize_by(kol_id: _kol.id, voter_id: current_kol.id)
           voter_ship.count += 1
@@ -60,34 +60,33 @@ module API
           present :vote_ranking, _kol.vote_ranking
         end
 
-        desc '获取投票验证码'
+        desc 'get vote sms valid_code'
         params do
           requires :mobile_number, type: String
           requires :kol_id,        type: Integer
         end
         get 'vote_sms' do
-          error_403!(detail: '手机号格式错误') unless params[:mobile_number].match(API::ApiHelpers::MOBILE_NUMBER_REGEXP)
-          error_403!(detail: '支持的信息有误') unless Kol.find_by_id(params[:kol_id]).try(:is_hot)
-
-          _key = "#{params[:mobile_number]}_vote_kol_#{params[:kol_id]}"
-
-          error_403!(detail: '24小时内针对一个kol只能投一次票') if $redis.get(_key) == '1'
+          error_403!(detail: '24小时内对每个kol不能重复投票')  if $redis.get("#{params[:mobile_number]}_vote_kol_#{params[:kol_id]}")
+          error_403!(detail: '手机号格式错误')               unless params[:mobile_number].match(API::ApiHelpers::MOBILE_NUMBER_REGEXP)
+          error_403!(detail: '支持的信息有误')               unless Kol.find_by_id(params[:kol_id]).try(:is_hot)
 
           YunPian::SendRegisterSms.new(params[:mobile_number]).send_sms
 
           present :error, 0
         end
 
-        desc '投票'
+        desc 'vote by sms valid_code'
         params do
           requires :mobile_number, type: String
           requires :kol_id,        type: Integer
           requires :code,          type: String
         end
         post 'vote_sms' do
+          error_403!(detail: '24小时内对每个kol不能重复投票') if $redis.get("#{params[:mobile_number]}_vote_kol_#{params[:kol_id]}")
+          
           code_right = YunPian::SendRegisterSms.verify_code(params[:mobile_number], params[:code])
 
-          error_403!(detail:  '验证码错误')   unless code_right
+          error_403!(detail: '验证码错误')    unless code_right
           error_403!(detail: '支持的信息有误') unless Kol.find_by_id(params[:kol_id]).try(:is_hot)
 
 
