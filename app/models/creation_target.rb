4dfc069c2e687e493798f7075eb2827029637a9c
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-01-07 18:28:02 +0800

    refs: add models

--- app/models/creation_target.rb
+++ app/models/creation_target.rb
@@ -1,3 +1,4 @@
 class CreationTarget < ActiveRecord::Base
   belongs_to :targetable, polymorphic: true
+  belongs_to :creation
 end
--- db/migrate/20190107092411_create_creation_targets.rb
+++ db/migrate/20190107092411_create_creation_targets.rb
@@ -1,8 +1,9 @@
 class CreateCreationTargets < ActiveRecord::Migration
   def change
     create_table :creation_targets do |t|
-      t.references :targetable, polymorphic: true
-      t.string :addressable_content
+      t.string :targetable_type
+      t.string :targetable_content
+      t.integer :creation_id
       t.timestamps null: false
     end
   end
