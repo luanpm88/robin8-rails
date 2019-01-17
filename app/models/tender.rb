d4e1cc1a793b7f03c89165f4fb6deba1b76d56a2
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-01-17 18:09:13 +0800

    refs:moder tender rollback modify

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -24,8 +24,11 @@ class Tender < ActiveRecord::Base
   private 
 
   def update_quoted
-    self.creation_selected_kol.update_columns(quoted: true)
+    if self.creation_selected_kol.present?
+      self.creation_selected_kol.update_columns(quoted: true)
+    else
+      self.creation_selected_kol = CreationSelectedKol.create(creation_id: self.creation_id, kol_id: self.kol_id, from_by: 'volunteered', quoted: true)
+    end
   end
-
   
 end
