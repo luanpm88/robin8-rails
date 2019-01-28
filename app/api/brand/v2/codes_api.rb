bef28790168e54e597936ed704047005246b7279
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-01-28 11:03:38 +0800

    refs: brand for the codes api modify

--- app/api/brand/v2/codes_api.rb
+++ app/api/brand/v2/codes_api.rb
@@ -5,11 +5,12 @@ module Brand
         resource :codes do
 
           params do 
-            requires :mobile_number, type: String 
+            requires :mobile_number, type: String
+            requires :login_type, values: %w(sign_up update_password)
           end
           get 'get_phone_code' do
             return {error: 1, detail: '手机号码格式错误'}   unless params[:mobile_number].match(Brand::V2::APIHelpers::MOBILE_NUMBER_REGEXP)
-            return {error: 1, detail: '帐号已存在，请登录'} if Kol.authenticate_login(params[:mobile_number])
+            return {error: 1, detail: '帐号已存在，请登录'} if params[:login_type] == "sign_up" && Kol.find_by_mobile_number(params[:mobile_number])
             
             sms_client = YunPian::SendRegisterSms.new(params[:mobile_number])
             res = sms_client.send_sms  rescue {}
@@ -22,11 +23,12 @@ module Brand
 
 
           params do 
-            requires :email, type: String 
+            requires :email, type: String
+            requires :login_type, values: %w(sign_up update_password)
           end
           get 'get_email_code' do
             return {error: 1, detail: '邮箱格式错误'}      unless params[:email].match(Brand::V2::APIHelpers::EMAIL_REGEXP)
-            return {error: 1, detail: '帐号已存在，请登录'} if Kol.authenticate_login(params[:email])
+            return {error: 1, detail: '帐号已存在，请登录'} if params[:login_type] == "sign_up" && Kol.find_by_email(params[:email])
             
             valid_code = SecureRandom.random_number(1000000)
             $redis.setex("valid_#{params[:email]}", 6000, valid_code)
