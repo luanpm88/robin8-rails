8052b9a4e1dbd7a59ea7f03d6bcb9a5daf1bf7b4
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-01-30 17:17:06 +0800

    refs: brand for the tenders api modify

--- app/api/brand/v2/tenders_api.rb
+++ app/api/brand/v2/tenders_api.rb
@@ -10,12 +10,18 @@ module Brand
 
           desc 'create tender for the head' #创建总报价
           params do
-            requires :tenders_ary, type: Array
+            requires :csk_ary, type: Array
             requires :creation_id, type: Integer
           end
 
           post "/create" do
-            tenders = Tender.where(id: params[:tenders_ary])
+            csks = CreationSelectedKol.where(id: params[:csk_ary])
+            tenders_ary = []
+            csks.each do |csk|
+              tenders_ary += csk.tenders.map &:id
+            end
+            binding.pry
+            tenders = Tender.where(id: tenders_ary)
             tender = Tender.new(head: true, creation_id: params[:creation_id])
             tender.price = tenders.sum(:price)
             tender.fee = tenders.sum(:fee)
@@ -31,6 +37,8 @@ module Brand
           end
 
 
+
+
           #验收作品成功
           params do 
             requires :creation_selected_kol_id,  type: String
@@ -43,8 +51,6 @@ module Brand
             else
               return {error: 1, detail: '数据错误，请确认'}
             end
-
-            
           end
 
 
