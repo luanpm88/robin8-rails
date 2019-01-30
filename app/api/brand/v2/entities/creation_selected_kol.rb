e01e743062cc0274577acfe958314193e1ac8c5a
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-01-30 11:21:47 +0800

    refs: brand creation selected kol api modify

--- app/api/brand/v2/entities/creation_selected_kol.rb
+++ app/api/brand/v2/entities/creation_selected_kol.rb
@@ -2,11 +2,11 @@ module Brand
   module V2
     module Entities
       class CreationSelectedKol < Entities::Base
-        expose :plateform_name, :plateform_uuid, :name, :avatar_url, :desc, :remark
+        expose :plateform_name, :plateform_uuid, :name, :avatar_url, :desc, :remark, :status
         expose :creation_selected_kol_id do |object, opts|
           object.id
         end
-        expose :status do |object, opts|
+        expose :status_zh do |object, opts|
           ::CreationSelectedKol::STATUS[object.status.to_sym]
         end
         expose :price_total do |object, opts|
