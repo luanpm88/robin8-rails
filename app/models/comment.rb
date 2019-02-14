57da221a41fa4c9d4b7163602f19fd8c9ce19d68
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-02-14 17:41:31 +0800

    refs: model comments modify

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
+  belongs_to :resource, :polymorphic => true
+  belongs_to :resource_from, :polymorphic => true
+  belongs_to :resource_to, :polymorphic => true
+
+  has_many :sub_comments, class_name: "Comment", foreign_key: :parent_id
   
 end
--- db/migrate/20190214091341_create_comments.rb
+++ db/migrate/20190214091341_create_comments.rb
@@ -1,7 +1,15 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+      t.integer :resource_id
+      t.string  :resource_type
+      t.integer :resource_from_id
+      t.string  :resource_from_type
+      t.integer :resource_to_id
+      t.string  :resource_to_type
+      t.integer :parent_id
+      t.text    :content
+      t.integer :score
       t.timestamps null: false
     end
   end
