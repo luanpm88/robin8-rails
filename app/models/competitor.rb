affba91144fba2f8af33dbbf0c0d35d544f30a74
evan zhang <evanzhang@evandeMacBook-Pro.local>
2019-02-14 18:01:14 +0800

    merge xliu

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -5,15 +5,8 @@ class Competitor < ActiveRecord::Base
     1 => '显示',
     -1 => '删除'
   }
-<<<<<<< HEAD
-  
-  default_scope ->{where.not(status: -1)}
-=======
 
   validates :status, :inclusion => { :in => [1, 0, -1] }
 
-  default_scope ->{where(status: 1)}
-
-
->>>>>>> 531a36354da66949fe90943f455954f5cdf5661b
+  default_scope ->{where.not(status: -1)}
 end
