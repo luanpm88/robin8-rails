96d17955faf54f4afd7bcd3e7498c823b63ca13a
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-02-14 14:46:59 +0800

    refs:add status to competitors and trademarks

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,3 +1,4 @@
 #竞争对手
 class Competitor < ActiveRecord::Base
+  default_scope ->{where(status: 1)}
 end
--- app/models/trademark.rb
+++ app/models/trademark.rb
@@ -2,4 +2,6 @@
 class Trademark < ActiveRecord::Base
 	belongs_to :user
 	has_many :creations
+
+  default_scope ->{where(status: 1)}
 end
