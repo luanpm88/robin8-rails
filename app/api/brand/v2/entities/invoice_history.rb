50ae5d10311b45364bebb7c42c7b299f9b622c93
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-03-05 18:06:06 +0800

    refs: incoice_history modify api

--- app/api/brand/v2/entities/invoice_history.rb
+++ app/api/brand/v2/entities/invoice_history.rb
@@ -3,8 +3,13 @@ module Brand
     module Entities
       class InvoiceHistory < Entities::Base
         expose :credits, :invoice_type, :title, :address, :status, :tracking_number
+        
         expose :created_at do |object|
           object.created_at.strftime('%Y年%m月%d日')
+        end
+
+        expose :status_zh do |object|
+          object.status_zh
         end
       end
     end
