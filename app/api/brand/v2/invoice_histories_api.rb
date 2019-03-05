8a36db366f2959b0b639d0a71e91629b530be0d5
liuxiaoyan <liuxiaoyan@liuxiaoyan.local>
2019-03-05 14:45:24 +0800

    refs: brand invoice api modify

--- app/api/brand/v2/invoice_histories_api.rb
+++ app/api/brand/v2/invoice_histories_api.rb
@@ -7,10 +7,11 @@ module Brand
       end
 
       resource :invoice_histories do
-        paginate per_page: 8
+        
         get "/" do
           invoice_histories = paginate(Kaminari.paginate_array(current_user.invoice_histories.order('created_at DESC')))
-          present invoice_histories
+
+          present invoice_histories, with: Entities::InvoiceHistory
         end
 
         desc '可申请发票的总额'
@@ -19,7 +20,7 @@ module Brand
           { appliable_credits: appliable_credits }
         end
 
-        paginate per_page: 8
+        
         params do
           requires :credits, type: String
           requires :type, type: String
@@ -27,11 +28,11 @@ module Brand
         end
         post '/' do
           if !current_user.special_invoice
-            error_unprocessable! "请完善发票信息，然后重试"
+            return {error: 1, detail: '请完善发票信息，然后重试'}
           elsif !current_user.invoice_receiver
-            error_unprocessable! "请完善发票邮寄地址信息，然后重试"
+            return {error: 1, detail: '请完善发票邮寄地址信息，然后重试'}
           elsif current_user.appliable_credits < params[:credits].to_i
-            error_unprocessable! "输入金额超出可申请金额"
+            return {error: 1, detail: '输入金额超出可申请金额'}
           end
 
           invoice_history_params = current_user.special_invoice.slice(:title, :taxpayer_id, :company_address, :company_mobile, :bank_name, :bank_account).merge(
@@ -44,7 +45,7 @@ module Brand
             invoice_histories = paginate(Kaminari.paginate_array(current_user.invoice_histories.order('created_at DESC')))
             present invoice_histories
           else
-            error_unprocessable! @invoice_history.errors.messages.first.last.first
+            return {error: 1, detail: @invoice_history.errors.messages.first.last.first}
           end
         end
 
--- app/api/brand/v2/invoice_receiver_api.rb
+++ app/api/brand/v2/invoice_receiver_api.rb
@@ -4,39 +4,37 @@ module Brand
       before do
         authenticate!
       end
+      resource :invoice_receiver do
 
-      get "/" do
-        @invoice_receiver = current_user.invoice_receiver
-        if @invoice_receiver
-          present @invoice_receiver
-        else
-          { no_invoice_receiver: true }
+        get "/" do
+          @invoice_receiver = current_user.invoice_receiver
+          if @invoice_receiver
+            present @invoice_receiver, with: Entities::InvoiceReceiver
+          else
+            { no_invoice_receiver: true }
+          end
         end
-      end
 
-      params do
-        requires :name, type: String
-        requires :phone_number, type: String
-        requires :address, type: String
-      end
-      post "/" do
-        @invoice_receiver = current_user.build_invoice_receiver(declared(params))
-        if @invoice_receiver.save
-          present @invoice_receiver
-        else
-          error_unprocessable! "保存失败，请重试"
+        params do
+          requires :name, type: String
+          requires :phone_number, type: String
+          requires :address, type: String
         end
-      end
+        post "/" do
+          @invoice_receiver = current_user.invoice_receiver
+          if @invoice_receiver
+            @invoice_receiver.update_attributes(declared(params))
+          else
+            @invoice_receiver = current_user.build_invoice_receiver(declared(params))
+          end
+          
+          if @invoice_receiver.save
+            present @invoice_receiver, with: Entities::InvoiceReceiver
+          else
+            return {error: 1, detail: '保存失败，请重试'}
+          end
+        end
 
-      params do
-        requires :name, type: String
-        requires :phone_number, type: String
-        requires :address, type: String
-      end
-      put "/" do
-        @invoice_receiver = current_user.invoice_receiver
-        @invoice_receiver.update_attributes(declared(params))
-        present @invoice_receiver
       end
     end
   end
