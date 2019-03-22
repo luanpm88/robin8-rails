d920715ce0f4634308244d9e5f381d3e4e2056f1
Evan Zhang <evan__zhang@yeah.net>
2019-03-22 10:19:10 +0800

    update selected_kol bigV_url

--- app/models/creation_selected_kol.rb
+++ app/models/creation_selected_kol.rb
@@ -51,7 +51,7 @@ class CreationSelectedKol < ActiveRecord::Base
   def bigV_url
     trademark = self.creation.user.trademarks.where(status: 1).first
 
-    "/kol/#{plateform_uuid}?type=#{plateform_name == 'public_weibo_account' ? 0 : 1}&brand_keywords=#{trademark.keywords}"
+    "/kol/#{plateform_uuid}?type=#{plateform_name == 'public_weibo_account' ? 0 : 1}&brand_keywords=#{trademark.try(:keywords)}"
   end
   
 end
