acf2dd9aaacea4efb66bfa7c7e386090e71df55a
Luan Pham <luanpm88@gmail.com>
2019-04-25 02:30:39 +0000

    fix encrease decimal demension

--- db/migrate/20190424104651_change_users_amount_column.rb
+++ db/migrate/20190424104651_change_users_amount_column.rb
@@ -1,5 +1,7 @@
 class ChangeUsersAmountColumn < ActiveRecord::Migration
   def change
     change_column :users, :amount, :decimal, :precision => 16, :scale => 2
+    change_column :users, :frozen_amount, :decimal, :precision => 16, :scale => 2
+    change_column :users, :appliable_credits, :decimal, :precision => 16, :scale => 2
   end
 end
